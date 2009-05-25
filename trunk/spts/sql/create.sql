b144dedcdbc73d52b2ebc0314d69f25d5db8e90c
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-05-25 18:08:27 +0000

    Рабочие скрипты генерации базы.Создание триггера на вставку данных в таблицу reports_info

--- trunk/spts/sql/create.sql
+++ trunk/spts/sql/create.sql
@@ -1,7 +1,20 @@
+--DROP TABLE "printers";
+CREATE TABLE printers (
+	id SERIAL,
+	IP varchar(10),
+	mandat_id  integer NOT NULL,
+        printer_name varchar(255) NOT NULL,
+	date_created date DEFAULT NOW() NOT NULL,
+	descr text,
+	PRIMARY KEY (id)
+);
+
+--DROP TABLE "reports";
 CREATE TABLE reports (
-	report_id SERIAL,
+	id SERIAL,
         printer_id integer NOT NULL,
-	fio varchar(255) NOT NULL,
+        firstpages_id integer NOT NULL,
+	user_fio varchar(255) NOT NULL,
 	doc_name varchar(255) NOT NULL,
 	date_created date DEFAULT NOW() NOT NULL,
 	mb_numder varchar(15) NOT NULL,
@@ -9,19 +22,46 @@ CREATE TABLE reports (
         page_count integer NOT NULL,
         page_copy  integer NOT NULL,
 	descr text,
-	PRIMARY KEY (report_id)
+	PRIMARY KEY (id)
 );
 
-
-CREATE TABLE printers (
-	printers_id SERIAL,
-        printer_name varchar(255) NOT NULL,
-	date_created date DEFAULT NOW() NOT NULL,
-	descr text,
-	PRIMARY KEY (printers_id)
+--DROP TABLE "firstpages";
+CREATE TABLE FirstPages (
+	id SERIAL,
+	fp_filename varchar(255) NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE VIEW "Reports" 
+-- Table: debug_log
+
+--DROP TABLE debug_log;
+
+CREATE TABLE debug_log(
+  	id SERIAL NOT NULL,
+	date_time date DEFAULT now(),
+	info_str text,
+        PRIMARY KEY (id)
+
+);
+
+ALTER TABLE debug_log OWNER TO postgres;
+
+
+--DROP TABLE print_permission;
+
+CREATE TABLE print_permission
+(
+  	id SERIAL,
+	mandat varchar(25),
+	PRIMARY KEY (id)
+)
+WITH (OIDS=FALSE);
+ALTER TABLE print_permission OWNER TO postgres;
+COMMENT ON TABLE print_permission IS '“аблица описывающа€ соотношени€ грифа документа и мандата принтера';
+
+/*
+DROP VIEW "JobsReport" ;
+CREATE VIEW "JobsReport" 
 	AS SELECT 
         "public"."reports"."date_created",
         "public"."reports"."mb_numder",
@@ -34,3 +74,4 @@ CREATE VIEW "Reports"
 	"public"."reports"."descr"
 		FROM "public"."reports" 
 		INNER JOIN "public"."printers" ON ("public"."reports"."printer_id" = "public"."printers"."printers_id")
+*/
