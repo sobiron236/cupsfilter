b27fb65906e9cb365ff0372d478a6b18bfe0a799
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-05-28 00:28:56 +0000

    Раскидал по отдельным скриптам

--- trunk/spts/sql/insert_document_level.sql
+++ trunk/spts/sql/insert_document_level.sql
@@ -1,12 +1,5 @@
-insert into document_level (marker,sec_level,ds) values ('НС','0','Не секретно');
-insert into document_level (marker,sec_level,ds) values ('С','1','Секретно');
-insert into document_level (marker,sec_level,ds) values ('СС','2','Совершенно секретно');
-insert into document_level (marker,sec_level,ds) values ('ОВ','3','Особой важности');
-
-insert into rel_mandat2doc_level(mandat_id,doclevel_id) values(1,1);
-insert into rel_mandat2doc_level(mandat_id,doclevel_id) values(1,2);
-insert into rel_mandat2doc_level(mandat_id,doclevel_id) values(1,3);
-insert into rel_mandat2doc_level(mandat_id,doclevel_id) values(1,4);
-insert into rel_mandat2doc_level(mandat_id,doclevel_id) values(2,1);
-
-select * from document_level
+insert into document_level (marker,ds) values ('НУ','Не установленно');
+insert into document_level (marker,ds) values ('НС','Не секретно');
+insert into document_level (marker,ds) values ('С','Секретно');
+insert into document_level (marker,ds) values ('СС','Совершенно секретно');
+insert into document_level (marker,ds) values ('ОВ','Особой важности');
