ed7113642a1dbbb590ae25b8f18c45ea0f4b917f
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-06-08 02:29:29 +0000

    Изменил вьювер

--- trunk/sql/view_JobsReport.sql
+++ trunk/sql/view_JobsReport.sql
@@ -1,29 +1,29 @@
---DROP VIEW "JobsReport";
+DROP VIEW "JobsReport";
 CREATE OR REPLACE VIEW "JobsReport" AS 
- SELECT 
-  printer_id,
-  printers.printer_name,
-  firstpages_id,
-  copy_number,
-  doc_level_id,
-  document_level.marker,
-  doc_name,
-  dt,
-  mb_number,
-  page_count,
-  page_copy,
-  status,
-  cups_user,
-  executor_id,
+SELECT
+  printers.printer_name as Printer_name,
+  firstpages.fp_filename as FP_path,
+  copy_number as CopyNumber,
+  document_level.marker as DocMarker,
+  doc_name as DocName,
+  dt	as DateTime,
+  mb_number as MB_Number,
+  page_count as PageCount,
+  page_copy as PageCopy,
+  cups_user as CupsUser,
   executors.fio as Executor_FIO,
-  inv_number,
-  punkt,
-  jobs_id,
-  pressman_id,
-  --executors.fio as Pressman_FIO,
-  info_str
+  inv_number as InvNumber,
+  punkt as Punkt,
+  jobs_id as JobsID,
+  pressman.fio as Pressman_FIO,
+  list_recivers.adress,
+  status as Status,
+  info_str as InfoStr
   FROM reports
    INNER JOIN printers ON reports.printer_id = printers.id
    INNER JOIN document_level ON reports.doc_level_id = document_level.id
-   INNER JOIN executors ON reports.executor_id = executors.id;
-   --INNER JOIN executors ON reports.pressman_id = executors.id;
+   INNER JOIN executors ON reports.executor_id = executors.id
+   INNER JOIN firstpages ON reports.firstpages_id = firstpages.id
+   INNER JOIN rel_list_recivers2reports ON rel_list_recivers2reports.reports_id = reports.id
+   INNER JOIN list_recivers ON rel_list_recivers2reports.list_recivers_id = list_recivers.id
+   INNER JOIN pressman ON reports.pressman_id = pressman.id;
