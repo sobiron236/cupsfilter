1e01a81848e1cfd35aebe059c5737daa21b987b2
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-06-22 03:04:12 +0000

    отладочная функцмя

---
+++ trunk/www/toLog.php
@@ -0,0 +1,35 @@
+<?php
+if (!function_exists('toLog')) {
+ /**
+  * Get var dump as a string
+  *
+  * @param $Var
+  * @return boolean
+  */
+ function print_r_log($Var) {
+  ob_start();
+  print_r($Var);
+  $ret_str = ob_get_contents();
+  ob_end_clean();
+  return $ret_str;
+ }
+
+ /**
+  * Retrieve all different elements
+  *
+  * @param $Message
+  * @return boolean
+  */
+ function toLog( $Message=false, $Method="w", $FileName="error.log" ) {
+  if(is_array($Message) or is_object($Message)) $Message = print_r_log($Message);
+  $fp=fopen("{$FileName}", $Method);
+  if($FileName=="error.log")
+  fwrite($fp, "LOG ".date("Y-m-d H:i:s").":rn".$Message."rn" );
+  else
+  fwrite($fp, $Message );
+  fclose($fp);
+  return true;
+ }
+}
+
+?>
