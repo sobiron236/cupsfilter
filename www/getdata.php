efdc5bfe694cf18dada30d0d46c806ebd1b37b3f
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-06-23 00:49:37 +0000

    Вынес js скрипты в отдельные файлы изменил форму запроса.Все поля теперь правлильно ищутся. Добавил выпадающий календарь в поле выбора даты

--- www/getdata.php
+++ www/getdata.php
@@ -3,20 +3,28 @@ require_once('dbdata.php');
 
 try {
     //читаем параметры
-    $curPage = $_POST['page'];
-    $rowsPerPage = $_POST['rows'];
-    $date_begin = $POST['dt_beg'];
-    $date_end = $POST['dt_end'];
+    $curPage      = $_POST['page'];
+    $rowsPerPage  = $_POST['rows'];
     $sortingField = $_POST['sidx'];
     $sortingOrder = $_POST['sord'];
-    $printer_id	  = $_POST['printer_name'];
-    $marker_id    = $_POST['docmarker'];
-    $cups_user	  = $_POST['cupsuser'];
-    $pressman_id  = $_POST['executor_fio'];
-    $executor_id  = $_POST['pressman_fio'];
-    toLog($cups_user);
-    //$curPage=1;
-    //$rowsPerPage=10;
+
+    $printer_id	  = $_POST['printer_name'] !='all' ? $_POST['printer_name'] : $printer_id  = NULL;
+    $executor_id  = $_POST['executor_fio'] !='all' ? $_POST['executor_fio'] : $executor_id = NULL;
+    $pressman_id  = $_POST['pressman_fio'] !='all' ? $_POST['pressman_fio'] : $pressman_id = NULL;
+    $marker_id    = $_POST['docmarker'] !='all' ? $_POST['docmarker'] : $marker_id = NULL;
+    $cups_user	  = $_POST['cupsuser'] !='all' ? $_POST['cupsuser'] : $cups_user = NULL;
+    #$date	  = $_POST['datetime'] !='all' ? $_POST['datetime'] : $date = NULL;	
+    #$docname	  = $_POST['docname'] !='all' ? $_POST['docname'] : $docname = NULL;	
+
+    $mb_number    = $_POST['mb_number'];
+    $inv_number   = $_POST['inv_number'];
+    $punkt        = $_POST['punkt'];
+    $copynumber   = $_POST['copynumber'];
+    $pagecount    = $_POST['pagecount'];
+    $pagecopy     = $_POST['pagecopy'];  
+
+
+    //toLog($docname);
 
     //подключаемся к базе
     $dbh = new PDO('pgsql:host='.$dbHost.';dbname='.$dbName, $dbUser, $dbPass);
@@ -44,7 +52,8 @@ try {
 				JOIN executors ON reports.executor_id = executors.id
 				JOIN firstpages ON reports.firstpages_id = firstpages.id
 				JOIN pressman ON reports.pressman_id = pressman.id";
-    if (($printer_id) and $printers_id !="all"){
+    
+    if (IsSet($printer_id)){
 
         if (!$where_str){
 	   $where_str=' WHERE ';
@@ -53,7 +62,7 @@ try {
 	}
 	$where_str.= " printers.id = '$printer_id'";
     }
-    if ($marker_id){
+    if (IsSet($marker_id)){
 
         if (!$where_str){
 	   $where_str=' WHERE ';
@@ -62,7 +71,7 @@ try {
 	}
 	$where_str.= " document_level.id = '$marker_id'";
     }
-    if ($cups_user){
+    if (IsSet($cups_user)){
 
         if (!$where_str){
 	   $where_str=' WHERE ';
@@ -71,7 +80,7 @@ try {
 	}
 	$where_str.= " reports.cups_user = '$cups_user'";
     }
-    if ($executor_id){
+    if (IsSet($executor_id)){
 
         if (!$where_str){
 	   $where_str=' WHERE ';
@@ -80,7 +89,7 @@ try {
 	}
 	$where_str.= " reports.executor_id= '$executor_id'";
     }
-    if ($pressman_id){
+    if (IsSet($pressman_id)){
 
         if (!$where_str){
 	   $where_str=' WHERE ';
@@ -88,6 +97,78 @@ try {
            $where_str.=' AND ';
 	}
 	$where_str.= " reports.pressman_id = '$pressman_id'";
+    }
+    if (IsSet($date)){
+
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	#С 0 часов запрашиваемых суток
+	$where_str.= " reports.datetime >= cast ('$date' as timestamp with time zone)'";
+    }
+
+    if (IsSet($docname)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	#С 0 часов запрашиваемых суток
+	$where_str.= " reports.doc_name LIKE '%$docname%'";
+    }
+
+
+    if (IsSet($mb_number)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	$where_str.= " reports.mb_number= '$mb_number'";
+    }
+    if (IsSet($inv_number)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	$where_str.= " reports.inv_number= '$inv_number'";
+    }
+
+
+    if (IsSet($punkt)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	$where_str.= " reports.punkt= '$punkt'";
+    }
+    if (IsSet($copynumber)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	$where_str.= " reports.copynumber= '$copynumber'";
+    }
+    if (IsSet($page_count)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	$where_str.= " reports.page_count= '$pagecount'";
+    }
+    if (IsSet($page_copy)){
+        if (!$where_str){
+	   $where_str=' WHERE ';
+	}else{
+           $where_str.=' AND ';
+	}
+	$where_str.= " reports.page_copy= '$pagecopy'";
     }
 
     if ($where_str){
