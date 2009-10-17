65235eb0f9ff539b5af5d655a88a6a6be0e6e123
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-10-17 19:29:10 +0000

    Файл в котором хранятся константы общие для клиента и для демона

--- safeFatPrinter/trunk/src/tech_global.h
+++ safeFatPrinter/trunk/src/tech_global.h
@@ -3,15 +3,25 @@
 
 
 typedef enum{ 
-   GET_PRINTER_LIST_CMD = 100
+   GET_PRINTER_LIST_CMD = 100,
+   GET_MB_PRINTS_TODAY_CMD =200,
+   DISCONNECT		= 5000	
 
 }commands_t;
 
+typedef enum{ 
+   REGISTER_ANS	    = 1000,
+   PRINTER_LIST_ANS = 1100,
+   MB_LIST_ANS = 1200
+
+
+}answer_t;
+
 typedef enum {
-	    NO_ERROR ,
-	    HOSTNAME_EMPTY_ERR ,
-	    INVALID_PORT_RANGE_ERR ,
-	    HOST_NOT_FOUND_ERR ,
+	    NO_ERROR,
+	    HOSTNAME_EMPTY_ERR,
+	    INVALID_PORT_RANGE_ERR,
+	    HOST_NOT_FOUND_ERR,
 	    CONNECTION_REFUSED_ERR
 }error_t;
 
