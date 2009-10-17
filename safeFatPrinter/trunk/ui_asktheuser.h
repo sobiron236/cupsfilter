b1febbdb1a4c59bf8d3b9ebd7766ee572bca4d74
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-10-17 19:58:32 +0000

    Файл автогенерируемый утилитой uic

--- safeFatPrinter/trunk/ui_asktheuser.h
+++ safeFatPrinter/trunk/ui_asktheuser.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'asktheuser.ui'
 **
-** Created: Sat 17. Oct 20:53:19 2009
+** Created: Sat 17. Oct 23:37:39 2009
 **      by: Qt User Interface Compiler version 4.5.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
