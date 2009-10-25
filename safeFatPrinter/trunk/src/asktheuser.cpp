bdf74059c3bb0f3e6253004f919e9fd429544ef7
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-10-25 18:43:55 +0000

    Вся информация о карточке документа теперь храниться в модели, соответственно функции установки значения полей удалены, так поля отображения связанны с моделью через mapper

--- safeFatPrinter/trunk/src/asktheuser.cpp
+++ safeFatPrinter/trunk/src/asktheuser.cpp
@@ -7,58 +7,152 @@ m_ui(new Ui::askTheUser)
 {
     m_ui->setupUi(this);
     MBcompleter = new QCompleter(this);
-    connect (m_ui->docName_plainTextEdit,SIGNAL(textChanged()),this,SLOT(docNameChanged()));
-    connect (m_ui->secretCBox,SIGNAL(currentIndexChanged(QString)),this,SLOT(secretStampChanged(QString)));
-    connect (m_ui->copyNumberSpinBox,SIGNAL(valueChanged(int)),this,SLOT(copyNumberChanged(int)));
-    connect (m_ui->punktLineEd,SIGNAL(textChanged(QString)),this,SLOT(punktChanged(QString)));
-    connect (m_ui->reciverOne_lineEd,SIGNAL(textChanged(QString)),this,SLOT(reciverOneChanged(QString)));
-    connect (m_ui->reciverTwo_lineEd,SIGNAL(textChanged(QString)),this,SLOT(reciverTwoChanged(QString)));
-    connect (m_ui->reciverThree_lineEd,SIGNAL(textChanged(QString)),this,SLOT(reciverThreeChanged(QString)));
-    connect (m_ui->reciverFour_lineEd,SIGNAL(textChanged(QString)),this,SLOT(reciverFourChanged(QString)));
-    connect (m_ui->reciverFive_lineEd,SIGNAL(textChanged(QString)),this,SLOT(reciverFiveChanged(QString)));
-    connect (m_ui->executor_lineEd,SIGNAL(textChanged(QString)),this,SLOT(executorChanged(QString)));
-    connect (m_ui->pressman_lineEd,SIGNAL(textChanged(QString)),this,SLOT(pressmanChanged(QString)));
-    connect (m_ui->telephone_lineEd,SIGNAL(textChanged(QString)),this,SLOT(phoneChanged(QString)));
-    connect (m_ui->invNumber_lineEd,SIGNAL(textChanged(QString)),this,SLOT(invNumberChanged(QString)));
-    connect (m_ui->dateField_dateEd,SIGNAL(dateChanged(QDate)),this,SLOT(dateFiledChanged(QDate)));
-    connect (m_ui->mbNumberLineEd,SIGNAL(textChanged(QString)),this,SLOT(mbNumChanged(QString)));
-    connect (m_ui->templatesCbox,SIGNAL(currentIndexChanged(QString)),this,SLOT(templatesChanged(QString)));
+    mapper = new QDataWidgetMapper(this);
+    mapper->setSubmitPolicy(QDataWidgetMapper::AutoSubmit);
+    createConnections();
     m_ui->dateField_dateEd->setDate(QDate::currentDate());
+
 }
 
-void askTheUser::setMode(int work_mode)
+void askTheUser::enableInputFields(bool state)
+{
+  //Запретим все поля ввода кроме МБ
+  m_ui->docName_plainTextEdit->setEnabled(state);
+  m_ui->secretCBox->setEnabled(state);
+  m_ui->copyNumberLineEd->setEnabled(state);
+  m_ui->punktLineEd->setEnabled(state);
+  m_ui->reciverOne_lineEd->setEnabled(state);
+  m_ui->reciverTwo_lineEd->setEnabled(state);
+  m_ui->reciverThree_lineEd->setEnabled(state);
+  m_ui->reciverFour_lineEd->setEnabled(state);
+  m_ui->reciverFive_lineEd->setEnabled(state);
+  m_ui->executor_lineEd->setEnabled(state);
+  m_ui->pressman_lineEd->setEnabled(state);
+  m_ui->telephone_lineEd->setEnabled(state);
+  m_ui->invNumber_lineEd->setEnabled(state);
+  m_ui->dateField_dateEd->setEnabled(state);
+  m_ui->templatesCbox->setEnabled(state);
+}
+
+void askTheUser::selectWorkPage()
+{
+    m_ui->stackedWidget->setCurrentIndex(0);
+}
+void askTheUser::selectInfoPage()
+{
+      m_ui->stackedWidget->setCurrentIndex(1);
+}
+void askTheUser::checkMBdublicat()
+{
+    emit isMBExist(m_ui->mbNumberLineEd->text());
+    //Переключим на вторую страницу, покажем пользователя чудесное сообщение
+    m_ui->informerTextEd->setText(QObject::trUtf8("Проверяю наличие регистрации в БД аудита документа с номером МБ: \n%1 .").arg(m_ui->mbNumberLineEd->text()));
+    this->selectInfoPage();
+}
+
+void askTheUser::createConnections()
+{
+    connect (m_ui->mbNumberLineEd,SIGNAL(editingFinished()),this,SLOT(checkMBdublicat()));
+    //TODO проверить не надо ли будет по нажатию на эту кнопку отменять что либо еще
+    connect (m_ui->abortInformerButton,SIGNAL(clicked()),m_ui->mbNumberLineEd,SLOT(clear()));
+    connect (m_ui->abortInformerButton,SIGNAL(clicked()),this,SLOT(selectWorkPage()));
+
+}
+void askTheUser::setModel (QStandardItemModel * model)
+{
+  mainModel = model;
+  QStringList stamp_items;
+  stamp_items << QObject::trUtf8("Секретно") << QObject::trUtf8("Сов. секретно") << QObject::trUtf8("Особой Важности");
+  QStringListModel *stampModel = new QStringListModel(stamp_items, this);
+  m_ui->secretCBox->setModel(stampModel);
+
+  //Свяжем  элементы диалогового окна с моделью через mapper
+  mapper->setModel(model);
+  mapper->toLast();
+
+  for (int i = 0; i < model->columnCount(); i++)
+    {
+      QStandardItem * header_item = model->horizontalHeaderItem(i);
+      QString header = header_item->data(Qt::EditRole).toString();
+
+      if (header.compare(QObject::trUtf8("Номер док-та")) == 0)
+        {
+          mapper->addMapping(m_ui->mbNumberLineEd, i);
+        }
+      else if (header.compare(QObject::trUtf8("Название док-та")) == 0)
+        {
+          mapper->addMapping(m_ui->docName_plainTextEdit, i);
+        }
+      else if (header.compare(QObject::trUtf8("Гриф")) == 0)
+        {
+          mapper->addMapping(m_ui->secretCBox, i, "currentIndex");
+        }
+      else if (header.compare(QObject::trUtf8("Пункт перечня"))==0)
+        {
+          mapper->addMapping(m_ui->punktLineEd, i);
+        }
+      else if (header.compare(QObject::trUtf8("Номер копии"))==0)
+        {
+	  mapper->addMapping(m_ui->copyNumberLineEd, i);
+	}else if (header.compare(QObject::trUtf8("Кол-во листов"))==0)
+	{
+	  mapper->addMapping(m_ui->pageCountEd, i);
+	}
+      else if (header.compare(QObject::trUtf8("Штамп последней стр."))==0)
+        {
+	  mapper->addMapping(m_ui->lastPageGroupBoxChecked, i,"checked");
+	}
+      else if (header.compare(QObject::trUtf8("Исполнитель"))==0)
+       {
+	 mapper->addMapping(m_ui->executor_lineEd, i);
+       }else if (header.compare(QObject::trUtf8("Отпечатал"))==0)
+       {
+	 mapper->addMapping(m_ui->pressman_lineEd, i);
+       } else if (header.compare(QObject::trUtf8("Телефон"))==0)
+       {
+	 mapper->addMapping(m_ui->telephone_lineEd, i);
+       } else if (header.compare(QObject::trUtf8("Инв. №"))==0)
+       {
+	 mapper->addMapping(m_ui->invNumber_lineEd, i);
+       }  else if (header.compare(QObject::trUtf8("Дата распечатки"))==0)
+       {
+	 mapper->addMapping(m_ui->dateField_dateEd, i);
+       }
+      else if (header.compare(QObject::trUtf8("Список рассылки"))==0)
+       {
+	 mapper->addMapping(m_ui->reciversListGroupBoxChecked, i,"checked");
+       }else if (header.compare(QObject::trUtf8("Получатель №1"))==0)
+       {
+	 mapper->addMapping(m_ui->reciverOne_lineEd, i);
+       }else if (header.compare(QObject::trUtf8("Получатель №2"))==0)
+       {
+	 mapper->addMapping(m_ui->reciverTwo_lineEd, i);
+       }else if (header.compare(QObject::trUtf8("Получатель №3"))==0)
+       {
+	 mapper->addMapping(m_ui->reciverThree_lineEd, i);
+       }else if (header.compare(QObject::trUtf8("Получатель №4"))==0)
+       {
+	 mapper->addMapping(m_ui->reciverFour_lineEd, i);
+       }else if (header.compare(QObject::trUtf8("Получатель №5"))==0)
+       {
+	 mapper->addMapping(m_ui->reciverFive_lineEd, i);
+       }
+  }
+}
+void askTheUser::setViewMode(int work_mode)
 {
     currentMode=work_mode;
     if (currentMode==2){
 	m_ui->previewBtn->setText(QObject::trUtf8("Печать на учтенных листах"));
-	//Запретим все поля ввода кроме МБ
-	m_ui->docName_plainTextEdit->setEnabled(false);
-m_ui->secretCBox->setEnabled(false);
-m_ui->copyNumberSpinBox->setEnabled(false);
-m_ui->punktLineEd->setEnabled(false);
-m_ui->reciverOne_lineEd->setEnabled(false);
-m_ui->reciverTwo_lineEd->setEnabled(false);
-m_ui->reciverThree_lineEd->setEnabled(false);
-m_ui->reciverFour_lineEd->setEnabled(false);
-m_ui->reciverFive_lineEd->setEnabled(false);
-m_ui->executor_lineEd->setEnabled(false);
-m_ui->pressman_lineEd->setEnabled(false);
-m_ui->telephone_lineEd->setEnabled(false);
-m_ui->invNumber_lineEd->setEnabled(false);
-m_ui->dateField_dateEd->setEnabled(false);
-m_ui->templatesCbox->setEnabled(false);
-//m_ui->mbNumberLineEd
+	this->enableInputFields(false);
     }
 
 }
-void askTheUser::docNameChanged()
-{
-    docName=m_ui->docName_plainTextEdit->document()->toPlainText();
-}
+
 
 void askTheUser::accept()
 {
-
+/*
 if (currentMode==1){
     QString msg;
     if (docName.isEmpty()){
@@ -104,12 +198,12 @@ if (currentMode==1){
 	}
 
 
-	}else if (currentMode==2){
+}else if (currentMode==2){
 	    QDialog::accept();
 	    qDebug() << Q_FUNC_INFO;
 		    emit printWithTemplate(m_ui->mbNumberLineEd->text());//TODO должен быть шаблон
-		}
-
+  }
+*/
 }
 
 void askTheUser::setMBCompleter(QStringList &cmp)
@@ -117,12 +211,6 @@ void askTheUser::setMBCompleter(QStringList &cmp)
     MBcompleter->setModel(new QStringListModel(cmp));
     MBcompleter->setCaseSensitivity(Qt::CaseInsensitive);
     m_ui->mbNumberLineEd->setCompleter(MBcompleter);
-}
-void askTheUser::mbNumChanged(QString mb)
-{
-    //m_ui->previewBtn->setEnabled(true);
-    mbNumber =mb;
-    emit isMBExist(mb); // запрос на сервер на наличие документа в базе
 }
 
 void askTheUser::parserDocData(QString doc_data)
@@ -139,125 +227,17 @@ void askTheUser::parserDocData(QString doc_data)
 	    // connect
 	    m_ui->previewBtn->setEnabled(true);
 	    // Разбор полей и запись в нужные места
-	    save2field(doc_data);
+	    //save2field(doc_data);
 	} else if (msgBox.clickedButton() == abortButton) {
 	    m_ui->mbNumberLineEd->clear();
 	}
     }else if (currentMode==2){
 		m_ui->previewBtn->setEnabled(true);
 		// Разбор полей и запись в нужные места
-		save2field(doc_data);
+		//save2field(doc_data);
 	    }
 }
 
-void  askTheUser::save2field(QString &data)
-{
-     QStringList list;
-     list = data.split("~");
-     for (int i = 0; i < list.size(); ++i){
-	 QRegExp rx("^(.+)=(.+)$");
-	 if (rx.indexIn(list.at(i))!=-1) {
-	     QString key =rx.cap(1);
-	     QString value = rx.cap(2);
-	 // Говонокод но пока не знаю как сделать лучше
-	     if (key=="docName"){
-		 m_ui->docName_plainTextEdit->setPlainText(value);
-	     }else if (key=="secretStamp"){
-			m_ui->secretCBox->setCurrentIndex(m_ui->secretCBox->findText(value));
-		    }else if (key=="punkt"){
-				m_ui->punktLineEd->setText(value);
-			    }else if (key=="mbNumber"){
-					m_ui->mbNumberLineEd->setText(value);
-				    }else if (key=="templ"){
-						m_ui->templatesCbox->setCurrentIndex(m_ui->templatesCbox->findText(value));
-					    }else if (key=="invNumber"){
-						    m_ui->invNumber_lineEd->setText(value);
-						}else if (key=="reciver_1"){
-							    m_ui->reciversList_chBox->setChecked(true);
-							    m_ui->reciverOne_lineEd->setText(value);
-							}else if (key=="reciver_2"){
-								    m_ui->reciverTwo_lineEd->setText(value);
-								}else if (key=="reciver_3"){
-									    m_ui->reciverThree_lineEd->setText(value);
-									}else if (key=="reciver_4"){
-										    m_ui->reciverFour_lineEd->setText(value);
-										}else if (key=="reciver_5"){
-											    m_ui->reciverFive_lineEd->setText(value);
-											}else if (key=="executor"){
-												    m_ui->executor_lineEd->setText(value);
-												    //TODO заменить на нормальное имя бокса
-												    m_ui->groupBox_2->setEnabled(true);
-												    m_ui->clisheOnLastPageCheckBox->setChecked(true);
-												}else if (key=="pressman"){
-													    m_ui->pressman_lineEd->setText(value);
-													}else if (key=="phoneNumber"){
-														    m_ui->telephone_lineEd->setText(value);
-														}else if (key=="copyNumber"){
-															    m_ui->copyNumberSpinBox->setValue(value.toInt());
-															}
-													/*
-													else if (key=="date"){
-															    m_ui->dateField_dateEd->setDate(value);
-															}*/
-	 }
-     }
-}
-void askTheUser::executorChanged(QString fio)
-{
-    executor =fio;
-}
-void askTheUser::pressmanChanged(QString fio)
-{
-    pressman=fio;
-}
-void askTheUser::phoneChanged(QString phone)
-{
-    phoneNumber =phone;
-}
-void askTheUser::invNumberChanged(QString iNumber)
-{
-    invNumber=iNumber;
-}
-void askTheUser::dateFiledChanged(QDate DT)
-{
-    date =DT;
-}
-void askTheUser::reciverOneChanged(QString res)
-{
-    reciver_1=res;
-}
-void askTheUser::reciverTwoChanged(QString res)
-{
-    reciver_2=res;
-}
-void askTheUser::reciverThreeChanged(QString res)
-{
-    reciver_3=res;
-}
-void askTheUser::reciverFourChanged(QString res)
-{
-    reciver_4=res;
-}
-void askTheUser::reciverFiveChanged(QString res)
-{
-    reciver_5=res;
-}
-void askTheUser::copyNumberChanged(int cpNum)
-{
-    copyNumber=cpNum;
-}
-void askTheUser::templatesChanged(QString Templates)
-{
-    templ=Templates;
-}
-void askTheUser::secretStampChanged(QString stamp)
-{
-    secretStamp=stamp;
-}
-void askTheUser::punktChanged(QString punktStr)
-{
-    punkt =punktStr;
-}
 askTheUser::~askTheUser()
 {
     delete m_ui;
