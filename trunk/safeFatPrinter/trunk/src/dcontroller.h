f4b16c4f6aa7d65101995ab5fb485bb697a5cc62
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-10-25 18:54:27 +0000

    Для работы с моделью добавил перегруженную функцию: insertDocToModel() - вставляет в модель пустую карточку документа insertDocToModel(QStringList &item); - добавляет к модели карточку документа полученную в виде набора строк КЛЮЧ=ЗНАЧЕНИЕ Добавли функцию отправки данных на сервер void sendServerCmd(commands_t cmd,QString & body); и функцию разбора ответа от сервера void parseNetwork(QString &message);

--- trunk/safeFatPrinter/trunk/src/dcontroller.h
+++ trunk/safeFatPrinter/trunk/src/dcontroller.h
@@ -16,6 +16,7 @@
 #include <QGraphicsRectItem>
 
 #include "proc_thread.h"
+#include "netagent.h"
 
 #define POINT_TO_CM(cm) ((cm)/28.3465058)
 #define POINT_TO_MM(mm) ((mm)/2.83465058)     ////////  0.352777778
@@ -38,6 +39,8 @@ public:
     void write_settings();
     QStandardItemModel *document_model () const;
 
+    void insertDocToModel();
+    void insertDocToModel(QStringList &item);
 public slots:
     // Преобразует выходной файл в pdf
     void convert2PDF(QString &in_file);
@@ -53,24 +56,32 @@ signals:
     void inputFileConverted();  // Файл преобразован в pdf
     void inputFileMerged();    // Файл объединен
     void connect2Demon();
+    void sendServerCmd(commands_t cmd,QString & body);
     void exchangeData2MB(QString field_data); // Выдает форматированну строчку с данными полученными от демона в ответ на запрос документа по МБ
     void sayMeGood();
 private slots:
     void parseCnvThread(int Code,QString output);
     void parseMergeThread(int Code,QString output);
+    void parseNetwork(QString &message);
 private:
     QString userName;
     QString userMandat;
-    void connect2Server(QString &host,int port,QString &sid);
+    void connect2Server(QString &host,int port);
     void createDocModel();
-    void insertDocToModel(QStringList &item);
 protected:
     QString SID;
     QStandardItemModel *doc_model;
     QStringList header;
+
+    netAgent *netClient;
     QString serverHostName;
     int serverPort;
     bool conn_state;
+// задержки сети для тонкой настройки
+    int timeout_connect;
+    int timeout_read;
+    int timeout_write;
+
 
     QStringList args;
     QStringList currentDoc;
@@ -81,10 +92,6 @@ protected:
     QString spoolDIR;	
 
     QString curPrinter; // Текущий выбранный пользователем принтер
-    // задержки сети для тонкой настройки
-    int timeout_connect;
-    int timeout_read;
-    int timeout_write;
 
     QGraphicsScene *scene;
     QMap<QString, int> page_size;
