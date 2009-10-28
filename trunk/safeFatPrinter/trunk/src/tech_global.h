6101365dfc58cc571e5e04699ad78d0cd1fd2c55
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-10-28 09:51:13 +0000

    Согласованные с Мишей изменения кодов команд

--- trunk/safeFatPrinter/trunk/src/tech_global.h
+++ trunk/safeFatPrinter/trunk/src/tech_global.h
@@ -1,20 +1,35 @@
 #ifndef TECH_GLOBAL_H
 #define TECH_GLOBAL_H
 
+#define splitter =":;:";
 
 typedef enum{ 
-   GET_PRINTER_LIST_CMD = 100,
+   REGISTER_CMD		= 10,
+   AUTHOR_USER          = 100,
    GET_MB_PRINTS_TODAY_CMD =200,
+
+   RAW_SQL_CMD		= 202,
+   GET_SEC_LEVEL	= 300, //запрос к демону на получение списка уровней секретности
+
+   GET_PRINTER_LIST_CMD = 400,
+
+   IS_MB_EXIST	=500,
    DISCONNECT		= 5000	
 
 }commands_t;
 
 typedef enum{ 
-   REGISTER_ANS	    = 1000,
-   PRINTER_LIST_ANS = 1100,
-   MB_LIST_ANS = 1200
+   REGISTER_ANS	    = 1010,
+   PRINT_ALLOWED    = 1100, // печать разрешена
+   PRINT_DENIED     = 1101, // печать запрещена
+   PRINTER_NOT_FOUND   = 1102, //  принтер не найден
 
 
+   PRINTER_LIST_ANS = 1400,
+   MB_LIST_ANS = 1200,
+   IS_MB_EXIST_AND_BRAK_ANS =1205,
+   IS_MB_EXIST_AND_NOT_BRAK_ANS =1210,
+   IS_MB_NOT_EXIST_ANS=1300
 }answer_t;
 
 typedef enum {
