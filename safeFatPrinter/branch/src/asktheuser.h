5cd36805213ac793bedddaf85fa1e06733f3db26
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-10-31 18:54:45 +0000

    Добавил два свойств: проверка разрешен или нет enabledPapersAccountsOutSide( листы учтены в другом органе) проверка установлен или нет checkedPapersAccountsOutSide

--- safeFatPrinter/branch/src/asktheuser.h
+++ safeFatPrinter/branch/src/asktheuser.h
@@ -7,12 +7,18 @@ namespace Ui {
     class AskTheUser;
 }
 
-class AskTheUser : public QDialog {
+class AskTheUser : public QDialog
+{
     Q_OBJECT
+    Q_PROPERTY(bool enabledPapersAccountsOutSide READ isEnabledPAO WRITE setEnabledPAO)
+    Q_PROPERTY(bool checkedPapersAccountsOutSide READ isCheckedPAO)
 public:
     AskTheUser(QWidget *parent = 0);
     ~AskTheUser();
-
+    bool isEnabledPAO() const;
+    bool isCheckedPAO() const;
+    void setEnabledPAO(bool flag);
+    void setWorkMode(int mode);
 protected:
     void changeEvent(QEvent *e);
 
