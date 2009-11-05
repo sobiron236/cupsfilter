4c40ddd534296ece76ae6d73f4e1a473f9067558
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-05 04:27:13 +0000

    Работа с моделью и с сетевыми потоками

--- trunk/safeFatPrinter/branch/src/dcontroller.cpp
+++ trunk/safeFatPrinter/branch/src/dcontroller.cpp
@@ -7,12 +7,233 @@ dController::dController(QObject * parent)
     SID=uSID.toString().replace("{","").replace("}","");
 
     this->read_settings();
-     createModel();
+    this->createModel();
+    this->getUserNameMandat();
+    this->createPrinterList();
+    netAgent = new net_agent(this);
+    connect (netAgent,SIGNAL(serverSay(QString&)),this,SLOT(readServerResponce(QString&)));
+    connect(this,SIGNAL(sendServerCmd(commands_t,QString&)),netAgent,SLOT(send2Server(commands_t,QString&)));
 }
 
+//----------------------------------------  Public   functions --------------------------------------------------------------------
+bool   dController::isUser_Mandat() const
+{
+    if (!Mandat.isEmpty() && !UserName.isEmpty()){
+	return true;
+    }else {
+	return false;
+    }
+}
+
+// ---------------------------------------      Public SLOTS     -------------------------------------------------------------------
+void dController::connect2Demon()
+{
+   netAgent->do_connect(serverHostName,serverPort,SID);
+}
+ void dController::convert2PDF(QString &in_file)
+{
+
+    QStringList args;
+    ProcessT *proc = new ProcessT();
+    // Обработчик сообщений файлового потока
+    connect(proc,SIGNAL(commandOutput(int,QString)),this,SLOT(parseCnvThread(int,QString)));
+
+    args.clear();
+    args.append( "-q" );
+    args.append( "-dQUIET" );
+    args.append( "-dNOPAUSE" );
+    args.append( "-dPARANOIDSAFER" );
+    args.append( "-dBATCH" );
+    args.append("-r300");
+    args.append( "-sDEVICE=pdfwrite" );
+    mainPDF=mainPDF.arg(SID);
+    outPDF=outPDF.arg(SID);
+    qDebug() << mainPDF;
+    args.append(QString("-sOutputFile=%1").arg(mainPDF));
+    args.append("-c");
+    args.append(".setpdfwrite");
+    args.append("-f");
+    args.append(in_file);
+    proc->execute(gsBin, args,QProcess::MergedChannels);
+    qDebug() <<Q_FUNC_INFO << "convert input file to pdf";
+
+}
+void dController::mergeTwoPDF(QString &fg_file,QString &bg_file,QString &out_file)
+{
+
+    QStringList args;
+    // Файл объединен
+    ProcessT *proc = new ProcessT();
+    // Обработчик сообщений файлового потока
+    connect(proc,
+		    SIGNAL(commandOutput(int,QString)),
+		this,
+		    SLOT(parseMergeThread(int,QString))
+		    );
+    args.clear();
+    //pdftk in.pdf background back.pdf output out.pdf
+    //pdftk in.pdf stamp back.pdf output out.pdf
+    args.append(fg_file);
+    args.append("stamp");
+    args.append(bg_file);
+    args.append("output");
+    args.append(out_file);
+    proc->execute(pdftkBin, args,QProcess::MergedChannels);
+    qDebug() << Q_FUNC_INFO <<"pdftk " << fg_file <<" stamp " <<bg_file <<" output " <<out_file;
+
+}
+
+
+//----------------------------------------	  Private SLOTS   --------------------------------------------------------------------
+void dController::done_ftp_transfer()
+{
+     QString msg= QObject::trUtf8("Успешно полученны глобальные шаблоны.");
+     init_done(GET_FROM_FTP_STEP,msg);
+}
+
+void dController::parseCnvThread(int Code,QString output )
+{
+    QString msg;
+	if (Code != QProcess::NormalExit) {
+	    msg =QObject::trUtf8("Ошибка предпечатной подготовки.\n%1:\nКод %2").arg(output).arg(Code,0,10);
+	    emit error(Code,msg);
+	}else{
+	// Файл преобразован в pdf
+	    QString msg= QObject::trUtf8("Исходный файл успешно конвертирован в pdf");
+	    emit init_done(CONVERT_STEP,msg);
+	    qDebug() << QObject::tr("Успешно завершена конвертация исходного файла в pdf.");
+	}
+}
+void dController::parseMergeThread(int Code,QString output )
+{
+    QString msg;
+	if (Code != QProcess::NormalExit) {
+	    msg =QObject::trUtf8("Ошибка предпечатной подготовки.\n%1:\nКод %2").arg(output).arg(Code,0,10);
+	    emit error(Code,msg);
+	}else{
+	emit inputFileMerged();
+	    qDebug() << QObject::tr("Успешно завершено добавление шаблона.");
+	}
+}
+
+void dController::readServerResponce(QString &line)
+{
+    QRegExp rx("^/(\\d+);:;(.+)$");
+    QString msg;
+    QString cmd;
+    QString body;
+    if(rx.indexIn(line) != -1)
+    {
+	cmd =rx.cap(1);
+	body = rx.cap(2);
+	qDebug() <<Q_FUNC_INFO<< cmd<<body;
+	switch (cmd.toInt()){
+	    case STAMP_LIST_ANS:
+		this->stampModel->setStringList(QStringList() << body.split(";:;"));
+		break;
+	    case REGISTER_ANS: // Соединились с сервером безопастности
+		 msg= QObject::trUtf8("Успешно соединились с сервером безопастности");
+		emit init_done(CONN_STEP,msg);
+		this->getStampName(); // Получим с сервера название уровней безопастности
+		break;
+	    case MB_EXIST_AND_BRAK_ANS:
+		this->insertDocToModel(body);
+		emit mbNumberExist(doc_model->rowCount());
+		break;
+	    case MB_EXIST_AND_NOT_BRAK_ANS:
+		emit mbNumberExist(DOC_PRINTED);
+		break;
+	    case MB_NOT_EXIST_ANS:
+		emit mbNumberNotExist();
+		break;
+	}
+    }else{
+	// emit error
+    }
+}
+
+//----------------------------------------  Private functions --------------------------------------------------------------------
+void dController::read_settings()
+{ // Читаем файл настроек
+    QSettings settings(QSettings::IniFormat, QSettings::UserScope,"Technoserv","safe_printer");
+
+    settings.setIniCodec("UTF-8");
+    settings.beginGroup("SERVICE");
+    serverHostName = settings.value("server","127.0.0.1").toString();
+    serverPort = settings.value("port",4242).toInt();
+    timeout_connect =settings.value("timeout_connect",5000).toInt();
+    timeout_read=settings.value("timeout_read",15000).toInt();
+    timeout_write=settings.value("timeout_write",15000).toInt();
+
+    settings.endGroup();
+    settings.beginGroup("PERIOD");
+    end_date =settings.value("end_date",QDate::currentDate ()).toDate();
+    QDate begin(end_date.year(), 1,1);
+    begin_date =settings.value("begin_date",begin).toDate();
+    settings.endGroup();
+#if defined(Q_OS_UNIX)
+    settings.beginGroup("POSTSCRIPT");
+    gsBin = settings.value("gs_bin","/usr/local/bin/gs").toString();
+    settings.endGroup();
+    settings.beginGroup("PDF");
+    pdftkBin = settings.value("pdfTK","/usr/local/bin/pdftk.py").toString();
+    settings.endGroup();
+    settings.beginGroup("USED_DIR_FILE");
+    spoolDIR = settings.value("spool_dir","/var/log/spool/cups/tmp/").toString();
+    printer_list_pid=settings.value("printers_pid","/var/log/pid/get_printer.pid").toString();
+    settings.endGroup();
+    settings.beginGroup("TEMPLATES");
+    localTemplates=settings.value("local_templates","$HOME/vprinter/local_templates/").toString();
+    globalTemplates=settings.value("global_templates","$HOME/vprinter/global_templates/").toString();
+    ftpTemplatesDir=settings.value("ftp_templates_dir","ftp://127.0.0.1/pub/templates/").toString();
+    settings.endGroup();
+#elif defined(Q_OS_WIN)
+    settings.beginGroup("POSTSCRIPT");
+    gsBin = settings.value("gs_bin","C:\\Program Files\\gs\\gs8.70\\bin\\gswin32c.exe").toString();
+    settings.endGroup();
+    settings.beginGroup("PDF");
+    pdftkBin = settings.value("pdfTK","c:\\Tools\\pdftk.exe").toString();
+    settings.endGroup();
+    settings.beginGroup("USED_DIR_FILE");
+    spoolDIR = settings.value("spool_dir","c:\\spool\\").toString();
+    printer_list_pid=settings.value("printers_pid","c:\\spool\\get_printer.pid").toString();
+    settings.endGroup();
+    settings.beginGroup("TEMPLATES");
+    localTemplates=settings.value("local_templates","%APPDATA%/vprinter/local_templates/").toString();
+    globalTemplates=settings.value("global_templates","%APPDATA%/vprinter/global_templates/").toString();
+    ftpTemplatesDir=settings.value("ftp_templates_dir","ftp://127.0.0.1/pub/templates/").toString();
+    settings.endGroup();
+#endif
+    mainPDF.append(spoolDIR).append("%1.pdf");
+    outPDF.append(spoolDIR). append("%1_out.pdf");
+
+    qDebug() << Q_FUNC_INFO << settings.fileName() ;
+
+    qDebug() << "\nSERVICE"
+	    << "\nserverHostName=" <<serverHostName
+	    << "\nserverPort="<< serverPort
+	    << "\ntimeout_connect=" << timeout_connect
+	    << "\ntimeout_read=" <<timeout_read
+	    << "\ntimeout_write" <<timeout_write;
+    qDebug() << "\nPERIOD"
+	    <<"\nbegin_date=" << begin_date.toString("dd.MM.yyyy")
+	    <<"\nend_date" <<end_date.toString("dd.MM.yyyy");
+    qDebug()   <<"\nPOSTSCRIPT"
+	    "\ngsBin="<< gsBin;
+    qDebug()   <<"\nPDF"<< "pdftkBin="<< pdftkBin;
+    qDebug()<<"\nUSED_DIR_FILE"
+    << "\nspoolDIR="    << spoolDIR
+    << "\nprinter_list_pid=" << printer_list_pid;
+    qDebug() << "\nTEMPLATES"
+    << "\nlocalTemplates="<< localTemplates
+    << "\nglobalTemplates="<< globalTemplates
+    << "\nftpTemplatesDir="<< ftpTemplatesDir;
+
+}
 void dController::createModel()
 {
-    doc_model = new QStandardItemModel();
+    stampModel = new  QStringListModel(this);
+    doc_model = new QStandardItemModel(this);
 
      header << QObject::trUtf8("Номер док-та")
 	    << QObject::trUtf8("Название док-та")
@@ -36,17 +257,12 @@ void dController::createModel()
 	    << QObject::trUtf8("Брак страниц")
 	    << QObject::trUtf8("Брак документа");
     doc_model->setHorizontalHeaderLabels(header);
-    this->insertDocToModel();
-
+   // this->insertDocToModel();
 }
-
 void dController::insertDocToModel()
-{
-    // add empty row to model
+{ // add empty row to model
     doc_model->insertRow(doc_model->rowCount());
 }
-
-
 void dController::insertDocToModel(QString &item)
 {
      QStringList itemList= item.split("~");
@@ -67,34 +283,58 @@ void dController::insertDocToModel(QString &item)
      doc_model->appendRow (cells);
 }
 
-QStandardItemModel *dController::document_model () const
+void dController::checkMB(QString  mb)
 {
-    return doc_model;
+    // Сервер проверят существует ли в базе документ с таким МБ за год
+    // если есть то он возвращает строку содержащую все его данные
+    QDate dt_end;
+    dt_end=QDate::currentDate (); // Текущая дата
+    QDate dt_begin (dt_end.year (), 1,1); // Первое января
+
+    QString cmd_body=QString(QObject::trUtf8("%1;:;%2;:;%3")).arg(mb).arg(dt_begin.toString("dd.MM.yyyy"),dt_end.toString("dd.MM.yyyy"));
+    qDebug() <<Q_FUNC_INFO<<cmd_body;
+    emit sendServerCmd(IS_MB_EXIST_CMD,cmd_body);
 }
 
-void dController::connect2Demon()
+void dController::getStampName()
+{// Получим название уровней секретности
+    QString cmd_body;
+    cmd_body=QObject::trUtf8("%1;:;%2").arg(UserName,Mandat);
+    emit sendServerCmd(GET_SEC_LEVEL_CMD,cmd_body);
+}
+void dController::getUserNameMandat()
 {
-    qDebug() <<"Connect";
-    // Соединились с сервером безопастности
-   QString msg= QObject::trUtf8("Успешно соединились с сервером безопастности");
-   emit init_done(CONN_STEP,msg);
+    // Выполним функции из ldap_auth.dll
+    UserName=QObject::trUtf8("sva");
+    Mandat=QObject::trUtf8("SYSPROG");
+}
+void dController::createPrinterList()
+{
+    QList<QPrinterInfo> plist;
+    plist = QPrinterInfo::availablePrinters () ;
+    for (int i = 0; i < plist.size(); ++i) {
+	if (plist.at(i).printerName()!="Защищенный принтер"){
+	   this->printerList.append(plist.at(i).printerName());
+	}
+    }
 }
 
-void dController::convert2PDF(QString &in_file)
-{
-    qDebug() <<"Convert";
-     // Файл преобразован в pdf
-    QString msg= QObject::trUtf8("Исходный файл успешно конвертирован в pdf");
-    emit init_done(CONVERT_STEP,msg);
 
-}
-void dController::mergeTwoPDF(QString &fg_file,QString &bg_file,QString &out_file)
+void dController::getTemplatesFromServer(QString &saveTo)
 {
-    emit inputFileMerged();
-    // Файл объединен
-}
+    QDir dir;
+    if (dir.mkpath(saveTo))
+    {
+	QFileInfoList list = dir.entryInfoList();
+	//Очистил каталог назначения
+	for (int i = 0; i < list.size(); ++i) {
+	    QFileInfo fileInfo = list.at(i);
+	    dir.remove(fileInfo.fileName());
+	}
+	Spider spider;
+	spider.getDirectory(QUrl(this->ftpTemplatesDir),this->globalTemplates);
+	QObject::connect(&spider, SIGNAL(done()), this, SLOT(done_ftp_transfer()));
 
-void dController::read_settings()
-{
-    // Читаем файл настроек
+    }
+
 }
--- trunk/safeFatPrinter/branch/src/dcontroller.h
+++ trunk/safeFatPrinter/branch/src/dcontroller.h
@@ -2,11 +2,21 @@
 #define DCONTROLLER_H
 #include <QDebug>
 #include <QObject>
+#include <QTextStream>
+#include <QRegExp>
+#include <QFileInfo>
 #include <QStringList>
 #include <QUuid>
 #include <QStandardItemModel>
+#include <QStringListModel>
+#include <QSettings>
+#include <QDate>
+#include <QPrinterInfo>
 
 #include "tech_global.h"
+#include "proc_thread.h"
+#include "net_agent.h"
+#include "spyder.h"
 
 
 class dController : public QObject
@@ -15,7 +25,10 @@ class dController : public QObject
 
 public:
     dController(QObject * parent = 0);
-    QStandardItemModel *document_model () const;
+    QStandardItemModel *document_model () const {return doc_model;}
+    QStringListModel *stamp_model() const{ return stampModel;}
+    bool isUser_Mandat() const;
+    QStringList printers_list() const {return printerList;}
 
 public slots:
     void connect2Demon(); // Соединение с настройками по умолчанию
@@ -23,28 +36,71 @@ public slots:
     void convert2PDF(QString &in_file);
     // Объединяет два файла в один печатая шаблон поверх исходного файла
     void mergeTwoPDF(QString &fg_file,QString &bg_file,QString &out_file);
-
+    void checkMB(QString mb); // Проверка наличия МБ в базе
+private slots:
+    void readServerResponce(QString &line);
+    void done_ftp_transfer();
+    void parseCnvThread(int Code,QString output );
+    void parseMergeThread(int Code,QString output );
 signals:
     void error(int ErrorCode,QString ErrorString);
     // Процесс настройки состоит из 2 шагов
     // 1 - Установка соединения с сервером
     // 2 - Успешное преобразование входного файла в pdf
-    void init_done(int step,QString &message);
+    void init_done(steps_t step,QString &message);
     void inputFileMerged();    // Файл объединен
+    void sendServerCmd(commands_t cmd,QString & body);
+
+    // в базе аудита такого документа (с таким МБ) нет
+    void mbNumberNotExist();
+     // В базе аудита есть такой документ, RowID -1 документ помечен ка распечатаный
+    void mbNumberExist(int RowID);
+
 
 private:
     void read_settings();
-    void createModel();
-
-    void insertDocToModel();
-    void insertDocToModel(QString &item);
     //void write_settings();
-    QString host;
-    int port;
+    void getTemplatesFromServer(QString &saveTo);
+
+    QDate begin_date; // Период за которы запрашивается с сервера МБ при проверке
+    QDate end_date;
+
+    QString serverHostName;
+    int serverPort;
     QString SID;
 
+    void createModel();
+    void getStampName();
+    void insertDocToModel();
+    void insertDocToModel(QString &item);
+
     QStandardItemModel *doc_model;
+    QStringListModel *stampModel;
     QStringList header;
+
+    void getUserNameMandat();
+    QString Mandat;
+    QString UserName;   
+
+    // задержки сети для тонкой настройки
+    net_agent *netAgent;
+    int timeout_connect;
+    int timeout_read;
+    int timeout_write;
+
+    QString mainPDF;	 // Файл который получился в результате конвертирования исходного
+    QString gsBin;	// путь к исполняемому файлу ghostscript
+    QString pdftkBin;	// Путь к файлу pdfTK (питоновский скрип под линуксом)
+    QString outPDF;
+    QString spoolDIR;
+    QString localTemplates;
+    QString globalTemplates;
+    QString ftpTemplatesDir;
+
+    void createPrinterList(); // Формирует список доступных принтеров
+    QString curPrinter; // Текущий выбранный пользователем принтер
+    QStringList printerList; // Список доступных принтеров
+    QString printer_list_pid; // файл который создает программа формирования списка принтеров
 };
 
 #endif // DCONTROLLER_H
