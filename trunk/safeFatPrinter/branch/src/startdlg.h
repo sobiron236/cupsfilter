5ce92dbd8f1fce553ea6968e07a1758a9e0fd417
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-05 04:23:38 +0000

    Добавил функцию rabbitHole  в которой соединяю внешние сигналы и слоты

--- trunk/safeFatPrinter/branch/src/startdlg.h
+++ trunk/safeFatPrinter/branch/src/startdlg.h
@@ -5,9 +5,12 @@
 #include <QDateTime>
 #include <QSignalMapper>
 #include <QPrinterInfo>
+#include <QStandardItemModel>
 
-#include "dController.h"
-#include "askTheUser.h"
+
+
+#include "dcontroller.h"
+#include "asktheuser.h"
 #include "workreport.h"
 
 namespace Ui {
@@ -20,23 +23,28 @@ public:
     StartDlg(QWidget *parent = 0);
     ~StartDlg();
     void convertToPDF(QString &filename);
+    void setModel (QStandardItemModel  *model);
 private slots:
-    void enableGUI(int step,QString &message);
+    void enableGUI(steps_t step,QString &message);
     void fill_docCard4Print(int Mode);
+
 protected:
     void changeEvent(QEvent *e);
     void createConnection();
     void printToLog(QString & log_mes);
     void setPrinterList();
+    void rabbitHole(); // Крольчья нора в которой сходяттся все сигналы и слоты
 private:
     Ui::StartDlg  *ui;
     AskTheUser *askDlg;
     workReport  *wrkDlg;
     dController  *control;
 
+
     QSignalMapper *signalMapper;
     bool connectStep;
     bool convertStep;
+    bool getMandatUserStep;
 };
 
 #endif // STARTDLG_H
