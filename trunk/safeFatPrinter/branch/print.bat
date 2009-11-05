7dd9c1e829377f10d924207755a9133aec71dba8
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-05 04:07:05 +0000

    Добавил переменные окружения

--- trunk/safeFatPrinter/branch/print.bat
+++ trunk/safeFatPrinter/branch/print.bat
@@ -1,2 +1,7 @@
-debug\safeFatPrinter.exe D:\safeprinter\cups_test_page-a4.ps doc_name
+echo Setting up environment...
+set QTDIR=C:\DevTools\QT-4.5.2
+set MINGW=C:\DevTools\mingw
+set PATH=%QTDIR%\bin;%MINGW%\bin;C:\PROGRA~1\JAVA\JRE6\BIN\
+
+release\safeFatPrinter.exe D:\safeprinter\cups_test_page-a4.ps doc_name
 
