7074da078cf64fd09d273990bec1be9af5507397
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-05 04:24:52 +0000

    Добавил команды в список

--- safeFatPrinter/branch/src/tech_global.h
+++ safeFatPrinter/branch/src/tech_global.h
@@ -1,10 +1,12 @@
 #ifndef TECH_GLOBAL_H
 #define TECH_GLOBAL_H
 
-#define CONN_STEP 0
-#define CONVERT_STEP 1
 
-#define splitter =":;:";
+typedef enum{
+    CONN_STEP,
+    CONVERT_STEP,
+    GET_FROM_FTP_STEP
+}steps_t;
 
 typedef enum{
    REGISTER_CMD		= 10,
@@ -12,35 +14,41 @@ typedef enum{
    GET_MB_PRINTS_TODAY_CMD =200,
 
    RAW_SQL_CMD		= 202,
-   GET_SEC_LEVEL	= 300, //запрос к демону на получение списка уровней секретности
+   GET_SEC_LEVEL_CMD	= 300, //Р·Р°РїСЂРѕСЃ Рє РґРµРјРѕРЅСѓ РЅР° РїРѕР»СѓС‡РµРЅРёРµ СЃРїРёСЃРєР° СѓСЂРѕРІРЅРµР№ СЃРµРєСЂРµС‚РЅРѕСЃС‚Рё
 
    GET_PRINTER_LIST_CMD = 400,
+   IS_MB_EXIST_CMD	= 500,
 
-   IS_MB_EXIST	=500,
    DISCONNECT		= 5000
 
 }commands_t;
 
 typedef enum{
    REGISTER_ANS	    = 1010,
-   PRINT_ALLOWED    = 1100, // печать разрешена
-   PRINT_DENIED     = 1101, // печать запрещена
-   PRINTER_NOT_FOUND   = 1102, //  принтер не найден
+   PRINT_ALLOWED    = 1100, // РїРµС‡Р°С‚СЊ СЂР°Р·СЂРµС€РµРЅР°
+   PRINT_DENIED     = 1101, // РїРµС‡Р°С‚СЊ Р·Р°РїСЂРµС‰РµРЅР°
+   PRINTER_NOT_FOUND   = 1102, //  РїСЂРёРЅС‚РµСЂ РЅРµ РЅР°Р№РґРµРЅ
 
 
    PRINTER_LIST_ANS = 1400,
    MB_LIST_ANS = 1200,
-   IS_MB_EXIST_AND_BRAK_ANS =1205,
-   IS_MB_EXIST_AND_NOT_BRAK_ANS =1210,
-   IS_MB_NOT_EXIST_ANS=1300
+   MB_EXIST_AND_BRAK_ANS =1205,
+   MB_EXIST_AND_NOT_BRAK_ANS =1210,
+   MB_NOT_EXIST_ANS=1220,
+   STAMP_LIST_ANS	= 1300	// РЎРїРёСЃРѕРє РЅР°Р·РІР°РЅРёР№ СѓСЂРѕРІРЅРµР№ СЃРµРєСЂРµС‚РЅРѕСЃС‚Рё
 }answer_t;
+
+typedef enum{
+   DOC_PRINTED =-1
+}doc_state_t;
 
 typedef enum {
 	    NO_ERROR,
 	    HOSTNAME_EMPTY_ERR,
 	    INVALID_PORT_RANGE_ERR,
 	    HOST_NOT_FOUND_ERR,
-	    CONNECTION_REFUSED_ERR
+	    CONNECTION_REFUSED_ERR,
+	    FILE_NOT_FOUND 	
 }error_t;
 
 #endif // TECH_GLOBAL_H
