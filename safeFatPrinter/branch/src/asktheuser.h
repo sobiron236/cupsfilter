b935d8e5f1b2097f7f871cf8f8f1a310dadbd288
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-05 04:26:15 +0000

    signalMapper и функции его обрабатывающие

--- safeFatPrinter/branch/src/asktheuser.h
+++ safeFatPrinter/branch/src/asktheuser.h
@@ -1,7 +1,17 @@
 #ifndef ASKTHEUSER_H
 #define ASKTHEUSER_H
-
+#include <QDebug>
 #include <QtGui/QDialog>
+#include <QStandardItemModel>
+#include <QStringListModel>
+#include <QDataWidgetMapper>
+#include <QSignalMapper>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QTimer>
+#include <QDialogButtonBox>
+#include "tech_global.h"
+
 
 namespace Ui {
     class AskTheUser;
@@ -19,11 +29,32 @@ public:
     bool isCheckedPAO() const;
     void setEnabledPAO(bool flag);
     void setWorkMode(int mode);
+    void setModel (QStandardItemModel * model);
+    void setStampModel(QStringListModel *stamp_model);
+public slots:
+    void do_mbExist(int RowId); // Приходят сообщения если такой документ в базе есть, если RowId =-1 значит они не бракованные
+    void do_mbNumberNotExist();
+signals:
+    void needCheckMB(QString mb_field);
+    void mbNumberNotExist();
+private slots:
+    void changePAOLabel(bool flag);
+    void isFileldNotEmpty(int sender_id);
+    void checkMB();
 protected:
     void changeEvent(QEvent *e);
+    void createConnections();
+    void enableInputFields(bool state);
+    void showAndClear(QString &msg,bool advanced);
 
 private:
-    Ui::AskTheUser *ui;
+    Ui::AskTheUser *m_ui;
+    QLineEdit documentStatus;
+    bool emptyFieldsExist;
+    QDataWidgetMapper *mapper;
+    QSignalMapper *signalMapper;
+    
+    int currentRow; // Текущая строка в модеи данных
 };
 
 #endif // ASKTHEUSER_H
