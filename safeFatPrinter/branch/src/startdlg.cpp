d4912d82bfcee69bd67bb9436520e8f4442c5af3
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-05 04:23:49 +0000

    Добавил функцию rabbitHole  в которой соединяю внешние сигналы и слоты

--- safeFatPrinter/branch/src/startdlg.cpp
+++ safeFatPrinter/branch/src/startdlg.cpp
@@ -6,29 +6,31 @@ StartDlg::StartDlg(QWidget *parent) :
     ui(new Ui::StartDlg)
 {
     ui->setupUi(this);
+
     askDlg = new AskTheUser(this);
     wrkDlg = new workReport(this);
     connectStep=false;
     convertStep=false;
+
     control = new dController(this);
     signalMapper = new QSignalMapper(this);
     createConnection();
+    rabbitHole();
     control->connect2Demon();
+
+    QString msg;
+    if (control->isUser_Mandat()){
+	msg= QObject::trUtf8("Успешно получили имя пользователя и его мандат");
+	getMandatUserStep=true;
+
+    }else{
+	getMandatUserStep=false;
+	msg= QObject::trUtf8("Ошибка получения имени пользователя или его мандата");
+    }
+    this->printToLog(msg);
+
 }
 
-
-void StartDlg::createConnection()
-{
-    // маппинг сигналов
-     connect(ui->markPaperBtn, SIGNAL(clicked()), signalMapper, SLOT(map()));
-     connect(ui->printOnMarkPaperBtn, SIGNAL(clicked()), signalMapper, SLOT(map()));
-     connect(ui->printWithAutoMarkPaperBtn, SIGNAL(clicked()), signalMapper, SLOT(map()));
-     signalMapper->setMapping(ui->markPaperBtn,0 );
-     signalMapper->setMapping(ui->printOnMarkPaperBtn,1);
-     signalMapper->setMapping(ui->printWithAutoMarkPaperBtn,2 );
-     connect(signalMapper, SIGNAL(mapped(int)),  this, SLOT(fill_docCard4Print(int)));
-     connect(control, SIGNAL(init_done(int,QString &)), this, SLOT(enableGUI(int,QString &)));
-}
 
 void StartDlg::convertToPDF(QString &filename)
 {
@@ -38,28 +40,20 @@ void StartDlg::convertToPDF(QString &filename)
     this->printToLog(msg);
 }
 
-void StartDlg::printToLog(QString & log_mes)
-{
-    ui->logList->addItem(QString("%1: %2").arg(QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss")).arg(log_mes));
-    ui->logList->update();
-}
-
-void StartDlg::setPrinterList()
-{
-    QList<QPrinterInfo> plist;
-    plist = QPrinterInfo::availablePrinters () ;
-    for (int i = 0; i < plist.size(); ++i) {
-	if (plist.at(i).printerName()!="Защищенный принтер"){
-	    ui->printerCBox->addItem(plist.at(i).printerName());
-	}
-    }
-    ui->printerCBox->setCurrentIndex(-1);
-}
-
 //************************************      SLOTS     *********************************************
+
+
+
+
+// --------------------------------------------------------------- private slots --------------------------------------------------------------------------------
+
 
 void StartDlg::fill_docCard4Print(int Mode)
 {  // покажем диалог ввода или выбора данных
+    askDlg->setWorkMode(Mode);
+    askDlg->setModel(control->document_model());
+    askDlg->setStampModel(control->stamp_model());
+
     int ret = askDlg->exec();
     if (ret ==QDialog::Accepted){
 	switch (Mode){
@@ -84,30 +78,62 @@ void StartDlg::fill_docCard4Print(int Mode)
 	// clearModel
     }
 }
-void StartDlg::enableGUI(int step,QString &message)
-{
 
-    qDebug() << step << message;
+void StartDlg::enableGUI(steps_t step,QString &message)
+{
     if (step==CONN_STEP){
 	connectStep=true;
     }else  if (step==CONVERT_STEP){
 	convertStep=true;
     }
-    this->printToLog(message);
-    if (connectStep && convertStep){
+    if (connectStep && convertStep && getMandatUserStep){
+	ui->printerCBox->addItems(control->printers_list());
 	ui->printerCBox->setEnabled(true);
 	ui->printOnMarkPaperBtn->setEnabled(true);
 	ui->printWithAutoMarkPaperBtn->setEnabled(true);
 	ui->markPaperBtn->setEnabled(true);
     }
+     this->printToLog(message);
 }
-
-//************************************ END SLOTS *********************************************
-
-
 StartDlg::~StartDlg()
 {
     delete ui;
+}
+//************************************ END SLOTS *********************************************
+
+// --------------------------------------------------------------- protected function-------------------------------------------------------------------------
+void StartDlg::rabbitHole()
+{
+    // Сигналы от контроллера
+    // Машина состояний
+    connect (control, SIGNAL(init_done(steps_t,QString &)), this, SLOT(enableGUI(steps_t,QString &)));
+    connect (control,SIGNAL(mbNumberExist(int)),askDlg,SLOT(do_mbExist(int)));
+    connect (control,SIGNAL(mbNumberNotExist()),askDlg,SLOT(do_mbNumberNotExist()));
+    // Сигналы от вопросника
+    connect (askDlg,SIGNAL(needCheckMB(QString )),control,SLOT(checkMB(QString )));
+
+    // Сигналы от диалога_полследнего слова
+}
+
+void StartDlg::printToLog(QString & log_mes)
+{
+    ui->logList->addItem(QString("%1: %2").arg(QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss")).arg(log_mes));
+    ui->logList->update();
+}
+
+
+
+void StartDlg::createConnection()
+{
+    // маппинг сигналов
+     connect(ui->markPaperBtn, SIGNAL(clicked()), signalMapper, SLOT(map()));
+     connect(ui->printOnMarkPaperBtn, SIGNAL(clicked()), signalMapper, SLOT(map()));
+     connect(ui->printWithAutoMarkPaperBtn, SIGNAL(clicked()), signalMapper, SLOT(map()));
+     signalMapper->setMapping(ui->markPaperBtn,0 );
+     signalMapper->setMapping(ui->printOnMarkPaperBtn,1);
+     signalMapper->setMapping(ui->printWithAutoMarkPaperBtn,2 );
+     connect(signalMapper, SIGNAL(mapped(int)),  this, SLOT(fill_docCard4Print(int)));
+
 }
 
 void StartDlg::changeEvent(QEvent *e)
