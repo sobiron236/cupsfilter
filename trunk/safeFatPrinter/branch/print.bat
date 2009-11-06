a275db73f0313d0f02968966df4e6949b43069a6
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-06 19:38:32 +0000

    Автогенерируемый файл проекта

--- trunk/safeFatPrinter/branch/print.bat
+++ trunk/safeFatPrinter/branch/print.bat
@@ -3,5 +3,5 @@ set QTDIR=C:\DevTools\QT-4.5.2
 set MINGW=C:\DevTools\mingw
 set PATH=%QTDIR%\bin;%MINGW%\bin;C:\PROGRA~1\JAVA\JRE6\BIN\
 
-release\safeFatPrinter.exe D:\safeprinter\cups_test_page-a4.ps doc_name
+debug\safeFatPrinter.exe D:\safeprinter\cups_test_page-a4.ps doc_name
 
