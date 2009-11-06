b5ff1b6b67c6a35bf860f7d7d5550c617c07eb2d
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-06 20:52:52 +0000

    Добавил команды согласованные c клиентом

--- trunk/simpleDemon/trunk/tech_global.h
+++ trunk/simpleDemon/trunk/tech_global.h
@@ -2,17 +2,59 @@
 #define TECH_GLOBAL_H
 
 
-typedef enum{ 
-   GET_PRINTER_LIST_CMD = 100
+typedef enum{
+    CONN_STEP,
+    CONVERT_STEP,
+    GET_FROM_FTP_STEP
+}steps_t;
+
+typedef enum{
+   REGISTER_CMD		= 10,
+
+   AUTHOR_USER          = 100,
+   GET_MB_PRINTS_TODAY_CMD =200,
+
+   RAW_SQL_CMD		= 202,
+   USER_SAY_DOC_GOOD	= 203, 
+   USER_SAY_DOC_BAD	= 204,
+
+   GET_SEC_LEVEL_CMD	= 300, //запрос к демону на получение списка уровней секретности
+
+   GET_PRINTER_LIST_CMD = 400,
+   IS_MB_EXIST_CMD	= 500,
+
+   DISCONNECT		= 5000
 
 }commands_t;
 
+typedef enum{
+   REGISTER_ANS	    = 1010,
+   PRINT_ALLOWED    = 1100, // печать разрешена
+   PRINT_DENIED     = 1101, // печать запрещена
+   PRINTER_NOT_FOUND   = 1102, //  принтер не найден
+
+
+   PRINTER_LIST_ANS = 1400,
+   PRINTER_LIST_EMPTY=1401,
+   MB_LIST_ANS = 1200,
+   MB_EXIST_AND_BRAK_ANS =1205,
+   MB_EXIST_AND_NOT_BRAK_ANS =1210,
+   MB_NOT_EXIST_ANS=1220,
+   STAMP_LIST_ANS	= 1300	// Список названий уровней секретности
+   
+}answer_t;
+
+typedef enum{
+   DOC_PRINTED =-1
+}doc_state_t;
+
 typedef enum {
-	    NO_ERROR ,
-	    HOSTNAME_EMPTY_ERR ,
-	    INVALID_PORT_RANGE_ERR ,
-	    HOST_NOT_FOUND_ERR ,
-	    CONNECTION_REFUSED_ERR
+	    NO_ERROR,
+	    HOSTNAME_EMPTY_ERR,
+	    INVALID_PORT_RANGE_ERR,
+	    HOST_NOT_FOUND_ERR,
+	    CONNECTION_REFUSED_ERR,
+	    FILE_NOT_FOUND 	
 }error_t;
 
-#endif
+#endif // TECH_GLOBAL_H
