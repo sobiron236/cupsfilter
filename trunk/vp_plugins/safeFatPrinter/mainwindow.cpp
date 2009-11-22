5d7a0bcd1ecd1f9d9cb6b6f936fcc2131865bb15
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-22 09:01:54 +0000

    Добавил реализацию функций updateStatusBar() showError()

--- trunk/vp_plugins/safeFatPrinter/mainwindow.cpp
+++ trunk/vp_plugins/safeFatPrinter/mainwindow.cpp
@@ -6,21 +6,43 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    errorMessageDialog = new QErrorMessage(this);
 
-    if (!loadPlugin()) {
-        statusBar()->showMessage(tr("Could not load the plugin"));
-    }else {
-        statusBar()->showMessage(tr("Ready"));
-     }
+    SpiderInTheMiddle = new Mediator(this);
+    connect (SpiderInTheMiddle,SIGNAL(StateChanged(PluginState)),this,SLOT(updateStatusBar(PluginState)));
+
+    connect (SpiderInTheMiddle,SIGNAL(error(QString &)),errorMessageDialog,SLOT(showMessage(QString &)));
+    SpiderInTheMiddle->loadPlugin(qApp->applicationDirPath());
+
    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(getUserName()));
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+void MainWindow::showError(QString &info)
+{
 
+
+}
+
+void MainWindow::updateStatusBar(Mediator::PluginState state)
+{
+    QString msg=QObject::trUtf8("Плагин: %1 успешно загружен.");
+    switch (state){
+    case Mediator::netPluginLoaded:
+        msg.arg("Работы с сетью");
+        break;
+    case Mediator::authPluginLoaded:
+        msg.arg("Аутентификации пользователя");
+        break;
+    case Mediator::gsPluginLoaded:
+        msg.arg("Обработки ps и pdf документов");
+        break;
+    }
+    statusBar()->showMessage(msg);
+}
 void MainWindow::showInfo(QString info)
 {
     QMessageBox msgBox;
@@ -32,7 +54,7 @@ void MainWindow::showInfo(QString info)
 
 void MainWindow::getUserName()
 {
-    net_plugin_Interface->sendData(tr("/cmd;:;300;:;test"));
+    //net_plugin_Interface->sendData(tr("/cmd;:;300;:;test"));
 }
 
 void MainWindow::changeEvent(QEvent *e)
