8666bc3b6c292633cae1a3effba4e3f13683d9de
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-22 08:55:21 +0000

    Перечисления свернул с помощью макроса Q_ENUMS(PluginState)

--- vp_plugins/safeFatPrinter/mediator.h
+++ vp_plugins/safeFatPrinter/mediator.h
@@ -4,35 +4,39 @@
 #include <QObject>
 #include <QPluginLoader>
 #include <QDir>
+#include <QUuid>
 #include "inet_plugin.h"
+#include "igs_plugin.h"
 
-class Mediator : public QObject
+class Mediator: public QObject
 {
-Q_OBJECT
-    typedef enum{
-        netPluginLoaded,
-        authPluginLoaded,
-        gsPluginLoaded,
-        connected,
-        converted,
-        merged
-    }State;
+    Q_OBJECT
+    Q_ENUMS(PluginState)
 public:
-    explicit Mediator(QObject *parent = 0);
-    bool loadPlugin();
+    Mediator(QObject *parent = 0);
+    void loadPlugin(const QString &app_dir);
+    enum PluginState { netPluginLoaded, authPluginLoaded, gsPluginLoaded };
 
 signals:
-    void StateChanged(State state);
-public slots:
+    void StateChanged(PluginState state);
+    void error (const QString &msg);
 
+public slots:
+    void convert2pdf(QString &input_fn);
 private:
     Inet_plugin *net_plugin_Interface;
+    Igs_plugin *gs_plugin_Interface;
+
+    QString sid;
+
     /**
      * Read global settings from Application Dir
      */
     void readGlobal();
-
-
+    /**
+     *  This is spider soul :) connect any with any
+     */
+    void spiderSoul();
 };
 
 #endif // MEDIATOR_H
