8a4db3cb8502ae5d2178f73c37803708b8b9100a
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-22 08:57:03 +0000

    loadPlugin() настроил на загрузку плагинов по их типам

--- trunk/vp_plugins/safeFatPrinter/mediator.cpp
+++ trunk/vp_plugins/safeFatPrinter/mediator.cpp
@@ -3,14 +3,18 @@
 Mediator::Mediator(QObject *parent) :
     QObject(parent)
 {
+    this->spiderSoul();
+    QUuid uSID=QUuid::createUuid () ;  //generate SID
+    sid=uSID.toString().replace("{","").replace("}","");
+
 }
 
-bool Mediator::loadPlugin()
+void Mediator::loadPlugin(const QString &app_dir)
 {
-    QDir pluginsDir(qApp->applicationDirPath());
+    QDir pluginsDir(app_dir);
 #if defined(Q_OS_WIN)
     if (pluginsDir.dirName().toLower() == "debug" || pluginsDir.dirName().toLower() == "release")
-        pluginsDir.cdUp();
+    pluginsDir.cdUp();
 #elif defined(Q_OS_MAC)
     if (pluginsDir.dirName() == "MacOS") {
         pluginsDir.cdUp();
@@ -19,29 +23,52 @@ bool Mediator::loadPlugin()
     }
 #endif
     pluginsDir.cd("plugins");
-    foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
-        QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
-        QObject *plugin = pluginLoader.instance();
-        if (plugin) {
-            net_plugin_Interface = qobject_cast<Inet_plugin *>(plugin);
+    foreach (QString fileName, pluginsDir.entryList(QDir::Files))
+        {
+            QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
+            QObject *plugin = pluginLoader.instance();
+            if (plugin) {
+                connect(plugin, SIGNAL(error(QString &)), this, SIGNAL (error(QString &)));
 
-            if (net_plugin_Interface){
-                connect(plugin,SIGNAL(serverResponse(QString)),this,SLOT(showInfo(QString)));
-                connect(plugin,SIGNAL(error(QString)),this,SLOT(showInfo(QString)));
+                net_plugin_Interface = qobject_cast<Inet_plugin *> (plugin);
 
-                QString host=QObject::trUtf8("localhost");
-                net_plugin_Interface->init(host,4242);
-                return true;
+                if (net_plugin_Interface) {
+                    //connect(plugin, SIGNAL(serverResponse(QString)), this, SLOT(showInfo(QString)));
+                    QString host = QObject::trUtf8("localhost");
+                    if (net_plugin_Interface->init(host, 4242)){
+                       emit StateChanged(netPluginLoaded);
+                    }
+                }
+
+                gs_plugin_Interface = qobject_cast<Igs_plugin *> (plugin);
+                if (gs_plugin_Interface) {
+                    // remove after debug
+
+                    QString gs_bin="C:\\Program Files\\gs\\gs8.70\\bin\\gswin32c.exe";
+                    QString pdftk_bin="c:\\tools\\pdftk.exe";
+                    QString temp_folder="c:\\spool";
+
+                    if (gs_plugin_Interface->init(gs_bin, pdftk_bin,temp_folder,sid)){
+                       emit StateChanged(gsPluginLoaded);
+                    }
+                }
             }
         }
-    }
-
-    return false;
 }
+
 
 //************************************** private function ***************************************
 void Mediator::readGlobal()
 {
 
 }
+void Mediator::spiderSoul()
+{
+
+}
 //***********************************************************************************************
+//*************************************** public slots*******************************************
+ void Mediator::convert2pdf(QString &input_fn)
+ {
+
+ }
