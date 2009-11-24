51e3699db23c78a932e275377769c5fc9937a900
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-24 03:52:57 +0000

    PageCount()  rename to getPageCount() Добавил параметры (HostName,Ip) в функцию  addPdfMark

--- vp_plugins/interfaces/igs_plugin.h
+++ vp_plugins/interfaces/igs_plugin.h
@@ -10,15 +10,15 @@ class Igs_plugin{
 
 public:
     virtual ~Igs_plugin() {}
-    virtual bool init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &sid)=0;
+    virtual bool init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &gs_rcp_file,const QString &sid)=0;
     virtual QString getFirstPages()=0;
     virtual QString getOtherPages()=0;
     virtual void convertPs2Pdf(const QString &input_fn)=0;
-    virtual int pageCount(const QString &input_fn)=0;
+    virtual void getPageCount(const QString &input_fn)=0;
     virtual QPixmap convertPdf2Png(const QString &fn, int beginPage, int Width )=0;
     virtual void merge2Pdf(const QString &input_fn,const QString &background_fn,const QString &output_fn)=0;
     virtual void printPdf(const QString &print_fn,const QString &printer_name)=0;
-    virtual void addPdfMark(const QString &input_fn,const QStringList mark)=0;
+    virtual void addPdfMark(const QString &input_fn, const QString &user_name, const QString &host_name,quint16 host_ip)=0;
 };
 
 
