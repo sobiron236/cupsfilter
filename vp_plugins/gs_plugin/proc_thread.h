dd97fe7d38a3d5a241ad45390d5ed0b574656a09
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-24 03:54:48 +0000

    Отдельный вычислительный поток для запуска внешних приложений. // TODO требуется переписать так чтобы он не создавался каждый раз заново а единожды созданный засыпал после выполнения задачи.

---
+++ vp_plugins/gs_plugin/proc_thread.h
@@ -0,0 +1,70 @@
+#ifndef PROC_THREAD_H
+#define PROC_THREAD_H
+
+#include <QtDebug>
+#include <QObject>
+#include <QThread>
+#include <QString>
+#include <QStringList>
+#include <QProcess>
+
+
+/**
+* 
+* 
+* @short Class which can execute a command as a seperate thread.
+* 
+*/
+
+class ProcessT : public QThread {
+
+	Q_OBJECT
+
+	public:
+		/**
+		 * Constructor. Creats a new ProcessT object.
+		 * @param parent The QObject parent of this object.
+		 */
+		ProcessT( QObject *parent = 0 );
+		/**
+		 * Destructor.
+		 */
+		virtual ~ProcessT();
+		/**
+		 * Reimplementation of QThread::run().
+		 * Executes the command which was set using setCommand(). 
+		 * If no command was set, this function does nothing.
+		 */
+		void run();
+		/**
+		 * Set the given command to be executed when run() is called.
+		 * @param name The name of the program.
+		 * @param args The command line arguments.
+		 * @param mode The process channel modes of the command which will be executed.
+		 */
+		void setCommand( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode = QProcess::SeparateChannels );
+		/**
+		 * Execute the given command. Calls setCommand() first and then just starts the thread if it's not running.
+		 * @param name The name of the program.
+		 * @param args The command line arguments.
+		 * @param mode The process channel modes of the command which will be executed.
+		 */
+		void execute( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode = QProcess::SeparateChannels );
+
+	signals:
+		/**
+		 * Emitted when a command has finished running. 
+		 * @param output The output of the command which was executed.
+		 */
+		void commandOutput(int Code,QString output);
+
+	private:
+		QString m_Command;
+		QStringList m_Args;
+		QProcess::ProcessChannelMode m_ChanMode;
+		QString m_Output;
+
+
+};
+
+#endif //PROC_THREAD_H
