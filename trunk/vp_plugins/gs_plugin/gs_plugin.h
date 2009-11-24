1ef82834b7ff85f65975e4b5135668307c18500f
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-11-24 03:57:40 +0000

    Убрал QProcess Добавил приватные слоты - обработчики результатов внешних программ //TODO добавить для всех этих слотов сигнал маппер и объединить в одну функцию void parseCnvThread(int Code,QString output); void parsePageCountThread(int Code,QString output); void parseFirstPageThread(int Code,QString output); void parseOtherPageThread(int Code,QString output); void parseMergeThread(int Code,QString output); void parseAddPdfMarkThread(int Code,QString output);

--- trunk/vp_plugins/gs_plugin/gs_plugin.h
+++ trunk/vp_plugins/gs_plugin/gs_plugin.h
@@ -9,9 +9,12 @@
 #include <QPainter>
 #include <QFile>
 #include <QDir>
-#include <QProcess>
 #include <QRegExp>
+#include <QTemporaryFile>
+#include <QTextStream>
+#include <QDateTime>
 
+#include "proc_thread.h"
 #include "igs_plugin.h"
 
 
@@ -20,27 +23,38 @@ class gs_plugin :public QObject, Igs_plugin
     Q_OBJECT
     Q_INTERFACES(Igs_plugin)
     Q_ENUMS(TaskState)
-
+   // Q_ENUMS(ErrorCode)
 public:
-    enum TaskState {converted,merged,splitted,printed };
-
+    enum TaskState {converted,merged,splitted_first,splitted_other,printed,previewedPage,pdfMarkAdded };
+    //enum ErrorCode {forkProccessDie};
     gs_plugin(QObject *parent=0){}
-    bool init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &sid);
+    bool init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &gs_rcp_file,const QString &sid);
     QString getFirstPages(){return firstPage_fn;};
     QString getOtherPages(){return otherPages_fn;};
     void convertPs2Pdf(const QString &input_fn);
-    int pageCount(const QString &input_fn);
+    void getPageCount(const QString &input_fn);
     QPixmap convertPdf2Png(const QString &fn, int beginPage,int Width);
     void merge2Pdf(const QString &input_fn,const QString &background_fn,const QString &output_fn);
     void printPdf(const QString &print_fn,const QString &printer_name);
-    void addPdfMark(const QString &input_fn,const QStringList mark);
+    void addPdfMark(const QString &input_fn,const QString &user_name, const QString &host_name,quint16 host_ip);
 
 signals:
     void error(const QString &error_message);
+    // Те ошибки которые не должны выходить за пределы моего модуля
+    //void local_error(const QString &error_message);
     void StateChanged(TaskState state);
 
+private slots:
+    //TODO добавить для всех этих слотов сигнал маппер и объединить в одну функцию
+    //TODO порождаемый поток держать в спящем состоянии и пробуждать при приходе новой команды
+    // а не создовать каждый раз заново.
+    void parseCnvThread(int Code,QString output);
+    void parsePageCountThread(int Code,QString output);
+    void parseFirstPageThread(int Code,QString output);
+    void parseOtherPageThread(int Code,QString output);
+    void parseMergeThread(int Code,QString output);
+    void parseAddPdfMarkThread(int Code,QString output);
 private:
-
     QString gsBin;
     QString tempPath;
     QString pdftkBin;
@@ -49,13 +63,12 @@ private:
     QString firstPage_fn;
     QString otherPages_fn;
     QString mainPDF;
+    QString gs_rcp;
 
     QPixmap pageSnapShot;
     int pagesCount; // Число страниц в конвертируемом документе
-
+protected:
     void splitPdf(const QString &source_fn,const QString &firts_page_fn,const QString &other_page_fn);
-    QString startProc(const QString &bin,const QStringList &proc_args);
-
 };
 
 
