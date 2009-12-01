9db6c8a5af37a77f590d8186e66ec61468726416
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-01 08:35:21 +0000

    Заменил QSplashScreen

--- vp_plugins/safeFatPrinter/main.cpp
+++ vp_plugins/safeFatPrinter/main.cpp
@@ -1,19 +1,20 @@
 #include <QtGui/QApplication>
 #include <QDebug>
 #include <QTextCodec>
-#include <QErrorMessage>
+#include <QSplashScreen>
+#include <QTimer>
 
-#include "mediator.h"
-#include "mainwindow.h"
-#include "firstask.h"
-#include "getusernamemandatdlg.h"
+//#include "mediator.h"
+//#include "mainwindow.h"
+#include "selectwindow.h"
+
 #include "config.h"
 #include "tech_global.h"
-
 
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(images);
+
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     QTextCodec::setCodecForTr(codec);
     //QTextCodec::setCodecForTr( QTextCodec::codecForName( "cp866" ) );
@@ -22,69 +23,34 @@ int main(int argc, char *argv[])
     qInstallMsgHandler(myMessageOutput);
 
     QApplication app(argc, argv);
-
-
     QStringList aList=app.arguments();
-    if (aList.size()< 1){
-            QMessageBox::critical(0,QObject::trUtf8("Обратитесь к системному администратору"),
-                  QObject::trUtf8("Ошибка запуска приложения, не достаточно параметров запуска"));
-            app.exit(1);
+    if (aList.size()< 2){
+        QMessageBox::critical(0,QObject::trUtf8("Обратитесь к системному администратору"),
+                              QObject::trUtf8("Ошибка запуска приложения, не достаточно параметров запуска"));
+       return 1;
     }else {
-            QString in_file =aList.at(1);
-            QPixmap pixmap(":images/splash.png");
+        SelectWindow window;
+        QString in_file=aList.at(1);
+        window.loadPlugin(qApp->applicationDirPath());
+        window.setFileToWork(in_file);
+        app.processEvents();
+        window.show();
+        return app.exec();
 
-            Mediator *SpiderInTheMiddle = new Mediator();
-            getUserNameMandatDlg *UMDlg = new getUserNameMandatDlg();
-            firstAsk *askDlg = new firstAsk();
+        /*
+        QPixmap pixmap(":images/splash.png");
+        QSplashScreen *splash  = new QSplashScreen(pixmap);
+        QObject::connect (&window,SIGNAL(pluginLoad(const QString &,int, const QColor &)),splash,SLOT(showMessage(const QString &,int, const QColor &)));
+        QObject::connect (&window,SIGNAL(closeSplash()),splash,SLOT(close()));
 
-            QSplashScreen *splash  = new QSplashScreen(pixmap);
-            QErrorMessage *errorMessage = new QErrorMessage();
+        splash->show();
+        splash->showMessage(QObject::trUtf8("Загрузка плагинов"),Qt::AlignLeft| Qt::AlignBottom, QColor::fromRgb(170,255,0));
+        QTimer::singleShot(1000, splash, SLOT(clearMessage()));
 
-            QObject::connect (SpiderInTheMiddle,SIGNAL(error (QString )),errorMessage,SLOT(showMessage(QString)));
-            QObject::connect (SpiderInTheMiddle,SIGNAL(needShowAuthWindow(QString &)),UMDlg,SLOT(setUserName(QString &)));
-            QObject::connect (SpiderInTheMiddle,SIGNAL(pluginLoad(QString &,int, QColor &)),splash,SLOT(showMessage(QString &,int, QColor &)));
-
-            splash->show();
-            splash->showMessage(QObject::trUtf8("Загрузка плагинов"),Qt::AlignLeft| Qt::AlignBottom, QColor::fromRgb(170,255,0));
-            QTimer::singleShot(1000, splash, SLOT(clearMessage()));
-            SpiderInTheMiddle->loadPlugin(qApp->applicationDirPath());
-
-            app.processEvents();
-            splash->showMessage(QObject::trUtf8("Инициализация"),Qt::AlignLeft| Qt::AlignBottom, QColor::fromRgb(170,255,0));
-            QTimer::singleShot(1000, splash, SLOT(clearMessage()));
-            SpiderInTheMiddle->plugin_init();
-
-            app.processEvents();
-            QTimer::singleShot(1000, splash, SLOT(close()));
-
-            // Определяемся с режимом работы
-            if (SpiderInTheMiddle->isValid()){
-                MainWindow window;
-                window.setSpider(SpiderInTheMiddle);
-
-                // Связь с могучим демоном установленна
-                if (!SpiderInTheMiddle->isAuth()){
-                   // Требуется показать Диалоговое окошко запроса USER|Mandat
-                    // К этому моменту я уже получил от демона список всех мандатов
-                    if (SpiderInTheMiddle->isHaveMandatList()){
-                        UMDlg->setMandatModel(SpiderInTheMiddle->mandat_model());
-                        if (UMDlg->exec() == QDialog::Accepted){
-                            SpiderInTheMiddle->setUserMandat(UMDlg->getCurrentMandat());
-                        }
-                    }
-                }else{
-                    // Пользоваталь авторизовался через агент безопастности
-                    // или существует удачно распрасенный файл session_ticket
-                    // Покажем чудесатое окошко с выбором режима работы
-                    if (askDlg->exec() == QDialog::Accepted){
-                        SpiderInTheMiddle->setWorkMode(askDlg->getCurrentMode());
-                        window.show();
-                    }
-                }
-            }else{
-                // Была ошибка загрузки плагинов или начальной инициализации
-                QTimer::singleShot(1000, qApp, SLOT(quit()));
-            }
-            return app.exec();
+        //QTimer::singleShot(5000, &window, SLOT(checkPluginReady()));
+        splash->finish(&window);//.hide();
+        app.processEvents();
+        */
     }
+
 }
