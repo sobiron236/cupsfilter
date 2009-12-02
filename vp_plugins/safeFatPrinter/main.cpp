6c14a9c56e047afe10f7986686d16aafdfbdec49
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-02 07:49:24 +0000

    Окончательно отказался от QSplashScreen тормознутый он и не удобный, так как не имеет собственного цикла событий eventloop

--- vp_plugins/safeFatPrinter/main.cpp
+++ vp_plugins/safeFatPrinter/main.cpp
@@ -29,28 +29,14 @@ int main(int argc, char *argv[])
                               QObject::trUtf8("Ошибка запуска приложения, не достаточно параметров запуска"));
        return 1;
     }else {
-        SelectWindow window;
         QString in_file=aList.at(1);
-        window.loadPlugin(qApp->applicationDirPath());
+        SelectWindow window;
         window.setFileToWork(in_file);
+
+        window.loadPlugin(qApp->applicationDirPath());
         app.processEvents();
         window.show();
         return app.exec();
-
-        /*
-        QPixmap pixmap(":images/splash.png");
-        QSplashScreen *splash  = new QSplashScreen(pixmap);
-        QObject::connect (&window,SIGNAL(pluginLoad(const QString &,int, const QColor &)),splash,SLOT(showMessage(const QString &,int, const QColor &)));
-        QObject::connect (&window,SIGNAL(closeSplash()),splash,SLOT(close()));
-
-        splash->show();
-        splash->showMessage(QObject::trUtf8("Загрузка плагинов"),Qt::AlignLeft| Qt::AlignBottom, QColor::fromRgb(170,255,0));
-        QTimer::singleShot(1000, splash, SLOT(clearMessage()));
-
-        //QTimer::singleShot(5000, &window, SLOT(checkPluginReady()));
-        splash->finish(&window);//.hide();
-        app.processEvents();
-        */
     }
 
 }
