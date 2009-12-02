c83c5d542795a78eb71734fdbcd553ff7d45f987
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-02 08:07:27 +0000

    private slots: исключил void setMode (int signal_mode); добавил void enableGUI(); void showPluginMessage(const QString &message)

--- vp_plugins/safeFatPrinter/selectwindow.h
+++ vp_plugins/safeFatPrinter/selectwindow.h
@@ -4,10 +4,13 @@
 #include <QMainWindow>
 #include <QMessageBox>
 #include <QDesktopWidget>
+#include <QSignalMapper>
+
 
 #include "mediator.h"
 
 #include "getusernamemandatdlg.h"
+#include "workfield.h"
 #include "tech_global.h"
 
 namespace Ui {
@@ -21,10 +24,14 @@ public:
     ~SelectWindow();
     void setFileToWork(QString &in_file);
     void loadPlugin(const QString &app_dir);
+    //int getCurrentMode(){return work_mode;};
 
 signals:
     void pluginMessage(const QString &message);
 
+private slots:
+    void setMode (int signal_mode);
+    void enableGUI();
 protected:
     void changeEvent(QEvent *e);
     QPoint SelectWindow::calcCenter();
@@ -32,14 +39,17 @@ protected:
 private slots:
     void showCritError(QString e_msg); // окажем окно с сообщением об ошибке и закроем приложение
     void showAuthWindow(QString &user_name);
-
+    void showPluginMessage(const QString &message);
 private:
     Ui::SelectWindow *ui;
     QString mainFileName; // Основной рабочий файл
 
     Mediator *SpiderInTheMiddle; // Центральный элемент который знает все о всех :)
     getUserNameMandatDlg *UMDlg;
+    workField *WorkDlg;
 
+    QSignalMapper *signalMapper;
+    int work_mode;
 };
 
 #endif // SELECTWINDOW_H
