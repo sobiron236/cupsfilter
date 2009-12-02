410f4607f3c6221961d900e38f2992b1f0826d3e
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-02 08:05:51 +0000

    Добавил QSignalMapper для выбора режима работы при нажатии на одну из 3 кнопок Отладил окно выбора пользователем мандата

--- vp_plugins/safeFatPrinter/selectwindow.cpp
+++ vp_plugins/safeFatPrinter/selectwindow.cpp
@@ -6,22 +6,67 @@ SelectWindow::SelectWindow(QWidget *parent) :
         ui(new Ui::SelectWindow)
 {
     ui->setupUi(this);
+    ui->printerCBox->setEnabled(false);
+    ui->printFromAccountPaper->setEnabled(false);
+    ui->printModeAccounting->setEnabled(false);
+    ui->printWithAccountingPaper->setEnabled(false);
 
+    QString msg=QObject::trUtf8("Выбери принтер и режим работы");
+    this->setWindowTitle(msg);
 
     SpiderInTheMiddle = new Mediator(this);
     UMDlg = new getUserNameMandatDlg(this);
+    signalMapper = new QSignalMapper(this);
+    WorkDlg = new workField(this);
+    WorkDlg->setWindowFlags(Qt::Dialog |  Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowSystemMenuHint);
 
 
     this->setWindowFlags(Qt::Dialog |  Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowSystemMenuHint);
-
+    this->move(calcCenter());
     connect (SpiderInTheMiddle,SIGNAL(error (QString )),this,SLOT(showCritError(QString)));
-    connect (SpiderInTheMiddle,SIGNAL(pluginMessage(QString &)),this,SIGNAL(pluginMessage(QString &)));
-
+    connect (SpiderInTheMiddle,SIGNAL(pluginMessage(const QString &)),this,SLOT (showPluginMessage(const QString &)));
     connect (SpiderInTheMiddle,SIGNAL(needShowAuthWindow(QString &)),this,SLOT(showAuthWindow(QString&)));
-    //connect (SpiderInTheMiddle,SIGNAL(needShowSelectWindow()),this,SLOT(showSelectWindow()));
+    connect (SpiderInTheMiddle,SIGNAL(needShowSelectWindow()),this,SLOT(enableGUI()));
     //connect (SpiderInTheMiddle,SIGNAL(pluginLoad(const QString &,int, const QColor &)),this,SIGNAL(pluginLoad(const QString &,int, const QColor &)));
     //connect (qApp,SIGNAL(aboutToQuit()),this,SLOT(cleanUp()));
     connect (ui->printerCBox,SIGNAL(currentIndexChanged(QString)),SpiderInTheMiddle,SLOT(authToPrinter(QString)));
+
+    connect(ui->printModeAccounting, SIGNAL(clicked()), signalMapper, SLOT(map()));
+    connect(ui->printFromAccountPaper, SIGNAL(clicked()), signalMapper, SLOT(map()));
+    connect(ui->printWithAccountingPaper, SIGNAL(clicked()), signalMapper, SLOT(map()));
+    signalMapper->setMapping(ui->printModeAccounting,0 );
+    signalMapper->setMapping(ui->printFromAccountPaper,1);
+    signalMapper->setMapping(ui->printWithAccountingPaper,2 );
+    connect(signalMapper, SIGNAL(mapped(int)),  this, SLOT(setMode(int)));
+
+}
+
+void SelectWindow::enableGUI()
+{
+    ui->printerCBox->setModel(SpiderInTheMiddle->printers_model());
+    ui->printerCBox->setEnabled(true);
+    ui->printFromAccountPaper->setEnabled(true);
+    ui->printModeAccounting->setEnabled(true);
+    ui->printWithAccountingPaper->setEnabled(true);
+}
+
+void SelectWindow::setMode (int signal_mode)
+{
+    work_mode = signal_mode;
+    QString title;
+    switch (signal_mode){
+    case 0:
+        title = QObject::trUtf8("Режим работы: [Учет листов]");
+        WorkDlg->setWindowTitle(title);
+        WorkDlg->move(calcCenter());
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    }
+    int ret = WorkDlg->exec();
+
 }
 
 void SelectWindow::showCritError(QString e_msg)
@@ -38,6 +83,7 @@ void SelectWindow::showCritError(QString e_msg)
 
 void SelectWindow::showAuthWindow(QString &user_name)
 {
+    this->hide();
     QString msg;
     UMDlg->setUserName(user_name);
     UMDlg->setMandatModel(SpiderInTheMiddle->mandat_model());
@@ -45,15 +91,20 @@ void SelectWindow::showAuthWindow(QString &user_name)
     int ret = UMDlg->exec();
     if (ret == QDialog::Accepted){
         SpiderInTheMiddle->setUserMandat(UMDlg->getCurrentMandat());
-
         msg= QObject::trUtf8("Авторизация с консоли");
         this->statusBar()->showMessage(msg);
     }else{
         close();
         //msg= QObject::trUtf8("Ошибка получения имени пользователя или его мандата");
     }
-
+    this->show();
 }
+
+void SelectWindow::showPluginMessage(const QString &message)
+{
+    statusBar()->showMessage(message);
+}
+
 SelectWindow::~SelectWindow()
 {
     delete ui;
