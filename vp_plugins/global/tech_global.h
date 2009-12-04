8b801cc7f86249f189491fff9fc1199c869c7606
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-04 04:06:09 +0000

    Блок данных для плагина работы с шаблонами

--- vp_plugins/global/tech_global.h
+++ vp_plugins/global/tech_global.h
@@ -3,7 +3,19 @@
 
 
 namespace SafeVirtualPrinter{
+
+#define POINT_TO_CM(cm) ((cm)/28.3465058)
+#define POINT_TO_MM(mm) ((mm)/2.83465058)     ////////  0.352777778
+#define POINT_TO_DM(dm) ((dm)/283.465058)
+#define POINT_TO_INCH(inch) ((inch)/72.0)
+
 #define MM_TO_POINT(mm) ((mm)*2.83465058)
+#define CM_TO_POINT(cm) ((cm)*28.3465058)     ///// 28.346456693
+#define DM_TO_POINT(dm) ((dm)*283.465058)
+#define INCH_TO_POINT(inch) ((inch)*72.0)
+
+static const int ObjectName = 0;
+
 typedef enum{
    REGISTER_CMD		= 10,
 
@@ -65,5 +77,7 @@ typedef enum WorkMode{
     PrintOverAccountPaper,
     PrintWithAccounting
 };
+
+
 }
 #endif // TECH_GLOBAL_H
