a1d15bb25fd0cc049014a6367e32cc32e6ba2988
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-04 01:51:13 +0000

    Плагин реализует 4 сцены по числу страниц шаблона при инициализации ему передается ссылка на модель // TODO необходимо реагировать на сигнал модели данные изменились и перестраивать данные в сценах

--- vp_plugins/tmpl_plugin/tmpl_plugin.h
+++ vp_plugins/tmpl_plugin/tmpl_plugin.h
@@ -10,6 +10,9 @@
 #include <QDir>
 #include <QTemporaryFile>
 #include <QDataStream>
+#include <QPrinter>
+#include <QPainter>
+#include <QDate>
 
 
 #include "itmpl_plugin.h"
@@ -23,22 +26,52 @@ class Tmpl_plugin :public QObject, Itmpl_plugin
     Q_INTERFACES(Itmpl_plugin)
 public:
 
-    Tmpl_plugin(QObject *parent=0);
-    void init(const QString &spool,const QString &sid,const QStandardItemModel *model);
+    Tmpl_plugin(QObject *parent=0){};
+    void init(const QString & spool,const QString & sid,const QStandardItemModel * model);
     // Возвращает полный путь к сформированному на основании шаблона и данных модели файлу
-    QString getFormatingPageFile(const QString &templates_in_file,int pageNum);
+    void setTemplates(const QString & templates_in_file);
+    void printFormatingPageToFile(int pageNum);
+    QGraphicsScene *getFirstPage(){return firstPage_tmpl;};
+    QGraphicsScene *getSecondPage(){return secondPage_tmpl;};
+    QGraphicsScene *getThirdPage(){return thirdPage_tmpl;};
+    QGraphicsScene *getFourthPage(){return fourthPage_tmpl;};
 
 signals:
     void error(QString error_message);
-protected:
-     QStandardItemModel *work_model;
-     QString spool_dir;
-     QString current_sid;
 
-     QString firstPage_tmpl_fn;
-     QString secondPage_tmpl_fn;
-     QString thirdPage_tmpl_fn;
-     QString fourthPage_tmpl_fn;
+protected:
+    bool parse_templates(int page,QGraphicsScene * scene);
+    static const int version = 1;
+
+    QStandardItemModel *work_model;
+
+    QString spool_dir;
+    QString current_sid;
+    QString currentTemplates;
+
+    QString firstPage_tmpl_fn;
+    QString secondPage_tmpl_fn;
+    QString thirdPage_tmpl_fn;
+    QString fourthPage_tmpl_fn;
+
+    QGraphicsScene * firstPage_tmpl;
+    QGraphicsScene * secondPage_tmpl;
+    QGraphicsScene * thirdPage_tmpl;
+    QGraphicsScene * fourthPage_tmpl;
+    // Текущий шаблон
+    QString date_time;               // дата и время создания шаблона
+    QString author;                  // автор шаблона (author [date_time]) отобразиться в tooltipe
+    QString templates_name;          // название шаблона, то что покажем в списке шаблонов
+    QString description;             // описание шаблона, может быть пустым
+    QPrinter::PaperSize paper_size;
+    qreal page_height;               //
+    qreal page_width;                //
+    bool  page_orient;               //
+    qreal margin_top;                //
+    qreal margin_bottom;             //
+    qreal margin_left;               //
+    qreal margin_right;              //
+
 };
 
 #endif
