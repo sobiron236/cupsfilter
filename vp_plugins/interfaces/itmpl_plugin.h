cd536ceeec8f3341cdacd7fe6e7fad045e2a5519
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-07 12:34:05 +0000

    Обновил интерфейс плагина

--- vp_plugins/interfaces/itmpl_plugin.h
+++ vp_plugins/interfaces/itmpl_plugin.h
@@ -11,13 +11,24 @@ class Itmpl_plugin{
 public:
     virtual ~Itmpl_plugin() {}
     virtual void init(const QString & spool,const QString & sid)=0;
-    virtual void createEmptyTemplate()=0;
+    virtual void createEmptyTemplate(const QString & file_name,
+                                     const QString & t_author,
+                                     const QString & t_name,
+                                     const QString & t_desc,
+                                     const QString & p_size,
+                                     bool p_orient,
+                                     const QString & c_date,
+                                     qreal m_top,
+                                     qreal m_bottom,
+                                     qreal m_right,
+                                     qreal m_left)=0;
     virtual void setTemplates(const QString & templates_in_file,QStandardItemModel * model)=0;
     virtual void printFormatingPageToFile(int pageNum)=0;
     virtual QGraphicsScene *getFirstPage()=0;
     virtual QGraphicsScene *getSecondPage()=0;
     virtual QGraphicsScene *getThirdPage()=0;
     virtual QGraphicsScene *getFourthPage()=0;
+    virtual QStringList getPageSizeList()=0;
 };
 
 
