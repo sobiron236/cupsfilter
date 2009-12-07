14efda3e3e9eb66dd6a3133a219d4ef1a20dd9f7
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-07 16:24:40 +0000

    Реализация слота setScene()

--- trunk/vp_plugins/safeFatPrinter/teditor.cpp
+++ trunk/vp_plugins/safeFatPrinter/teditor.cpp
@@ -8,14 +8,23 @@ TEditor::TEditor(QWidget *parent) :
     ui(new Ui::TEditor)
 {
     ui->setupUi(this);
-    View *view1 = new View("test");
-    ui->tabWidget->addTab(view1,QObject::trUtf8("1-я страница"));
-    View *view2 = new View("test");
-    ui->tabWidget->addTab(view2,QObject::trUtf8("2-я страница"));
-    View *view3 = new View("test");
-    ui->tabWidget->addTab(view3,QObject::trUtf8("3-я страница"));
-    View *view4 = new View("test");
-    ui->tabWidget->addTab(view4,QObject::trUtf8("4-я страница"));
+    view_1 = new View("test");
+    ui->tabWidget->addTab(view_1,QObject::trUtf8("1-я страница"));
+    view_2 = new View("test");
+    ui->tabWidget->addTab(view_2,QObject::trUtf8("2-я страница"));
+    view_3 = new View("test");
+    ui->tabWidget->addTab(view_3,QObject::trUtf8("3-я страница"));
+    view_4 = new View("test");
+    ui->tabWidget->addTab(view_4,QObject::trUtf8("4-я страница"));
+}
+
+void TEditor::setScene(QGraphicsScene *scene_1,QGraphicsScene *scene_2,
+                       QGraphicsScene *scene_3,QGraphicsScene *scene_4)
+{
+    view_1->gr_view()->setScene(scene_1);
+    view_2->gr_view()->setScene(scene_2);
+    view_3->gr_view()->setScene(scene_3);
+    view_4->gr_view()->setScene(scene_4);
 }
 
 TEditor::~TEditor()
