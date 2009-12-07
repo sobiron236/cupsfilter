b755ea0204dd2df49827d1394c5c4137450f1788
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-07 01:50:21 +0000

    Создание сеттеров void setUserName(const QString & name); void setPageSize (const QStringList & p_list,int page_height,int page_width);

--- trunk/vp_plugins/safeFatPrinter/addtemplate.cpp
+++ trunk/vp_plugins/safeFatPrinter/addtemplate.cpp
@@ -1,11 +1,20 @@
 #include "addtemplate.h"
 #include "ui_addtemplate.h"
+#include <QDateTime>
 
 AddTemplate::AddTemplate(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddTemplate)
 {
     ui->setupUi(this);
+    ui->date_lineEd->setText(QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss"));
+}
+
+void AddTemplate::setUserName(const QString & name)
+{
+    if (!name.isEmpty()){
+        ui->author_lineEd->setText(name);
+    }
 }
 
 AddTemplate::~AddTemplate()
