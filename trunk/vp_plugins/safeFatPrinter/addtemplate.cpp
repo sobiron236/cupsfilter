6b223cfed26cb9b2fb0f4c7b1307d59a7d83f5ce
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-07 03:14:47 +0000

    Создание геттеров и сеттеров для установки и получения данных из диалога

--- trunk/vp_plugins/safeFatPrinter/addtemplate.cpp
+++ trunk/vp_plugins/safeFatPrinter/addtemplate.cpp
@@ -2,12 +2,19 @@
 #include "ui_addtemplate.h"
 #include <QDateTime>
 
+
 AddTemplate::AddTemplate(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddTemplate)
 {
     ui->setupUi(this);
     ui->date_lineEd->setText(QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss"));
+    boldFont = QFont("Times", 10, QFont::Bold);
+    normalFont = QFont("Times", 10, QFont::Normal);
+
+    connect (ui->portretBtn,SIGNAL(clicked()),this,SLOT(set_portret()));
+    connect (ui->landscapeBtn,SIGNAL(clicked()),this,SLOT(set_landscape()));
+    connect (ui->pageSizeCBox,SIGNAL(currentIndexChanged(QString)),this,SLOT(setCurrentPageSize(QString)));
 }
 
 void AddTemplate::setUserName(const QString & name)
@@ -15,6 +22,78 @@ void AddTemplate::setUserName(const QString & name)
     if (!name.isEmpty()){
         ui->author_lineEd->setText(name);
     }
+}
+
+void AddTemplate::setPageSize(QStringListModel *page_size_model)
+{
+    ui->pageSizeCBox->setModel(page_size_model);
+    //ui->p_heightSpBox->setValue(page_height);
+    //ui->p_widthSpBox->setValue(page_width);
+}
+
+// --------------------------------------Гетеры
+QString AddTemplate::getTemplName()
+{
+
+}
+
+QString AddTemplate::getTemplDesc()
+{
+
+}
+
+QString AddTemplate::getPageSize()
+{
+
+}
+
+QString AddTemplate::getCreationDate()
+{
+
+}
+
+qreal AddTemplate::getMarginTop()
+{
+
+}
+
+qreal AddTemplate::getMarginBottom()
+{
+
+}
+
+qreal AddTemplate::getMarginLeft()
+{
+
+}
+
+qreal AddTemplate::getMarginRight()
+{
+
+}
+
+bool AddTemplate::getPageOrient()
+{
+
+}
+
+// ------------------------------------- private slots
+
+void AddTemplate::setCurrentPageSize(const QString &psize)
+{
+    page_size =psize;
+}
+
+void AddTemplate::set_portret()
+{
+    ui->portretBtn->setFont(boldFont);
+    ui->landscapeBtn->setFont(normalFont);
+}
+
+void AddTemplate::set_landscape()
+{
+    ui->portretBtn->setFont(normalFont);
+    ui->landscapeBtn->setFont(boldFont);
 }
 
 AddTemplate::~AddTemplate()
