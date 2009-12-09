9850027a3c800b28a4dd197579033171c34395a0
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-09 11:13:29 +0000

    Выдает следующие сигналы void addBaseElementToPage(int page); // добавить в страницу шаблона № page базовый элемент void saveTemplates(); сохранит набор сцен в шаблон

--- vp_plugins/safeFatPrinter/teditor.h
+++ vp_plugins/safeFatPrinter/teditor.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QGraphicsScene>
+#include <QSignalMapper>
 
 #include "view.h"
 
@@ -19,16 +20,23 @@ public slots:
     // Установка сцен
     void setScene(QGraphicsScene *scene_1,QGraphicsScene *scene_2,
                   QGraphicsScene *scene_3,QGraphicsScene *scene_4);
+signals:
+    void addBaseElementToPage(int page);
+    void saveTemplates();
+private slots:
 
 protected:
     void changeEvent(QEvent *e);
 
 private:
     Ui::TEditor *ui;
+
+    QSignalMapper *signalMapper;
     View *view_1;
     View *view_2;
     View *view_3;
     View *view_4;
+    QString curTemplFile;
 };
 
 #endif // TEDITOR_H
