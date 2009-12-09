cef7259c17019b507f4b973ea6b558c94de9256c
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-09 11:03:04 +0000

    Объявление и реализация void doAddBaseElementToPage(int page); // Добавим базовый элемент на страницу page void doSaveTemplates();     // сохраним текущий набор сцен в файл шаблона

--- trunk/vp_plugins/tmpl_plugin/tmpl_plugin.h
+++ trunk/vp_plugins/tmpl_plugin/tmpl_plugin.h
@@ -2,20 +2,21 @@
 #define TMPL_PLUGIN_H
 
 #include <QObject>
-
+#include <QMap>
 #include "itmpl_plugin.h"
 #include "tech_global.h"
+
 
 #define MM_TO_POINT(mm) ((mm)*2.83465058)
 
 using namespace SafeVirtualPrinter;
+
 
 class Tmpl_plugin :public QObject, Itmpl_plugin
 {
     Q_OBJECT
     Q_INTERFACES(Itmpl_plugin)
 public:
-
     Tmpl_plugin(QObject *parent=0){};
     void init(const QString & spool,const QString & sid);
     void createEmptyTemplate(const QString & file_name,
@@ -33,64 +34,52 @@ public:
 
 
     void printFormatingPageToFile(int pageNum);
-    QGraphicsScene *getFirstPage(){return firstPage_tmpl;};
-    QGraphicsScene *getSecondPage(){return secondPage_tmpl;};
-    QGraphicsScene *getThirdPage(){return thirdPage_tmpl;};
-    QGraphicsScene *getFourthPage(){return fourthPage_tmpl;};
+    QGraphicsScene *getFirstPage(){return firstPage_scene;};
+    QGraphicsScene *getSecondPage(){return secondPage_scene;};
+    QGraphicsScene *getThirdPage(){return thirdPage_scene;};
+    QGraphicsScene *getFourthPage(){return fourthPage_scene;};
     QStringList getPageSizeList();
+
 
 signals:
     void error(QString error_message);
+    void toLog(QString log_message);
     void allTemplatesPagesParsed(QGraphicsScene *scene_1,QGraphicsScene *scene_2,
                                  QGraphicsScene *scene_3,QGraphicsScene *scene_4);
     void emptyTemplateCreate(const QString & t_name);
 public slots:
     void update_scene(int pageNum);
     void setTemplates(const QString & templates_in_file,QStandardItemModel * model);
+    // Добавим базовый элемент на страницу page
+    void doAddBaseElementToPage(int page);
+    // сохраним текущий набор сцен в файл шаблона
+    void doSaveTemplates();
 protected:
     qreal findPageSize_H(int page_size_id);
     qreal findPageSize_V(int page_size_id);
     int getElemIdByName(const QString &elem_name);
     bool parse_templates(const QString & in_file);
     QString findFromModel(const QString &find_line);
+private:
+    templates_info_t t_info; // Информация о шаблоне
     static const int version = 1;
 
     QStandardItemModel *work_model;
 
     QString spool_dir;
     QString current_sid;
-    QString currentTemplates;
-    QString emptyTemplate_file; //
 
     QString firstPage_tmpl_fn;
     QString secondPage_tmpl_fn;
     QString thirdPage_tmpl_fn;
     QString fourthPage_tmpl_fn;
 
-    QGraphicsScene * firstPage_tmpl;
-    QGraphicsScene * secondPage_tmpl;
-    QGraphicsScene * thirdPage_tmpl;
-    QGraphicsScene * fourthPage_tmpl;
-
-
-    QString date_time;               //
-    QString author;                  //   (author [date_time])   tooltipe
-    QString templates_name;          //  ,
-    QString description;             //  ,
-    QString paper_size;
+    QGraphicsScene * firstPage_scene;
+    QGraphicsScene * secondPage_scene;
+    QGraphicsScene * thirdPage_scene;
+    QGraphicsScene * fourthPage_scene;
     QMap<QString, int> page_size;
-    qreal page_height;               //
-    qreal page_width;                //
-    bool  page_orient;               //
-    qreal margin_top;                //
-    qreal margin_bottom;             //
-    qreal margin_left;               //
-    qreal margin_right;              //
 
-    int firstPageElemCount;
-    int secondPageElemCount;
-    int thirdPageElemCount;
-    int fourthPageElemCount;
 };
 
 #endif
