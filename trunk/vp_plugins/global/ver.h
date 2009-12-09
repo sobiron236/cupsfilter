6570a9b2c788f7611f0bbadf45e1cfbe273eb9bd
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-09 11:03:30 +0000

    add properties

--- trunk/vp_plugins/global/ver.h
+++ trunk/vp_plugins/global/ver.h
@@ -1,9 +1,27 @@
 #ifndef VER_H
 #define VER_H
-$HeadURL$:  project uri
-$Id$:	 Project ID	
-$Rev::               $:  Revision of last commit
-$Author::            $:  Author of last commit
-$Date$:  Date of last commit
+$HeadURL: 	$:  project uri             
+$Id:		$:  project ID	            
+$Rev:: 		$:  Revision of last commit 
+$Author:: 	$:  Author of last commit   
+$Date:		$:  Date of last commit     
 
+/***************************************************************************
+ *   Copyright (C) 2009 by Paul Mosentsev	p.slant@gmail.com	   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 #endif
