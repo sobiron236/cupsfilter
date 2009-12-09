4fca845d7c9ea29fa309979a814159cad99336e4
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-09 11:19:45 +0000

    minor fix

--- trunk/vp_plugins/safeFatPrinter/view.h
+++ trunk/vp_plugins/safeFatPrinter/view.h
@@ -16,14 +16,14 @@ public:
     View(const QString &name, QWidget *parent = 0);
 
     QGraphicsView *gr_view() const;
-    //void setPageNum(int p_num){pageNum = p_num;};
+
 private slots:
     void addElemToPage(); // Добавим базовый элемент на страницу
     void resetView();
     void setResetButtonEnabled();
     void setupMatrix();
     void toggleAntialiasing();
-    void print();
+    void saveTemplates();
 
     void zoomIn();
     void zoomOut();
@@ -31,12 +31,13 @@ private slots:
 signals:
     // Требование плагину добавить элемент на страницу
     void addBaseElementToPage();
+
 private:
     QGraphicsView *graphicsView;
     QLabel *label;
 
     QToolButton *antialiasButton;
-    QToolButton *printButton;
+
     QToolButton *resetButton;
     QToolButton *addElemButton;
     QSlider *zoomSlider;
