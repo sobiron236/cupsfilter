ee52d677217fb9f16653ea22d357314aab0e1dd5
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-12 06:38:35 +0000

    Команда установки переменных окружения Для gs  TMPDIR = каталог спула указанный в ini

--- trunk/vp_plugins/gs_plugin/proc_thread.cpp
+++ trunk/vp_plugins/gs_plugin/proc_thread.cpp
@@ -5,10 +5,11 @@
 #include <QProcess>
 
 
+
 ProcessT::ProcessT( QObject *parent ) :
-	QThread( parent )
+        QThread( parent )
 {
-	m_ChanMode = QProcess::SeparateChannels;
+        m_ChanMode = QProcess::SeparateChannels;
 }
 
 ProcessT::~ProcessT() {
@@ -20,52 +21,62 @@ ProcessT::~ProcessT() {
 void ProcessT::run() {
 
 
-	if ( m_Command.isEmpty() ) {
-		qWarning() << Q_FUNC_INFO << "No command set, doing nothing";
-		return;
-	}
+        if ( m_Command.isEmpty() ) {
+                qWarning() << Q_FUNC_INFO << "No command set, doing nothing";
+                return;
+        }
 
-	qDebug() << Q_FUNC_INFO << "Executing command" << m_Command << m_Args << "\n";
+        qDebug() << Q_FUNC_INFO << "Executing command" << m_Command << m_Args << "\n";
 
-	QProcess proc;
+        QProcess proc;
+        // Add  to the environment
+        if (!n_Env.isEmpty()){
+           proc.setEnvironment(n_Env);
+        }
 
-	proc.setProcessChannelMode( m_ChanMode );
-	proc.start( m_Command, m_Args );
-	if (!proc.waitForStarted()) {
-	    qDebug()<< QString("Unable to launch application %1").arg(m_Command);
-	}else{
-	    proc.waitForFinished(-1);
-	    proc.closeWriteChannel();
-	    m_Output = proc.readAll().trimmed();
-	    qDebug() << QString("Exit code %1").arg(proc.exitCode());
+        proc.setProcessChannelMode( m_ChanMode );
+        proc.start( m_Command, m_Args );
+        if (!proc.waitForStarted()) {
+            qDebug()<< QString("Unable to launch application %1").arg(m_Command);
+        }else{
+            proc.waitForFinished(-1);
+            proc.closeWriteChannel();
+            m_Output = proc.readAll().trimmed();
+            qDebug() << QString("Exit code %1").arg(proc.exitCode());
 
-	    emit commandOutput(proc.exitCode(), m_Output );
-	    qDebug() << Q_FUNC_INFO << "Command execution finished.\n" <<m_Output;
-	}
+            emit commandOutput(proc.exitCode(), m_Output );
+            qDebug() << Q_FUNC_INFO << "Command execution finished.\n" <<m_Output;
+        }
+}
+
+void ProcessT::addToEnv(const QStringList & pairs_list )
+{
+  n_Env = QProcess::systemEnvironment();
+  n_Env << pairs_list;
 }
 
 void ProcessT::setCommand( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode ) {
 
-	if ( name.isEmpty() ) {
-		qWarning() << Q_FUNC_INFO << "Empty command given, doing nothing";
-		return;
-	}
-	m_Command = name;
-	m_Args = args;
-	m_ChanMode = mode;
+        if ( name.isEmpty() ) {
+                qWarning() << Q_FUNC_INFO << "Empty command given, doing nothing";
+                return;
+        }
+        m_Command = name;
+        m_Args = args;
+        m_ChanMode = mode;
 }
 
 void ProcessT::execute( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode ) {
 
-	setCommand( name, args, mode );
-	
-	if ( !isRunning() ) {
-		start();
-	}
-	else {
-		qWarning() << Q_FUNC_INFO << "Thread already running, doing nothing.";
-		return;
-	}
+        setCommand( name, args, mode );
+
+        if ( !isRunning() ) {
+                start();
+        }
+        else {
+                qWarning() << Q_FUNC_INFO << "Thread already running, doing nothing.";
+                return;
+        }
 
 }
 
--- trunk/vp_plugins/gs_plugin/proc_thread.h
+++ trunk/vp_plugins/gs_plugin/proc_thread.h
@@ -11,59 +11,65 @@ class QProcess;
 
 
 /**
-* 
-* 
+*
+*
 * @short Class which can execute a command as a seperate thread.
-* 
+*
 */
 
 class ProcessT : public QThread {
 
-	Q_OBJECT
+        Q_OBJECT
 
-	public:
-		/**
-		 * Constructor. Creats a new ProcessT object.
-		 * @param parent The QObject parent of this object.
-		 */
-		ProcessT( QObject *parent = 0 );
-		/**
-		 * Destructor.
-		 */
-		virtual ~ProcessT();
-		/**
-		 * Reimplementation of QThread::run().
-		 * Executes the command which was set using setCommand(). 
-		 * If no command was set, this function does nothing.
-		 */
-		void run();
-		/**
-		 * Set the given command to be executed when run() is called.
-		 * @param name The name of the program.
-		 * @param args The command line arguments.
-		 * @param mode The process channel modes of the command which will be executed.
-		 */
-		void setCommand( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode = QProcess::SeparateChannels );
-		/**
-		 * Execute the given command. Calls setCommand() first and then just starts the thread if it's not running.
-		 * @param name The name of the program.
-		 * @param args The command line arguments.
-		 * @param mode The process channel modes of the command which will be executed.
-		 */
-		void execute( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode = QProcess::SeparateChannels );
+        public:
+                /**
+                 * Constructor. Creats a new ProcessT object.
+                 * @param parent The QObject parent of this object.
+                 */
+                ProcessT( QObject *parent = 0 );
+                /**
+                 * Destructor.
+                 */
+                virtual ~ProcessT();
+                /**
+                 * Reimplementation of QThread::run().
+                 * Executes the command which was set using setCommand().
+                 * If no command was set, this function does nothing.
+                 */
+                void run();
+                /**
+                 * Set the given command to be executed when run() is called.
+                 * @param name The name of the program.
+                 * @param args The command line arguments.
+                 * @param mode The process channel modes of the command which will be executed.
+                 */
+                void setCommand( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode = QProcess::SeparateChannels );
+                /**
+                 * Execute the given command. Calls setCommand() first and then just starts the thread if it's not running.
+                 * @param name The name of the program.
+                 * @param args The command line arguments.
+                 * @param mode The process channel modes of the command which will be executed.
+                 */
+                void execute( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode = QProcess::SeparateChannels );
+                /**
+                 * Sets the environment that QProcess will use when starting a process to the environment
+                 * @param environment  consists of a list of key=value pairs will add to evn
+                 */
 
-	signals:
-		/**
-		 * Emitted when a command has finished running. 
-		 * @param output The output of the command which was executed.
-		 */
-		void commandOutput(int Code,QString output);
+                void addToEnv(const QStringList & pairs_list );
+        signals:
+                /**
+                 * Emitted when a command has finished running.
+                 * @param output The output of the command which was executed.
+                 */
+                void commandOutput(int Code,QString output);
 
-	private:
-		QString m_Command;
-		QStringList m_Args;
-		QProcess::ProcessChannelMode m_ChanMode;
-		QString m_Output;
+        private:
+                QString m_Command;
+                QStringList m_Args;
+                QStringList n_Env;
+                QProcess::ProcessChannelMode m_ChanMode;
+                QString m_Output;
 
 
 };
