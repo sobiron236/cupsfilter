619ca0a5eced3d3630e09a370fcb0b4fc85c5370
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-14 10:50:18 +0000

    Основной файл

--- trunk/vp_plugins/tEditor/main.cpp
+++ trunk/vp_plugins/tEditor/main.cpp
@@ -6,20 +6,19 @@
 #include "tech_global.h"
 #include "mainwindow.h"
 
+
 int main(int argc, char *argv[])
 {
-    Q_INIT_RESOURCE(images);
-
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     QTextCodec::setCodecForTr(codec);
-    //QTextCodec::setCodecForTr( QTextCodec::codecForName( "cp866" ) );
     QTextCodec::setCodecForCStrings(codec);
     QTextCodec::setCodecForLocale(codec);
     qInstallMsgHandler(myMessageOutput);
 
+    QApplication app(argc, argv);
+    Q_INIT_RESOURCE(images);
+
     MainWindow window;
-    window.loadPlugin(qApp->applicationDirPath());
-        //app.processEvents();
     window.show();
     return app.exec();
 
