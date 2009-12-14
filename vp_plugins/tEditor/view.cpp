3fa18c0ac148aad22d18de9d0604735785df8f23
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-14 10:54:30 +0000

    Добавил функцию -> String &getPageName(){return pageName;}; возвращает имя страницы заданное при создании Освободил окно от всех кнопок кроме слайдера масштаба

--- vp_plugins/tEditor/view.cpp
+++ vp_plugins/tEditor/view.cpp
@@ -7,6 +7,7 @@
 View::View(const QString &name, QWidget *parent)
     : QFrame(parent)
 {
+    pageName = name;
     setFrameStyle(Sunken | StyledPanel);
     graphicsView = new QGraphicsView;
     graphicsView->setRenderHint(QPainter::Antialiasing, false);
@@ -42,60 +43,18 @@ View::View(const QString &name, QWidget *parent)
     zoomSliderLayout->addWidget(zoomOutIcon);
 
 
-    resetButton = new QToolButton;
-    resetButton->setText(tr("0"));
-    resetButton->setEnabled(false);
-
-    // Label layout
-    QHBoxLayout *labelLayout = new QHBoxLayout;
-    label = new QLabel(name);
-    antialiasButton = new QToolButton;
-    antialiasButton->setText(QObject::trUtf8("Сглаживание"));
-    antialiasButton->setCheckable(true);
-    antialiasButton->setChecked(false);
-
-
-    QToolButton *saveButton = new QToolButton;
-    saveButton->setIcon(QIcon(QPixmap(":/images/save.png")));
-
-    addElemButton = new QToolButton;
-    addElemButton->setIcon(QPixmap(":/images/edit_add.png"));
-    addElemButton->setIconSize(iconSize);
-
-    labelLayout->addWidget(addElemButton);
-    labelLayout->addStretch();
-    labelLayout->addWidget(label);
-    labelLayout->addStretch();
-    labelLayout->addWidget(antialiasButton);
-
-    labelLayout->addWidget(saveButton);
-
     QGridLayout *topLayout = new QGridLayout;
-    topLayout->addLayout(labelLayout, 0, 0);
+
     topLayout->addWidget(graphicsView, 1, 0);
     topLayout->addLayout(zoomSliderLayout, 1, 1);
-
-    topLayout->addWidget(resetButton, 2, 1);
     setLayout(topLayout);
 
-    connect(resetButton, SIGNAL(clicked()), this, SLOT(resetView()));
     connect(zoomSlider, SIGNAL(valueChanged(int)), this, SLOT(setupMatrix()));
-
     connect(graphicsView->verticalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(setResetButtonEnabled()));
     connect(graphicsView->horizontalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(setResetButtonEnabled()));
-    connect(antialiasButton, SIGNAL(toggled(bool)), this, SLOT(toggleAntialiasing()));
 
     connect(zoomInIcon, SIGNAL(clicked()), this, SLOT(zoomIn()));
     connect(zoomOutIcon, SIGNAL(clicked()), this, SLOT(zoomOut()));
-
-
-    connect(addElemButton,SIGNAL(clicked()),this,SIGNAL(addBaseElementToPage()));
-
-    connect (saveButton,
-             SIGNAL(clicked()),
-             this,
-             SLOT(saveTemplates())
-             );
     setupMatrix();
 }
 
@@ -111,12 +70,12 @@ void View::resetView()
     setupMatrix();
     graphicsView->ensureVisible(QRectF(0, 0, 0, 0));
 
-    resetButton->setEnabled(false);
+    //resetButton->setEnabled(false);
 }
 
 void View::setResetButtonEnabled()
 {
-    resetButton->setEnabled(true);
+    //resetButton->setEnabled(true);
 }
 
 void View::setupMatrix()
@@ -134,26 +93,12 @@ void View::setupMatrix()
 
 void View::toggleAntialiasing()
 {
+    /*
     graphicsView->setRenderHint(QPainter::Antialiasing,
                                 antialiasButton->isChecked());
+                                */
 }
 
-void View::saveTemplates()
-{
-    QPrinter pdfprinter;
-    pdfprinter.setOutputFormat(QPrinter::PdfFormat);
-    pdfprinter.setOutputFileName("d:/test.pdf");
-
-    QPainter painter(&pdfprinter);
-    graphicsView->scene()->render(&painter);
-
-    QPrinter pdfprinter2;
-    pdfprinter2.setOutputFormat(QPrinter::PdfFormat);
-    pdfprinter2.setOutputFileName("d:/test2.pdf");
-
-    QPainter painter2(&pdfprinter2);
-    graphicsView->render(&painter2);
-}
 
 void View::zoomIn()
 {
@@ -164,11 +109,3 @@ void View::zoomOut()
 {
     zoomSlider->setValue(zoomSlider->value() - 1);
 }
-// --------------------------- private slots ----------------------------------
-
-void View::addElemToPage()
-{
-    emit addBaseElementToPage();
-}
-
-
