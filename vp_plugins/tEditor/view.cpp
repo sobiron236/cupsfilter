fac02b4e2e8a1dc3e207834cf68b0bf34f17749f
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-14 10:46:46 +0000

    Для редактора использую облегченную версию, потом она вернется в основную программу

---
+++ vp_plugins/tEditor/view.cpp
@@ -0,0 +1,174 @@
+#include "view.h"
+
+#include <QtGui>
+#include <qmath.h>
+
+
+View::View(const QString &name, QWidget *parent)
+    : QFrame(parent)
+{
+    setFrameStyle(Sunken | StyledPanel);
+    graphicsView = new QGraphicsView;
+    graphicsView->setRenderHint(QPainter::Antialiasing, false);
+    graphicsView->setDragMode(QGraphicsView::RubberBandDrag);
+    graphicsView->setOptimizationFlags(QGraphicsView::DontSavePainterState);
+    graphicsView->setViewportUpdateMode(QGraphicsView::SmartViewportUpdate);
+
+    int size = style()->pixelMetric(QStyle::PM_ToolBarIconSize);
+    QSize iconSize(size, size);
+
+    QToolButton *zoomInIcon = new QToolButton;
+    zoomInIcon->setAutoRepeat(true);
+    zoomInIcon->setAutoRepeatInterval(33);
+    zoomInIcon->setAutoRepeatDelay(0);
+    zoomInIcon->setIcon(QPixmap(":/zoomin.png"));
+    zoomInIcon->setIconSize(iconSize);
+    QToolButton *zoomOutIcon = new QToolButton;
+    zoomOutIcon->setAutoRepeat(true);
+    zoomOutIcon->setAutoRepeatInterval(33);
+    zoomOutIcon->setAutoRepeatDelay(0);
+    zoomOutIcon->setIcon(QPixmap(":/zoomout.png"));
+    zoomOutIcon->setIconSize(iconSize);
+    zoomSlider = new QSlider;
+    zoomSlider->setMinimum(0);
+    zoomSlider->setMaximum(500);
+    zoomSlider->setValue(250);
+    zoomSlider->setTickPosition(QSlider::TicksRight);
+
+    // Zoom slider layout
+    QVBoxLayout *zoomSliderLayout = new QVBoxLayout;
+    zoomSliderLayout->addWidget(zoomInIcon);
+    zoomSliderLayout->addWidget(zoomSlider);
+    zoomSliderLayout->addWidget(zoomOutIcon);
+
+
+    resetButton = new QToolButton;
+    resetButton->setText(tr("0"));
+    resetButton->setEnabled(false);
+
+    // Label layout
+    QHBoxLayout *labelLayout = new QHBoxLayout;
+    label = new QLabel(name);
+    antialiasButton = new QToolButton;
+    antialiasButton->setText(QObject::trUtf8("Сглаживание"));
+    antialiasButton->setCheckable(true);
+    antialiasButton->setChecked(false);
+
+
+    QToolButton *saveButton = new QToolButton;
+    saveButton->setIcon(QIcon(QPixmap(":/images/save.png")));
+
+    addElemButton = new QToolButton;
+    addElemButton->setIcon(QPixmap(":/images/edit_add.png"));
+    addElemButton->setIconSize(iconSize);
+
+    labelLayout->addWidget(addElemButton);
+    labelLayout->addStretch();
+    labelLayout->addWidget(label);
+    labelLayout->addStretch();
+    labelLayout->addWidget(antialiasButton);
+
+    labelLayout->addWidget(saveButton);
+
+    QGridLayout *topLayout = new QGridLayout;
+    topLayout->addLayout(labelLayout, 0, 0);
+    topLayout->addWidget(graphicsView, 1, 0);
+    topLayout->addLayout(zoomSliderLayout, 1, 1);
+
+    topLayout->addWidget(resetButton, 2, 1);
+    setLayout(topLayout);
+
+    connect(resetButton, SIGNAL(clicked()), this, SLOT(resetView()));
+    connect(zoomSlider, SIGNAL(valueChanged(int)), this, SLOT(setupMatrix()));
+
+    connect(graphicsView->verticalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(setResetButtonEnabled()));
+    connect(graphicsView->horizontalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(setResetButtonEnabled()));
+    connect(antialiasButton, SIGNAL(toggled(bool)), this, SLOT(toggleAntialiasing()));
+
+    connect(zoomInIcon, SIGNAL(clicked()), this, SLOT(zoomIn()));
+    connect(zoomOutIcon, SIGNAL(clicked()), this, SLOT(zoomOut()));
+
+
+    connect(addElemButton,SIGNAL(clicked()),this,SIGNAL(addBaseElementToPage()));
+
+    connect (saveButton,
+             SIGNAL(clicked()),
+             this,
+             SLOT(saveTemplates())
+             );
+    setupMatrix();
+}
+
+QGraphicsView *View::gr_view() const
+{
+    return graphicsView;
+}
+
+void View::resetView()
+{
+    zoomSlider->setValue(250);
+
+    setupMatrix();
+    graphicsView->ensureVisible(QRectF(0, 0, 0, 0));
+
+    resetButton->setEnabled(false);
+}
+
+void View::setResetButtonEnabled()
+{
+    resetButton->setEnabled(true);
+}
+
+void View::setupMatrix()
+{
+    qreal scale = qPow(qreal(2), (zoomSlider->value() - 250) / qreal(50));
+
+    QMatrix matrix;
+    matrix.scale(scale, scale);
+
+    graphicsView->setMatrix(matrix);
+    setResetButtonEnabled();
+}
+
+
+
+void View::toggleAntialiasing()
+{
+    graphicsView->setRenderHint(QPainter::Antialiasing,
+                                antialiasButton->isChecked());
+}
+
+void View::saveTemplates()
+{
+    QPrinter pdfprinter;
+    pdfprinter.setOutputFormat(QPrinter::PdfFormat);
+    pdfprinter.setOutputFileName("d:/test.pdf");
+
+    QPainter painter(&pdfprinter);
+    graphicsView->scene()->render(&painter);
+
+    QPrinter pdfprinter2;
+    pdfprinter2.setOutputFormat(QPrinter::PdfFormat);
+    pdfprinter2.setOutputFileName("d:/test2.pdf");
+
+    QPainter painter2(&pdfprinter2);
+    graphicsView->render(&painter2);
+}
+
+void View::zoomIn()
+{
+    zoomSlider->setValue(zoomSlider->value() + 1);
+}
+
+void View::zoomOut()
+{
+    zoomSlider->setValue(zoomSlider->value() - 1);
+}
+// --------------------------- private slots ----------------------------------
+
+void View::addElemToPage()
+{
+    emit addBaseElementToPage();
+}
+
+
