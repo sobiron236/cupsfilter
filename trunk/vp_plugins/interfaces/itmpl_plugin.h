e7c8f75b41f6843e53f3eeea0c4ab886207d770f
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-16 09:46:09 +0000

    Интерфейс новой функции QSize getPageSizeFromString()

--- trunk/vp_plugins/interfaces/itmpl_plugin.h
+++ trunk/vp_plugins/interfaces/itmpl_plugin.h
@@ -12,18 +12,20 @@ class Itmpl_plugin{
 public:
     virtual ~Itmpl_plugin() {}
     virtual void init(const QString & spool,const QString & sid)=0;
-    virtual void loadTemplates(const QString & templates_in_file)=0;	
+    virtual void loadTemplates(const QString & templates_in_file)=0;
     virtual void createEmptyTemplate(const QString & file_name,
-                                     const QString & t_author,
-                                     const QString & t_name,
-                                     const QString & t_desc,
-                                     const QString & p_size,
-                                     bool p_orient,
-                                     const QString & c_date,
-                                     qreal m_top,
-                                     qreal m_bottom,
-                                     qreal m_right,
-                                     qreal m_left)=0;
+                             const QString & t_author,
+                             const QString & t_name,
+                             const QString & t_desc,
+                             const QString & p_size,
+
+                             bool pages_orient,
+                             const QString & c_date,
+                             qreal m_top,
+                             qreal m_bottom,
+                             qreal m_right,
+                             qreal m_left)=0;
+
     virtual void setTemplates(const QString & templates_in_file,QStandardItemModel * model)=0;
     virtual void convertTemplatesToPdf(const QString & templates_in_file,QStandardItemModel * model)=0;
     virtual void printFormatingPageToFile(int pageNum)=0;
@@ -31,9 +33,11 @@ public:
     virtual QGraphicsScene *getSecondPage()=0;
     virtual QGraphicsScene *getThirdPage()=0;
     virtual QGraphicsScene *getFourthPage()=0;
-    virtual QStringList getPageSizeList()=0;
+    virtual QStringList    getPageSizeList()=0;
+    virtual QSize getPageSizeFromString(QString & page_str)=0;
     virtual bool getPageOrientation()=0;
     virtual void setPageOrientation(bool p_orient)=0;
+
 };
 
 
