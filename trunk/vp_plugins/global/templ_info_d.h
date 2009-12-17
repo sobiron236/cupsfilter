2c94d2ade9df06ac70a8d65d2493d54e974282c5
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-17 10:32:25 +0000

    Ghbd//Приватный класс с разделением данных. Используется QSharedData. //класс Templ_info_d, представляющий информацию о шаблоне,

---
+++ trunk/vp_plugins/global/templ_info_d.h
@@ -0,0 +1,36 @@
+#ifndef TEMPL_INFO_D_H
+#define TEMPL_INFO_D_H
+//Приватный класс с разделением данных. Используется QSharedData.
+//класс Templ_info_d, представляющий информацию о шаблоне,
+#include <QSharedData>
+#include <QString>
+
+class Templ_info_d : public QSharedData
+{
+public:
+    // Конструкторы
+    Templ_info_d();
+    Templ_info_d (const Templ_info_d& other);
+    ~Templ_info_d() {};
+
+    int t_ver; 	        // версия шаблона
+    QString date_time;  // дата и время создания
+    QString t_author;   // автор шаблона
+    QString t_name;     // имя шаблона
+    QString t_desc;     // описание шаблона
+    QString p_size;     // размер бумаги (Для человека)
+    bool  page_orient;  // ориентация страницы Книжная/альбомная true/false
+    qreal page_width;   // ширина листа в  [мм -> point] (для компьютера)
+    qreal page_height;  // высота листа в  [мм -> point] (для компьютера)
+    qreal m_top;        // отступ сверху в [мм -> point] (для компьютера)
+    qreal m_bottom;     // отступ снизу в [мм -> point] (для компьютера)
+    qreal m_left;       // отступ слева в [мм -> point] (для компьютера)
+    qreal m_right;      // отступ справа в [мм -> point] (для компьютера)
+    int firstPageElemCount;    // число элементов на первой странице шаблона
+    int secondPageElemCount;   // число элементов на второй странице шаблона
+    int thirdPageElemCount;    // число элементов на третьей странице шаблона
+    int fourthPageElemCount;   // число элементов на четвертой странице шаблона
+};
+
+
+#endif
