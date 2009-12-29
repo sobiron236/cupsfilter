b9cfb677e2062b7cb2c1048b8a4afee4c372d301
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-29 04:08:12 +0000

    Селектор выбора режима запуска

--- trunk/vp_plugins/tEditor/main.cpp
+++ trunk/vp_plugins/tEditor/main.cpp
@@ -1,6 +1,7 @@
 #include <QtGui/QApplication>
 #include <QDebug>
 #include <QTextCodec>
+#include <QStringList>
 
 #include "config.h"
 #include "tech_global.h"
@@ -18,8 +19,33 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     Q_INIT_RESOURCE(images);
 
+    QStringList aList=app.arguments();
+    /*
+     * @brief По количеству аргументов определяем режим работы
+     * - sizeOf (argv[]) = 0 (без аргументов) режим создания пустого шаблона 
+     * - sizeOf (argv[]) = 1 (имя_файла_шаблона) редактирование выбранного шаблона
+     * - sizeOf (argv[]) = 2 (имя_файла_шаблона) (имя файла с данными модели)
+     *                       показ шаблона с установленными значениями аргументов  
+    */
+    QString file_name;
+    QString file_name_dat;
     MainWindow window;
     window.show();
-    return app.exec();
 
+    switch (aList.size()){
+     case 0:
+       window.createNewTemplate();
+       break;
+     case 1:
+       file_name = aList.at(1);
+       window.loadFromFile(file_name);
+       break;
+     case 2:
+       file_name = aList.at(1);
+       file_name_dat = aList.at(2);
+       window.loadFromFileWithDat(file_name,file_name_dat);
+       break;
+    }
+    
+    return app.exec();
 }
