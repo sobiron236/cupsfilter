0362608998ae1f8afa506b72e89315e719e0b53c
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2009-12-29 04:02:47 +0000

    Добавлены новые функции в интерфейс работа с dat файлом

--- trunk/vp_plugins/interfaces/itmpl_plugin.h
+++ trunk/vp_plugins/interfaces/itmpl_plugin.h
@@ -13,44 +13,36 @@ class Itmpl_plugin{
 
 public:
     virtual ~Itmpl_plugin() {}
-    virtual void init(const QString & spool,const QString & sid)=0;
-    virtual QStandardItemModel * getModel()=0;
+    virtual void init(const QString & spool,const QString & sid) = 0;
+    virtual QStandardItemModel * getModel() = 0;
 
-    // сохранение текущей модели в xml файл
-    virtual QString saveModel2Xml() = 0;
+    // сохранение текущей модели в data файл [model_data_file]
+    virtual bool saveModel2Data() = 0;
     //Загрузка модели из xml файла
-    virtual void loadModel4Xml(const QString &in_file) = 0;
+    virtual bool loadModel4Data(const QString &in_file) = 0;
 
-    virtual void loadTemplates(const QString & templates_in_file)=0;
-    virtual void saveTemplatesAs(const QString & save_file)=0;
+    virtual QString & getModelDataFile() = 0;
+
+    virtual void loadTemplates(const QString & templates_in_file) = 0;
+    virtual void loadTemplatesWithDat(const QString & templates_in_file,
+                                      const QString & in_file_dat) = 0;
+
+    virtual void saveTemplatesAs(const QString & save_file) = 0;
     virtual void createEmptyTemplate(const QString & file_name) = 0;
-    virtual void createEmptyTemplate(const QString & file_name,
-                             const QString & t_author,
-                             const QString & t_name,
-                             const QString & t_desc,
-                             const QString & p_size,
-
-                             bool pages_orient,
-                             const QString & c_date,
-                             qreal m_top,
-                             qreal m_bottom,
-                             qreal m_right,
-                             qreal m_left)=0;
-
-    virtual void setTemplates(const QString & templates_in_file)=0;
-    virtual void convertTemplatesToPdf(const QString & templates_in_file)=0;
-    virtual void printFormatingPageToFile(int pageNum)=0;
-    virtual QGraphicsScene *getFirstPage()=0;
-    virtual QGraphicsScene *getSecondPage()=0;
-    virtual QGraphicsScene *getThirdPage()=0;
-    virtual QGraphicsScene *getFourthPage()=0;
-    virtual QStringList getPageSizeList()=0;
-    virtual QStringList getElemNameList()=0;
-    virtual QSize getPageSizeFromString(QString & page_str)=0;
+    virtual void setTemplates(const QString & templates_in_file) = 0;
+    virtual void convertTemplatesToPdf(const QString & templates_in_file) = 0;
+    virtual void printFormatingPageToFile(int pageNum) = 0;
+    virtual QGraphicsScene *getFirstPage()  = 0;
+    virtual QGraphicsScene *getSecondPage() = 0;
+    virtual QGraphicsScene *getThirdPage()  = 0;
+    virtual QGraphicsScene *getFourthPage() = 0;
+    virtual QStringList getPageSizeList() = 0;
+    virtual QStringList getElemNameList() = 0;
+    virtual QSize getPageSizeFromString(QString & page_str) = 0;
 //    virtual bool getPageOrientation()=0;
-    virtual void setPageOrientation(bool p_orient)=0;
-    virtual Templ_info getTemplInfo() =0;
-    virtual void setTemplInfo(Templ_info tInfo) =0;
+    virtual void setPageOrientation(bool p_orient) = 0;
+    virtual Templ_info getTemplInfo() = 0;
+    virtual void setTemplInfo(Templ_info tInfo) = 0;
 
 };
 
