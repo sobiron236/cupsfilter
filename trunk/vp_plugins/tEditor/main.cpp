2bf7f413455cb47dbe9fbe930eecc1e92beba02f
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-02-14 23:08:14 +0000

    При запуске программы воозможны все два варианта:  1 нет аргументов командной строки - создаем новый шаблон  2 Есть путь к файлу шаблона загружаем его

--- trunk/vp_plugins/tEditor/main.cpp
+++ trunk/vp_plugins/tEditor/main.cpp
@@ -22,10 +22,8 @@ int main(int argc, char *argv[])
     QStringList aList=app.arguments();
     /*
      * @brief По количеству аргументов определяем режим работы
-     * - sizeOf (argv[]) = 0 (без аргументов) режим создания пустого шаблона
-     * - sizeOf (argv[]) = 1 (имя_файла_шаблона) редактирование выбранного шаблона
-     * - sizeOf (argv[]) = 2 (имя_файла_шаблона) (имя файла с данными модели)
-     *                       показ шаблона с установленными значениями аргументов
+     * - sizeOf (argv[]) = 1 (без аргументов) режим создания пустого шаблона
+     * - sizeOf (argv[]) = 2 (имя_файла_шаблона) редактирование выбранного шаблона
     */
     QString file_name;
     QString file_name_dat;
@@ -39,11 +37,6 @@ int main(int argc, char *argv[])
      case 2:
        file_name = aList.at(1);
        window.loadFromFile(file_name);
-       break;
-     case 3:
-       file_name = aList.at(1);
-       file_name_dat = aList.at(2);
-       window.loadFromFileWithDat(file_name,file_name_dat);
        break;
     }
 
