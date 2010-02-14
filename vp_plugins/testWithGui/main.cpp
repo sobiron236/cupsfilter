5291cd3ddc77e9e9cff254d04df2c22b5b359ecf
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-02-14 15:27:17 +0000

    small fix

--- vp_plugins/testWithGui/main.cpp
+++ vp_plugins/testWithGui/main.cpp
@@ -16,26 +16,26 @@ void myMessageOutput(QtMsgType type, const char *msg)
         logFile.open(stderr, QIODevice::WriteOnly);
     }
 
-     QTextStream out;
-     out.setDevice(&logFile);
-     out.setCodec("UTF-8");
+    QTextStream out;
+    out.setDevice(&logFile);
+    out.setCodec("UTF-8");
 
-     out << "\nDateTime: " << QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss");
-     switch (type) {
-     case QtDebugMsg:
+    out << "\nDateTime: " << QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss");
+    switch (type) {
+    case QtDebugMsg:
         out << QObject::trUtf8("Debug: %1\n").arg(QString(msg)) <<"\n";
-         break;
-     case QtWarningMsg:
-         out << QObject::trUtf8("Warning: %1\n").arg(QString(msg))<<"\n";
-         break;
-     case QtCriticalMsg:
-         out << QObject::trUtf8("Critical: %1\n").arg(QString(msg))<<"\n";
-         break;
-     case QtFatalMsg:
-         out << QObject::trUtf8("Fatal: %1\n").arg(QString(msg))<<"\n";
-         abort();
-     }
-     logFile.close();
+        break;
+    case QtWarningMsg:
+        out << QObject::trUtf8("Warning: %1\n").arg(QString(msg))<<"\n";
+        break;
+    case QtCriticalMsg:
+        out << QObject::trUtf8("Critical: %1\n").arg(QString(msg))<<"\n";
+        break;
+    case QtFatalMsg:
+        out << QObject::trUtf8("Fatal: %1\n").arg(QString(msg))<<"\n";
+        abort();
+    }
+    logFile.close();
 }
 
 void createView(const QString &title, QAbstractItemModel * model )
@@ -46,6 +46,7 @@ void createView(const QString &title, QAbstractItemModel * model )
     view->setModel(model);
     view->setWindowTitle(title);
     view->move(100 + offset, 100 + offset);
+    view->verticalHeader()->setVisible(true);
     offset += 20;
     view->show();
 }
@@ -64,20 +65,22 @@ int main(int argc, char *argv[])
     pluginWorker *testObj = new pluginWorker();
 
     if (testObj->loadPlugin()){
-       testObj->createEmptyTemplate("d://test_templ.tmpl");
-       /*
+
+        if (testObj->createEmptyTemplate()){
+            createView(QObject::tr("Info Query Model"), testObj->getInfoModel());
+            //createView(QObject::tr("PageSize Query Model"), testObj->getPSizeModel());
+            addTempl->setInfoModel(testObj->getInfoModel());
+            addTempl->setPageSizeModel(testObj->getPSizeModel());
+            addTempl->default_init();
+            addTempl->setEnableGUI(true);
+            addTempl->show();
+
+        }
+        /*
        MyFilterProxyModel *proxyModel = new MyFilterProxyModel;
        proxyModel->setSourceModel(testObj->getPSizeModel());
        createView(QObject::tr("Proxy Model"), proxyModel);
        */
-
-       createView(QObject::tr("Info Query Model"), testObj->getInfoModel());
-       createView(QObject::tr("PageSize Query Model"), testObj->getPSizeModel());
-
-       addTempl->setInfoModel(testObj->getInfoModel());
-       addTempl->setPageSizeModel(testObj->getPSizeModel());
-
-       addTempl->show();
     }
 
     return app.exec();
