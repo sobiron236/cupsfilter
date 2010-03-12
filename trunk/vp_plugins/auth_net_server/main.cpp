a68a2b2bb034eac90bcbb3293397187384a0b7a2
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-12 08:08:30 +0000

    Настройка запуска приложения

--- trunk/vp_plugins/auth_net_server/main.cpp
+++ trunk/vp_plugins/auth_net_server/main.cpp
@@ -5,17 +5,34 @@
  *  net_plugin   Передает демону команды и выдает ответ от демона
 */
 
-#include <QApplication>
-#include <QtCore>
-
-#include <stdlib.h>
+#include <QtGui/QSystemTrayIcon>
+#include <QtGui/QMessageBox>
+#include <QtCore/QTextCodec>
 
 #include "server.h"
 
 int main(int argc, char *argv[])
 {
+
+    Q_INIT_RESOURCE(images);
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForTr(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForLocale(codec);
+
     QApplication app(argc, argv);
+
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(0, QObject::tr("Auth_net_Server"),
+                              QObject::tr("Не могу определить тип system tray "
+                                          " для вашей системы."));
+        return 1;
+    }
+
+    QApplication::setQuitOnLastWindowClosed(false);
     Server server;
     server.show();
-    return server.exec();
+
+    return app.exec();
 }
