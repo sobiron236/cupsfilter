ed5536618d8f89d316d8ab88978edf7dadbf9342
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-12 11:19:51 +0000

    Информационное окно, сразу скрывается в Трей

--- trunk/vp_plugins/auth_net_server/main.cpp
+++ trunk/vp_plugins/auth_net_server/main.cpp
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
     QApplication::setQuitOnLastWindowClosed(false);
     Server server;
     server.show();
+    server.setVisible(false);
 
     return app.exec();
 }
