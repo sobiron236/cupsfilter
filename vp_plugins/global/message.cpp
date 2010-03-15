b2df61fd16d6d114738804f6a23da7417c41686d
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-15 06:59:14 +0000

    @fn QByteArray createPacket() const; @brief Создает и возвращает пакет данных готовый для передачи @return QByteArray
    @fn bool isMessageReady();
    @brief Возвращает TRUE Если сообщение готов для отправки иначе FALSE
    @return bool
    
    @fn void prepareMessage();
    @brief Подготовка сообщения перед  отправкой
    
    @fn void setMessage( const QByteArray &msg );
    @param msg const QByteArray &
    @brief Запись msg во внутреннюю структуру и разбор его как сообщения

--- vp_plugins/global/message.cpp
+++ vp_plugins/global/message.cpp
@@ -1,6 +1,74 @@
 #include "message.h"
 
-Message::Message(QObject *parent) :
-    QObject(parent)
+#include <QtCore/QMetaType>
+#include <QtCore/QDataStream>
+
+Message::Message(QObject *parent)
+    : QObject(parent)
+    , messageType( NoType )
+    , msgData()
+    , ready( false )
+
 {
+
 }
+
+bool Message::isMessageReady()
+{
+    return ready;
+}
+
+void Message::setType( MessageType tp )
+{
+    messageType =  tp ;
+}
+
+MessageType Message::type() const
+{
+    return messageType;
+}
+
+void Message::setMessage( const QByteArray &msg )
+{
+    msgData = msg;
+}
+
+QByteArray Message::messageData() const
+{
+    return msgData;
+}
+
+void Message::prepareMessage()
+{
+
+}
+
+QByteArray Message::createPacket() const
+{
+//    qDebug() << "createPacket(): before creating: \n"
+//             << "messageData() "  << this->msgData
+//             << "type() "         << this->type();
+
+    QByteArray packet;
+    QDataStream out(&packet, QIODevice::WriteOnly );
+    out.setVersion(QDataStream::Qt_4_0);
+    // ¬ставим размер пакета равный нулю, но отведем под него 4 байта
+    out << (qint32)0;
+
+    //¬ставим формат протокола
+    out<<VPrn::format;
+
+    //¬ставим “ип сообщени€ и само сообщение в пакет
+    out << ( int ) this->type();
+    out << this->msgData;
+    // возврат на начало блока
+    out.device()->seek(0);
+    // ¬ычислим размер блока данных и запишем их на свое место
+    out << (qint32)(packet.size() - sizeof(qint32));
+
+//    qDebug() << "\ncreatePacket(): after creating "
+//             << " packet " << packet
+//             << " full packet size: " << packet.size();
+    return packet;
+}
+
