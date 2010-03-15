6d820d8da8cf6fd7199216441e9b781571b9df38
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-15 06:56:15 +0000

    Добавил: @fn bool isError() @fn QString lastError() signals:     void messageReady( const Message &msg );     void networkProtocolError(); private slots:
    @fn void readyRead()
    @brief Клиент передает данные, прочтем  и определим что он там шлет
    
    @fn void disconnected();
    @brief Клиент покинул, наше общество! Вычеркнем предателя из списка живых
    
    @fn void prepareError(QLocalSocket::LocalSocketError socketError);
    @brief Обработчик ошибок возникших в сокете
    
    private:
        /**
          * @var m_serverName;     Имя локального сервера для общения с Локальным миром
          * @var packetSize;       Размер передаваемого блока данных
          * @var currentDataBlock; Текущий полученный блок данных
          * @var e_state;          Статус Есть ошибка или нет
          * @var e_info;           Последняя возникшая ошибка
          * @var clients;          Список подключенных клиентов
          * @var clients_name;     Список имен подключенных клиентов
          */
    //-----------------------------------------------------------------------------
    @fn void parseMessage( const Message &msg, const QLocalSocket *client);
    @brief Обработка сообщения от конкретного клиента
    
    @fn void sendMessage( const Message &msg, const QLocalSocket *client);
    @brief Отправка сообщения конкретному клиенту
    
    @fn setError(const QString &info);
    @brief Устанавливает состояние ошибки,  записывает новое описание ошибки

--- vp_plugins/auth_net_server/servergears.h
+++ vp_plugins/auth_net_server/servergears.h
@@ -1,59 +1,101 @@
 #ifndef SERVERGEARS_H
 #define SERVERGEARS_H
 
-#include <QObject>
-#include <QLocalSocket>
+#include "message.h"
+#include "mytypes.h"
+
+#include <QtCore/QObject>
+#include <QtCore/QMap>
+#include <QtCore/QSet>
+
+#include <QtNetWork/QLocalServer>
+#include <QtNetWork/QLocalSocket>
+
 
 class QByteArray;
 
-class serverGears : public QObject
+using namespace VPrn;
+
+class serverGears : public QLocalServer
 {
     Q_OBJECT
+
 public:
-    explicit serverGears(QObject *parent = 0,const QString &srvName = QString());
+    explicit serverGears(QObject *parent = 0,const QString &srvName = QString());    
 
+    bool isError(){return e_state;};
+    QString lastError(){return e_info;};
 
+signals:
+    void messageReady( const Message &msg );
+    void networkProtocolError();
 
-    //signals:
-
-    //public slots:
+//public slots:
 private slots:
     /**
-      * @fn void readData();
-      * @brief Читает блок данных из сокета
+      * @fn void readyRead()
+      * @brief Клиент передает данные, прочтем  и определим что он там шлет
       */
-    void readData();
+    void readyRead();
+    /**
+      * @fn void disconnected();
+      * @brief Клиент покинул, наше общество! Вычеркнем предателя из списка живых
+      */
+    void disconnected();
     /**
       * @fn void prepareError(QLocalSocket::LocalSocketError socketError);
       * @brief Обработчик ошибок возникших в сокете
       */
     void prepareError(QLocalSocket::LocalSocketError socketError);
+
+    void client_init();
+
+protected:
+    /**
+      * @fn void incomingConnection(quintptr socketDescriptor);
+      * @brief Как только происходит новое подключение к локальному серверу,
+      * происходит вызов этой функции, и создание нового Локального сокета
+      * работающего с клиентом
+      */
+    //void incomingConnection(quintptr socketDescriptor);
+
 private:
     /**
-      * @var m_serverName; имя локального сервера для общения с внешним миром
-      * @var connState; Статус соединения с локальным сервером
-      * @var socket; Основной рабочий сокет
-      * @var blockSize; Размер передаваемого блока данных
+      * @var m_serverName;     Имя локального сервера для общения с Локальным миром
+      * @var packetSize;       Размер передаваемого блока данных
       * @var currentDataBlock; Текущий полученный блок данных
+      * @var e_state;          Статус Есть ошибка или нет
+      * @var e_info;           Последняя возникшая ошибка
+      * @var clients;          Список подключенных клиентов
+      * @var clients_name;     Список имен подключенных клиентов
       */
-    bool connState;
-    QString m_serverName;
-    QLocalSocket *socket;
-    //QLocalServer *server;
-    QByteArray currentDataBlock;
-    quint16 blockSize;
 
-    //-----------------------------------------------------------------------------
+    QString m_serverName;    
+    QLocalServer *m_server;
+    qint32 packetSize;
+    bool e_state;
+    QString e_info;
+    QSet<QLocalSocket *> clients;
+    QMap<QLocalSocket *,QString> clients_uuid;
+//-----------------------------------------------------------------------------
     /**
-      * @fn void sendCommand(const QString &cmd);
-      * @brief Передача локальному серверу команды
+      * @fn void parseMessage( const Message &msg, const QLocalSocket *client);
+      * @brief Обработка сообщения от конкретного клиента
       */
-    void sendCommand(const QString &cmd);
+    void parseMessage( const Message &m_msg, QLocalSocket *client);
+
     /**
-      * @fn bool isLocalServerExist();
-      * @brief возвращает статус работы, с локальным сервером или с плагинами
+      * @fn void sendMessage( const Message &msg, const QLocalSocket *client);
+      * @brief Отправка сообщения конкретному клиенту
       */
-    bool isLocalServerExist(){return connState;};
+    void sendMessage( const Message &m_msg, QLocalSocket *client);
+
+    /**
+      * @fn setError(const QString &info);
+      * @brief Устанавливает состояние ошибки,  записывает новое описание ошибки
+      */
+    void setError(const QString &info);
+
 };
 
 #endif // SERVERGEARS_H
