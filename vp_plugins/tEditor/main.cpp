6e82cfa062b27ef510402abf1ff10391ec3a92b6
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-16 16:05:01 +0000

    Шаблон для Doxygen

--- vp_plugins/tEditor/main.cpp
+++ vp_plugins/tEditor/main.cpp
@@ -3,6 +3,46 @@
 #include <QTextCodec>
 #include <QStringList>
 
+/*! \mainpage OMSp_MBT_ComposTest
+ *
+ * \section intro_sec Introduction
+ *
+ * <b>Developer</b>:
+ * - ���
+ *
+ * \section requirements Requirements
+ * The application OMSp_MBT_ComposTest.dll requires the following external stuff:
+ * - lib1
+ * - dll1
+ *
+ * \section changelog Changelog
+ *
+ * - February 2009: initial release
+ *
+ * \section About_sec About this documentation
+ *
+ * \subsection step1 Location
+ *  
+ * This documentation can be found on file://dir1/dir2/index.html
+ * 
+ * \subsection step2 Updating
+ * 
+ * <b>Requirements</b>
+ * - Installed <b><i>doxygen</i></b> package (from http://www.stack.nl/~dimitri/doxygen)
+ * - Optional: Installed <b><i>graphviz</i></b> tool for diagrams (from http://www.graphviz.org) 
+ *   <br><sup><b>Note:</b> you have add the <b>bin</b> path of the installation to the 
+ *   <tt>PATH</tt> environment variable</sup>
+ *
+ * <b>Generating</b>
+ *
+ * - go to source location <b> dir1\project_dir </b>
+ * - open a cmd window
+ * - type <tt>doxygen tEditor.doxygen </tt>
+ *
+ * <b>Configuration files</b>
+ * - <tt> tEditor.doxygen </tt> (text file; general configuration of input files and output)
+ * - <tt> main.cpp</tt> (text file for processing this Main Page)
+ */
 
 #include "config.h"
 //#include "tech_global.h"
@@ -28,7 +68,7 @@ int main(int argc, char *argv[])
      * - sizeOf (argv[]) = 2 (имя_файла_шаблона) редактирование выбранного шаблона
     */
     QString file_name;
-    QString file_name_dat;
+
     MainWindow window;
     window.show();
 
