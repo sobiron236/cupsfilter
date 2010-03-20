8d13fd0af9ac0f997d91fcf5c7bebdfc0f6e3e21
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-20 11:19:18 +0000

    Реализация: @fn void messageReady( const Message &msg ); @brief Как только полученно полноценное сообщение из сети оно в формате Message передается основному приложению
    @fn void stateChanged(socketState state);
    @brief Испукает сигнал при смене состоянии
    
    @fn void readyRead();
    @brief  При получении пакета идет анализ на предмет наличия в пакете сообщения
    
    @fn void onConnected();
    @brief При соединении с сервером происходит авторизация
    
    @fn selectError(QAbstractSocket::SocketError err);
    @brief Обработчик ошибок возникших в сокете
    
    @fn setError(const QString &info);
    @brief Устанавливает состояние ошибки,  записывает новое описание ошибки
    
    @fn void setState(socketState state);
    @brief Меняет состояние и испускает соответсвующий сигнал

--- vp_plugins/net_plugin/net_plugin.cpp
+++ vp_plugins/net_plugin/net_plugin.cpp
@@ -5,32 +5,25 @@
 
 
 net_plugin::net_plugin(QObject *parent)
+    : QObject(parent)
+    , HostName(QString())
+    , Port (0)
+    , client(0)
+    , packetSize(-1)
+    , Sid(QString())
+    , m_state(VPrn::InitClientState)
+    , e_info(QString())
 {
 
 }
 
-void net_plugin::onConnected()
-{
-    QString message =QString("/me;:;%1;:;%2").arg(Sid).arg(REGISTER_CMD,0,10);
-    client->write(QString(message+"\n").toUtf8());
-
-}
-void net_plugin::readyRead()
-{
-    qDebug() << "readyRead";
-    while (client->canReadLine()) {
-        QString line = QString::fromUtf8(client->readLine()).trimmed();
-        qDebug() << "Read line and send to app:" << line;
-        emit serverResponse(line);
-    }
-
-}
-
+//---------------------------- PUBLIC ------------------------------------------
 void net_plugin::init(const QString &host, int port,const QString &sid)
 {
     this->HostName = host;
     this->Port = port;
-    this->Sid=sid;
+    this->Sid = sid;
+
     client = new QTcpSocket(this);
     connect(client, SIGNAL(readyRead()),
             this, SLOT(readyRead()));
@@ -38,7 +31,98 @@ void net_plugin::init(const QString &host, int port,const QString &sid)
             this, SLOT(onConnected()));
     connect(client, SIGNAL(error(QAbstractSocket::SocketError)),
             this, SLOT(selectError(QAbstractSocket::SocketError)));
+
     client->connectToHost(HostName, Port);
+/*
+    Для отладки    	
+*/
+    Message message( this );
+    message.setType(Ans_RegisterGlobal);
+    //message.setMessage( Sid.toUtf8() ); // Пробразуем в QByteArray
+    emit messageReady(message);
+}
+
+void net_plugin::sendMessage(const Message &m_msg)
+{
+    //Сформируем пакет И пошлем его ветром гонимого
+    client->write(m_msg.createPacket());
+    client->flush();
+}
+
+socketState net_plugin::state() const
+{
+    return m_state;
+}
+
+//---------------------------- PRIVATE -----------------------------------------
+ void net_plugin::setError(const QString &info)
+ {
+     e_info = info;
+ }
+
+ void net_plugin::setState(socketState state)
+ {
+     m_state = state;
+     emit stateChanged(m_state);
+ }
+
+//---------------------------- PRIVATE SLOTS -----------------------------------
+void net_plugin::readyRead()
+{
+    //Свяжем поток и сокет
+    QDataStream in ( client );
+    in.setVersion(QDataStream::Qt_3_0);
+
+    while (client->bytesAvailable() > 0){
+        if (packetSize == -1) {
+            //Определим количество байт доступных для чтения;
+            //на этом шаге необходимо получить больше 4-х байт
+            if( client->bytesAvailable() < sizeof(qint32) ){
+                return;
+            }
+            //Читаем размер пакета
+            in >> packetSize;
+        }
+        //Проверим что в сокет пришел весь пакет а не его огрызки
+        if (client->bytesAvailable() < packetSize){
+            return;
+        }
+        //Сбросим размер пакета, для обработки следующего
+        packetSize = -1;
+
+        //Прочтем и проверим формат протокола
+        qint32 m_format;
+        in >> m_format;
+        if( m_format != format ) {
+            setError(tr("Ошибка в формате протокола, при обмене данными с сервером %1")
+                     .arg(HostName));
+            return;
+        }
+
+        // Прочтем тип сообщения
+        int m_Type;
+        in >> m_Type;
+
+        //Прочтем само сообщение
+        QByteArray msg;
+        in >> msg;
+        Message message( this );
+        message.setType((MessageType) m_Type); //Проверить как конвертирует
+        message.setMessage( msg );
+
+        // Отправка сообщения
+        emit messageReady(message);
+    }
+}
+
+void net_plugin::onConnected()
+{
+    //QString m_body =QString("/me;:;%1;:;%2").arg(Sid).arg(REGISTER_CMD,0,10);
+/// @todo k МИШЕ УСТРАНИТЬ ДУБЛИРОВАНИЕ ИНФОРМАЦИИ
+    Message message( this );
+    message.setType(VPrn::Que_RegisterGlobal);
+    message.setMessage( Sid.toUtf8() ); // Пробразуем в QByteArray
+    sendMessage(message);
 }
 
 void net_plugin::selectError(QAbstractSocket::SocketError err)
@@ -47,28 +131,23 @@ void net_plugin::selectError(QAbstractSocket::SocketError err)
     switch(err)
     {
     case QAbstractSocket::ConnectionRefusedError :
-        e_msg =QObject::trUtf8("Cоединение не выполнено | connection refused");
+        e_msg =QObject::trUtf8("Соединение отклоненно удаленным сервером [%1]")
+                .arg(HostName);
         break;
     case QAbstractSocket::HostNotFoundError :
-        e_msg =QObject::trUtf8("Удаленный сервер не найден | host not found");
+        e_msg =QObject::trUtf8("Удаленный сервер не найден!")
+               .arg(HostName);
         break;
     case QAbstractSocket::SocketTimeoutError :
-        e_msg =QObject::trUtf8("Превышено время ожидания ответа от сервера | The socket operation timed out.");
+        e_msg =QObject::trUtf8("Превышено время ожидания ответа от сервера")
+               .arg(HostName);
         break;
     default:
-        e_msg = QObject::trUtf8("Error code %1").arg(err,0,10);
+        e_msg = QObject::trUtf8("Код ошибки %1").arg(err,0,10);
     }
     emit error(VPrn::NetworkTransError,e_msg);
+    setState(VPrn::InternalError);
 }
 
-void net_plugin::sendData(const QString &cmd)
-{
-    qDebug()<< Q_FUNC_INFO << cmd << client->state();
+Q_EXPORT_PLUGIN2(Inet_plugin, net_plugin);
 
-    if (!cmd.isEmpty()) {
-        client->write(QString(cmd+"\n").toUtf8());
-    }
-}
-
-Q_EXPORT_PLUGIN2(Inet_plugin, net_plugin)
-;
--- vp_plugins/net_plugin/net_plugin.h
+++ vp_plugins/net_plugin/net_plugin.h
@@ -8,10 +8,9 @@
 #include "inet_plugin.h"
 
 #include "mytypes.h"
+#include  "message.h"
 
 using namespace VPrn;
-
-
 
 class net_plugin :public QObject, Inet_plugin
 {
@@ -20,27 +19,58 @@ class net_plugin :public QObject, Inet_plugin
 
 public:
     net_plugin(QObject *parent=0);
+    socketState state() const;
     void init(const QString &host,int port,const QString &sid);
-    void sendData(const QString &cmd);
-    //bool state(){return con_state;};
+    void sendMessage(const Message &m_msg);
 
 signals:
-    void serverResponse(QString &line);
     void error(pluginsError errCode,QString error_message);
-
-
-private:
-    QString HostName;
-    quint16 Port;
-    QTcpSocket *client;
-    //bool con_state;
-    QString Sid;
+    /**
+      * @fn void messageReady( const Message &msg );
+      * @brief Как только полученно полноценное сообщение из сети оно в 
+      * формате Message передается основному приложению
+      */
+    void messageReady( const Message &msg );
+    void stateChanged(socketState state);
 
 private slots:
     void readyRead();
     void onConnected();
+    /**
+      * @fn selectError(QAbstractSocket::SocketError err);
+      * @brief Обработчик ошибок возникших в сокете
+      */
     void selectError(QAbstractSocket::SocketError err);
 
+
+private:
+    /**
+      * @var HostName;         Имя хоста для связи
+      * @var Port;             Порт для связи
+      * @var client;           Указатель на клиента
+      * @var packetSize;       Размер передаваемого блока данных
+      * @var Sid;              Уникальный номер      
+      * @var m_state;          Статус Локального сервера
+      * @var e_info;           Последняя возникшая ошибка
+      */
+
+    QString HostName;
+    quint16 Port;
+    QTcpSocket *client;  
+    qint32 packetSize;
+    QString Sid;
+    socketState m_state;
+    QString e_info;
+    /**
+      * @fn setError(const QString &info);
+      * @brief Устанавливает состояние ошибки,  записывает новое описание ошибки
+      */
+    void setError(const QString &info);
+    /**
+      * @fn void setState(socketState state);
+      * @brief Меняет состояние и испускает соответсвующий сигнал
+      */
+    void setState(socketState state);
 };
 
 
