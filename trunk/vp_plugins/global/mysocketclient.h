b37ba2250b3172d1ae88648c45bc98dbc361e3a4
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-22 14:09:57 +0000

    Клиент локального сервера

---
+++ trunk/vp_plugins/global/mysocketclient.h
@@ -0,0 +1,66 @@
+#ifndef MYSOCKETCLIENT_H
+#define MYSOCKETCLIENT_H
+/**
+  * @class Базовый клиент сокетный клиент, является базовым классом для реализации
+  * в последующем специализированных клиентов наследующих от \sa QLocalSocket
+  * или от \sa QTcpSocket. Клиенты реализуют абстрактые функции:
+  * \fn onConnect()    реализующую обработку сигнала connected
+  * \fn parseMessage() реализующую обработку полностью полученного сообщения
+  * типа \sa \class Message.
+  */
+#include <QObject>
+
+#include <QtNetwork/QLocalSocket>
+#include "message.h"
+
+#include "mytypes.h"
+using namespace VPrn;
+
+
+class mySocketClient : public QLocalSocket
+{
+    Q_OBJECT
+public:
+
+
+    explicit mySocketClient(QLocalSocket *parent = 0);
+    QString lastError() const {return e_info;};    
+    void sendMessage(const Message &msg);
+
+signals:
+    void messageReady( const Message &msg );
+    void checkPointChanged(MyCheckPoints state);
+
+private slots:
+    /**
+      * @fn void readyRead()
+      * @brief Сервер передает данные, прочтем, преобразуем в сообщение Message
+      */
+    void readyRead();
+    void do_connected();
+    void do_error(QLocalSocket::LocalSocketError r_error);
+private:
+    /**
+      * @var packetSize; Размер передаваемого блока данных
+      * @var m_state;    Статус клиента
+      * @var e_info;     Последняя возникшая ошибка
+      * @var client;     Указательна клиента
+      */
+
+    qint32 packetSize;
+    MyCheckPoints m_chekpoints;
+    QString e_info;
+    /**
+      * @fn setError(const QString &info);
+      * @brief Устанавливает состояние ошибки,  записывает новое описание ошибки
+      */
+    void setError(const QString &info);
+    /**
+      * @fn void setMyState(socketState state);
+      * @brief Меняет состояние и испускает соответсвующий сигнал
+      */
+    void setCheckPoint(MyCheckPoints m_cpoint);
+};
+
+
+#endif // MYSOCKETCLIENT_H
