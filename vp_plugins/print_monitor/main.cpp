028f539892bae3b77cd29f0467e36437a04697c3
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-28 18:53:19 +0000

    При сатрте приложения происходит проверка нет ли запущеннго приложения, если есть то в его очередь печати добавляется новый файл.
    После того как пользователь заканчивает работу с текущим документом Происходит проверка очереди, нет ли там ожидающих документов,если есть, то весь цикл запускается заново, но с шага 2 так как логин, мандат уже есть, связь с локальным сервером установленна, единственная проверка которая необходима, эта связь с мишиным демоном, не отвалилась ли

--- vp_plugins/print_monitor/main.cpp
+++ vp_plugins/print_monitor/main.cpp
@@ -3,6 +3,7 @@
 #include <QtCore/QTextCodec>
 #include <QtCore/QTranslator>
 #include <QtCore/QLibraryInfo>
+#include <QtGui/QMessageBox>
 
 #include "config.h"
 #include "mytypes.h"
@@ -22,21 +23,46 @@ int main(int argc, char *argv[])
 
     QtSingleApplication app(argc, argv, true);
 
-    if (app.sendMessage(QObject::trUtf8("Потребован повторный запуск приложения!"))
-        || app.isRunning())
-        return 0;
+    QString in_file;
+#if defined (MY_DEBUG)
+    in_file="test.ps";
+#else
+    QStringList aList=app.arguments();
+    if (aList.size()< 2){
+        QMessageBox::critical(0,QObject::trUtf8("Обратитесь к системному администратору"),
+                              QObject::trUtf8("Ошибка запуска приложения, не достаточно параметров запуска"));
+        return 1;
+    }
+    QString in_file=aList.at(1);
+#endif
+    /**
+      @todo На базе сообщения организовать очередь печати !
+      * После того как пользователь заканчивает работу с текущим документом
+      * Происходит проверка очереди, нет ли там ожидающих документов,
+      * если есть, то весь цикл запускается заново, но с шага 2 так как
+      * логин, мандат уже есть, связь с локальным сервером установленна,
+      * единственная проверка которая необходима, эта связь с мишиным демоном,
+      * не отвалилась ли
+      */
 
+    if (app.sendMessage(in_file)
+        || app.isRunning())
+        return 0;        
     QString translatorFileName = QLatin1String("qt_");
     translatorFileName += QLocale::system().name();
     QTranslator *translator = new QTranslator(&app);
-    if (translator->load(translatorFileName, QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+    if (translator->load(translatorFileName,
+                         QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
         app.installTranslator(translator);
 
-    PrintMonitor monitor;
-    app.setActivationWindow(&monitor);
+    PrintMonitor monitor;    
 
     QObject::connect(&app, SIGNAL(messageReceived(const QString&)),
-		     &monitor, SLOT(appendStartMsg(const QString&)));
+                     &monitor, SLOT(appendStartMsg(const QString&)));
+
+    app.setActivationWindow(&monitor);    
+    monitor.setFile4Work(in_file);
+    app.processEvents();
     monitor.show();
     return app.exec();
 }
