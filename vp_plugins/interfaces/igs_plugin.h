f4ba86949417b3bd1e59b289383411e0b9b3b15a
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-28 18:43:22 +0000

    Текущая версия интерфейса gs_plugin

--- vp_plugins/interfaces/igs_plugin.h
+++ vp_plugins/interfaces/igs_plugin.h
@@ -1,32 +1,22 @@
 #ifndef IGS_PLUGIN_H
 #define IGS_PLUGIN_H
+#include "mytypes.h"
 
+using namespace VPrn;
 class QString;
-class QStringList;
-class QPixmap;
 
 
 class Igs_plugin{
 
 public:
     virtual ~Igs_plugin() {}
-    virtual bool init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &gs_rcp_file,const QString &sid)=0;
-    virtual QString getFirstPages()=0;
-    virtual QString getOtherPages()=0;
-    virtual QPixmap getSnapShot()=0;
-    virtual void convertPs2Pdf(const QString &input_fn)=0;
-    virtual void getPageCount(const QString &input_fn)=0;
-    virtual void convertPdf2Png(const QString &fn, int convertedPage )=0;
-    virtual void merge2Pdf(const QString &input_fn,const QString &background_fn,const QString &output_fn)=0;
-    virtual void printPdf(const QString &print_fn,const QString &printer_name)=0;
-    virtual void addPdfMark(const QString &input_fn, const QString &user_name, const QString &host_name,quint16 host_ip)=0;
-    virtual void clearAll()=0;
-    virtual void merge_mark_print(const QString &input_fn,const QString &background_fn,const QString &user_name,const QString &printer_name)=0;
+    virtual void init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &sid)=0;
+    virtual void convertPs2Pdf(const QString &client_uuid,const QString &input_fn) = 0;
 };
 
 
 QT_BEGIN_NAMESPACE
-Q_DECLARE_INTERFACE(Igs_plugin,"com.technoserv.Plugin.Igs_plugin/1.0");
+Q_DECLARE_INTERFACE(Igs_plugin,"com.technoserv.Plugin.Igs_plugin/2.0");
 QT_END_NAMESPACE
 
 
