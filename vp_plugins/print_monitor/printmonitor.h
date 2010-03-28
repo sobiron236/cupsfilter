8eb829888a3e4766a716db7d4c409d647f150917
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-28 18:58:31 +0000

    public slots: @fn void setFile4Work(const QString &input_file); @brief На вход виртуального принтера поступает файл в формате ps После проверки на валидность, т.е файл есть и доступен, он помещается в очередь печати, которая обрабатывается на GateKeeper
    void showHelp(); Ну очень краткий help
    Печать следующего в очереди
    void print_next();

--- vp_plugins/print_monitor/printmonitor.h
+++ vp_plugins/print_monitor/printmonitor.h
@@ -9,6 +9,7 @@
 
 #include <QtGui/QWizard>
 #include <QtCore/QMap>
+#include <QtCore/QQueue>
 
 using namespace VPrn;
 
@@ -18,6 +19,7 @@ using namespace VPrn;
 //}
 
 class QPoint;
+class QErrorMessage;
 
 class PrintMonitor : public QWizard {
     Q_OBJECT
@@ -26,12 +28,17 @@ public:
     PrintMonitor(QWidget *parent = 0);
     ~PrintMonitor();
 
-//protected:
-//    void changeEvent(QEvent *e);
-//    int nextId() const;
+
+    int nextId() const;
+public slots:   
+    /**
+      * @fn void setFile4Work(const QString &input_file);
+      * @brief На вход виртуального принтера поступает файл в формате ps
+      * После проверки на валидность, т.е файл есть и доступен, он помещается в очередь
+      * печати, которая обрабатывается на GateKeeper
+      */
+    void setFile4Work(const QString &input_file);
 private slots:
-    void showInfoMessage(const QString &info);
-    void showCritMessage(const QString &info);
     /**
       * @fn void do_needAuthUser(const QString &login_mandat_list);
       * @param QString &login_mandat_list сообщение в формате:
@@ -40,27 +47,37 @@ private slots:
       * прислал логин и набор мандатов, просит выбрать.
       */
     void do_needAuthUser(const QString &login_mandat_list);
-    /**
-      * @fn void setPrinterList (const QString &pList);
-      * @brief Сервер вернул список доступных принтеров, отформатируем его
-      * по канонам дзенбуддизма и сделаем доступным для приложения
-      */
-    void setPrinterList (const QString &pList);
-
     void showHelp();
-private:
-//    Ui::PrintMonitor *ui;
     /**
-      * @var core_app Основной объект программы
+      * @fn void print_next();
+      * @brief отправляет на обработку следующий файл из очереди печати
       */
-    Engine   *core_app;
-    getUserNameMandatDlg *UMDlg;
-    QStringList printer_list;
-    QMap <QString,QString> printer_device_list;
+    void print_next();
+    //protected:
+    //    void changeEvent(QEvent *e);
+private:
+    //    Ui::PrintMonitor *ui;
+    /**
+      * @var core_app;      Основной объект программы
+      * @var UMDlg;         Диалоговое окно выбора мандата
+      * @var m_InputFile;   Основной обрабатываемый файл
+      * @var intro_page;    Стартовая страница
+      * @var select_page;   Страница выбора режимов работы
+      * @var printData_page; Страница заполнения карточки документа
+      * @var job_list;       Очередь документов на печать,
+        возникает если была попытка запуска еще одной копии приложения
+      */
+    Engine               * core_app;
+    getUserNameMandatDlg * UMDlg;
+    QString                m_InputFile;
+    IntroPage            * intro_page;
+    SelectPage           * select_page;
+    PrintDataPage        * printData_page;
+    QErrorMessage        * eMsgBox;
+    QQueue <QString>       job_list;
 
+    //-------------------------------------------------------------------------
     QPoint getDeskTopCenter(int width,int height);
-    IntroPage * intro_page;
-    SelectPage * select_page;
 };
 
 
