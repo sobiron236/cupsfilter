3e6d985a74f3448dfbcbe2d81e9a6d32b482ce55
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-03-28 18:47:20 +0000

    В конструкторе  ProcessT( QObject *parent = 0 ,               VPrn::Jobs job_id = VPrn::job_NoJob, <--- Какую работу               const QString &client = QString());  <--- В чьих интересах
    При завершении работы испускается сигнал
     void jobFinish(const QString & client,VPrn::Jobs job_id,int Code,const QString &output);

--- vp_plugins/gs_plugin/proc_thread.cpp
+++ vp_plugins/gs_plugin/proc_thread.cpp
@@ -6,77 +6,83 @@
 
 
 
-ProcessT::ProcessT( QObject *parent ) :
-        QThread( parent )
+ProcessT::ProcessT( QObject *parent,VPrn::Jobs job_id,const QString &client)
+    : QThread( parent )
 {
-        m_ChanMode = QProcess::SeparateChannels;
+    m_JobId    = job_id;
+    client_id  = client;
+    m_ChanMode = QProcess::SeparateChannels;
 }
 
-ProcessT::~ProcessT() {
-
+ProcessT::~ProcessT()
+{
     wait();
     qDebug() << Q_FUNC_INFO << "Thread destroyed";
 }
 
-void ProcessT::run() {
+void ProcessT::run()
+{
+    if ( m_Command.isEmpty() ) {
+        qWarning() << Q_FUNC_INFO << "No command set, doing nothing";
+        return;
+    }
 
+    qDebug() << Q_FUNC_INFO << "Executing command" << m_Command << m_Args << "\n";
 
-        if ( m_Command.isEmpty() ) {
-                qWarning() << Q_FUNC_INFO << "No command set, doing nothing";
-                return;
-        }
+    QProcess proc;
+    // Add  to the environment
+    if (!n_Env.isEmpty()){
+        proc.setEnvironment(n_Env);
+    }
 
-        qDebug() << Q_FUNC_INFO << "Executing command" << m_Command << m_Args << "\n";
+    proc.setProcessChannelMode( m_ChanMode );
+    proc.start( m_Command, m_Args );
+    if (!proc.waitForStarted()) {
+        m_Output =QString("Ошибка при запуске приложения %1").arg(m_Command);
+        emit jobFinish(client_id,m_JobId,-1, m_Output );
+        qDebug()<< m_Output;
+    }else{
+        proc.waitForFinished(-1);
+        proc.closeWriteChannel();
+        m_Output = proc.readAll().trimmed();
+        qDebug() << QString("Exit code %1").arg(proc.exitCode());
 
-        QProcess proc;
-        // Add  to the environment
-        if (!n_Env.isEmpty()){
-           proc.setEnvironment(n_Env);
-        }
-
-        proc.setProcessChannelMode( m_ChanMode );
-        proc.start( m_Command, m_Args );
-        if (!proc.waitForStarted()) {
-            qDebug()<< QString("Unable to launch application %1").arg(m_Command);
-        }else{
-            proc.waitForFinished(-1);
-            proc.closeWriteChannel();
-            m_Output = proc.readAll().trimmed();
-            qDebug() << QString("Exit code %1").arg(proc.exitCode());
-
-            emit commandOutput(proc.exitCode(), m_Output );
-            qDebug() << Q_FUNC_INFO << "Command execution finished.\n" <<m_Output;
-        }
+        //emit commandOutput(proc.exitCode(), m_Output );
+        emit jobFinish(client_id,m_JobId,proc.exitCode(), m_Output );
+        qDebug() << Q_FUNC_INFO << "Command execution finished,"<<m_Output
+                << " for job " << m_JobId;
+    }
 }
 
 void ProcessT::addToEnv(const QStringList & pairs_list )
 {
-  n_Env = QProcess::systemEnvironment();
-  n_Env << pairs_list;
+    n_Env = QProcess::systemEnvironment();
+    n_Env << pairs_list;
 }
 
-void ProcessT::setCommand( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode ) {
-
-        if ( name.isEmpty() ) {
-                qWarning() << Q_FUNC_INFO << "Empty command given, doing nothing";
-                return;
-        }
-        m_Command = name;
-        m_Args = args;
-        m_ChanMode = mode;
+void ProcessT::setCommand( const QString &name, const QStringList &args,
+                           const QProcess::ProcessChannelMode &mode )
+{
+    if ( name.isEmpty() ) {
+        qWarning() << Q_FUNC_INFO << "Empty command given, doing nothing";
+        return;
+    }
+    m_Command = name;
+    m_Args = args;
+    m_ChanMode = mode;
 }
 
-void ProcessT::execute( const QString &name, const QStringList &args, const QProcess::ProcessChannelMode &mode ) {
+void ProcessT::execute( const QString &name, const QStringList &args,
+                        const QProcess::ProcessChannelMode &mode )
+{
+    setCommand( name, args, mode );
 
-        setCommand( name, args, mode );
-
-        if ( !isRunning() ) {
-                start();
-        }
-        else {
-                qWarning() << Q_FUNC_INFO << "Thread already running, doing nothing.";
-                return;
-        }
-
+    if ( !isRunning() ) {
+        start();
+    }
+    else {
+        qWarning() << Q_FUNC_INFO << "Thread already running, doing nothing.";
+        return;
+    }
 }
 
