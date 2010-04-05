c66ccd67107ffbe7695c578a1272f8fef907c95e
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-04-05 02:33:33 +0000

    Добавил конструктор копирования

--- trunk/vp_plugins/global/mytextitem.h
+++ trunk/vp_plugins/global/mytextitem.h
@@ -4,13 +4,28 @@
 #include <QGraphicsTextItem>
 #include <QtGlobal>
 
+#include "mytypes.h"
+using namespace VPrn;
+
+QT_FORWARD_DECLARE_CLASS(QString)
+QT_FORWARD_DECLARE_CLASS(QPointF)
+QT_FORWARD_DECLARE_CLASS(QFont)
+QT_FORWARD_DECLARE_CLASS(QColor)
+
+
 class myTextItem : public QGraphicsTextItem
 {
 public:
     myTextItem (QGraphicsItem * parent = 0 );
-    QString getETag()   const {return m_tag;};
-    QString getEText()  const {return m_text;};
-    qreal   getAngle()  const {return m_Angle;};
+
+    myTextItem (const QString &tag, const QString &text,
+                const QPointF &ps,  const QFont &fnt,
+                const QColor &col,  bool v_mode);
+
+
+    QString getETag()   const {return m_tag;}
+    QString getEText()  const {return m_text;}
+    qreal   getAngle()  const {return m_Angle;}
 
     void    setMode  (bool tagTextMode);
     void    setETag  (const QString &t);
@@ -20,8 +35,11 @@ public:
 private:
     QString m_tag;
     QString m_text;
-    bool    v_mode; //Режим отображения Tag/Text
-    qreal m_Angle;
+    bool    m_mode; //Р РµР¶РёРј РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ Tag/Text
+    qreal   m_Angle;
 };
+
+
+
 
 #endif // MYTEXTITEM_H
