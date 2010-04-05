5777442903d8c2c05b1e6d0184ef9e9758cc1f74
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-04-05 02:14:32 +0000

    Измененния в интерфейсах

--- trunk/vp_plugins/interfaces/igs_plugin.h
+++ trunk/vp_plugins/interfaces/igs_plugin.h
@@ -3,15 +3,21 @@
 #include "mytypes.h"
 
 using namespace VPrn;
-class QString;
 
+QT_FORWARD_DECLARE_CLASS(QString)
+QT_FORWARD_DECLARE_CLASS(QByteArray)
 
 class Igs_plugin{
 
 public:
     virtual ~Igs_plugin() {}
-    virtual void init(const QString &gs_bin,const QString &pdftk_bin,const QString &temp_folder,const QString &sid)=0;
-    virtual void convertPs2Pdf(const QString &client_uuid,const QString &input_fn) = 0;
+    virtual void init(const QString &gs_bin,const QString &pdftk_bin,
+                      const QString &temp_folder)=0;
+    virtual void convertPs2Pdf(const QString &client_uuid,
+                               const QString &input_fn) = 0;
+    virtual void final_clear() = 0;
+//    virtual void createFormatedDoc(const QString &client_uuid,
+//                                   const QByteArray r_data) =0;
 };
 
 
