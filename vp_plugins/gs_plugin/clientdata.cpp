efd1a3d934a200060f6c334d26d04215c2c0cf2a
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-05-25 19:02:45 +0000

    Класс описывающие задание каждого клиента GateKeeper Файл отправленный на печать проходит через различные стадии, ключевые из которых (требуемые на следующих этапах работы) сохраняются в полях класса, по сути дела это просто хранилище атрибутов каждого задания

---
+++ vp_plugins/gs_plugin/clientdata.cpp
@@ -0,0 +1,94 @@
+#include "clientdata.h"
+
+ClientData::ClientData(QObject *parent)
+    : QObject(parent)
+    , fullDocConvert( false )
+    , main_file  ( QString() )
+    , first_page ( QString() )
+    , other_page ( QString() )
+    , page_count(0)
+{
+
+}
+
+void ClientData::clear()
+{
+    fullDocConvert =false;
+    first_page.clear();
+    other_page.clear();
+    page_count = 0;
+    metaJob_merge.clear();
+}
+
+void ClientData::setConvertMode(bool b)
+{
+    fullDocConvert = b;
+}
+
+bool ClientData::isFullDocConvert()
+{
+    return fullDocConvert;
+}
+
+
+void ClientData::setMainFile (const QString &m)
+{
+    main_file = m;
+}
+
+QString ClientData::getMainFile() const
+{
+    return main_file;
+}
+
+void ClientData::setFirstPage (const QString &fp)
+{
+    first_page = fp;
+}
+
+QString ClientData::getFirstPage() const
+{
+    return first_page;
+}
+
+void ClientData::setOtherPage (const QString &op)
+{
+    other_page = op;
+}
+
+QString ClientData::getOtherPage() const
+{
+    return other_page;
+}
+
+void ClientData::setPageCount(int p)
+{
+    page_count = p;
+}
+
+int ClientData::getPageCount()
+{
+    return page_count;
+}
+
+void ClientData::startMerge(int number)
+{
+    metaJob_merge.push(number);
+}
+
+bool ClientData::isFinishedMerge()
+{
+    metaJob_merge.pop();
+    return metaJob_merge.isEmpty();
+}
+
+void  ClientData::startConverPdf2Png(int number)
+{
+    metaJob_pdf2png.push(number);
+}
+
+bool ClientData::isFinishedConvertPdf2Png()
+{
+    metaJob_pdf2png.pop();
+    return metaJob_pdf2png.isEmpty();
+}
---
+++ vp_plugins/gs_plugin/clientdata.h
@@ -0,0 +1,57 @@
+#ifndef CLIENTDATA_H
+#define CLIENTDATA_H
+
+#include <QObject>
+#include <QtCore/QStack>
+
+QT_FORWARD_DECLARE_CLASS(QStringList)
+
+class ClientData : public QObject
+{
+Q_OBJECT
+public:
+
+    explicit ClientData(QObject *parent = 0 );
+    void clear();
+
+    void setConvertMode(bool b);
+    bool isFullDocConvert();
+
+    void setMainFile (const QString &m);
+    QString getMainFile() const;
+
+    void setFirstPage (const QString &fp);
+    QString getFirstPage() const;
+
+    void setOtherPage (const QString &op);
+    QString getOtherPage() const;
+
+    void setPageCount(int p );
+    int getPageCount();
+
+    void startMerge(int number);
+    bool isFinishedMerge();
+
+    void startConverPdf2Png(int number);
+    bool isFinishedConvertPdf2Png();
+
+private:
+    bool fullDocConvert;
+
+    QString main_file;
+    QString first_page;
+    QString other_page;
+    int page_count;
+    /**
+      * @var clients_MetaJobMerge;
+      * @brief  Для каждого файла который будет объдиняться формируется запись в этом стеке
+      * после успешного выполнения команды merge  одна запись из стека удаляется,
+      * соответственно когда стек пуст, это служит сигналом об окончании  процесса
+      * наложения шаблона на документ
+      */
+    QStack <int> metaJob_merge;
+
+    QStack <int> metaJob_pdf2png;
+};
+
+#endif // CLIENTDATA_H
