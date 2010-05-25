8669eeef7bd5eca73f0d8a729d93b3e3b4599222
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-05-25 19:05:28 +0000

    @fn void print2devide (const QString &client_uuid,  const QString &print_file,                                             const QString &prn_device,bool usePageCount); @brief Печать  документа сформированного клиентом на устройство
    @fn  QString getUuid() const
    @brief служебная функция генерирует уникальный ключ
    
    @fn void mergeWithTemplate(const QString &client_uuid,const QStringList &t_files);
    @brief Объединяет исходный файл состоящий из 2х частей с элементами шаблона для каждого экз документа
    
    @fn void convertPdfToPng(const QString &client_uuid,const QString &filename, bool fullFile);
    @brief Преобразует исходный файл в набор картинок
    
    @fn void createClientData(const QString &client_uuid);
    @brief Создает новый экз. хранилища данных для клиента
    
    @fn void deleteClientData(const QString &client_uuid);
    @brief удаляет экз. хранилища данных для клиента

--- trunk/vp_plugins/gs_plugin/gs_plugin.cpp
+++ trunk/vp_plugins/gs_plugin/gs_plugin.cpp
@@ -1,5 +1,6 @@
 #include "gs_plugin.h"
 #include "proc_thread.h"
+#include "clientdata.h"
 
 #include <QtCore/QDebug>
 #include <QtCore/QString>
@@ -7,11 +8,11 @@
 #include <QtCore/QFile>
 #include <QtCore/QFileInfo>
 #include <QtCore/QDir>
+#include <QtCore/QFileInfoList>
 #include <QtCore/QRegExp>
 #include <QtCore/QTemporaryFile>
 #include <QtCore/QTextStream>
 #include <QtCore/QDateTime>
-//#include <QtCore/QMetaType>
 #include <QtCore/QUuid>
 
 #include <QtGui/QPixmap>
@@ -47,7 +48,7 @@ void GS_plugin::init(const QString &gs_bin, const QString &pdftk_bin,
 
             Ok &= dir.cd(temp_folder) && !temp_folder.isEmpty();
             if (Ok){// Проверим факт существования временного каталога
-                tempPath = temp_folder;
+                spoolDir = temp_folder;
                 myEnv << QObject::trUtf8("TMPDIR=%1").arg(temp_folder);
 
                 Ok &=QFile::exists(pdftk_bin);
@@ -95,15 +96,19 @@ void GS_plugin::convertPs2Pdf(const QString &client_uuid,const QString &input_fn
     // Проверим фактик наличия входного файла
     qDebug() << Q_FUNC_INFO << input_fn;
     if (QFile::exists(input_fn)) {
+        // Поиск данных для клиента
+        ClientData *c_data = findClientData(client_uuid);
+
         // Формируем основной выходной файл для клиента
-        QString mainPDF = QObject::trUtf8("%1/%2_main.pdf")
-                          .arg( tempPath, client_uuid );
+        c_data->setMainFile(QString("%1/%2/main.pdf")
+                            .arg( spoolDir, client_uuid ) );
 
         // Формируем строку команды для первого этапа
-        args.clear();
+        QStringList args;
+
         args.append("-q" );
         args.append(QString("@%1").arg(gs_rcp));
-        args.append(QString("-sOutputFile=%1").arg(mainPDF));
+        args.append(QString("-sOutputFile=%1").arg(c_data->getMainFile()));
         args.append("-c");
         args.append(".setpdfwrite");
         args.append("-f");
@@ -116,7 +121,196 @@ void GS_plugin::convertPs2Pdf(const QString &client_uuid,const QString &input_fn
     }
 }
 
+void GS_plugin::print2devide (const QString &client_uuid,  const QString &print_file,
+                              const QString &prn_device,bool usePageCount)
+{
+    //-q -dQUIET -dNOPAUSE -dPARANOIDSAFER -dBATCH -r300 -sDEVICE=pdfwrite -sOutputFile="%printer%pdfcreator" -c .setpdfwrite -f %1
+    // Поиск данных для клиента
+    ClientData *c_data = findClientData(client_uuid);
+    int print_copy;
+    if (usePageCount){
+        print_copy = c_data->getPageCount() -1;
+    }else{
+        print_copy = 1;
+    }
+    QStringList args;
+    for (int i=0;i<print_copy;i++){
+        args.clear();
+        args.append("-q");
+        args.append("-dQUIET");
+        args.append("-dNOPAUSE");
+        args.append("-dBATCH");
+        args.append("-dPARANOIDSAFER");
+        args.append("-r600");
+        args.append("-sDEVICE=pdfwrite");
+        args.append(QString("-sOutputFile=\%printer\%%1").arg(prn_device));
+        args.append("-c");
+        args.append(".setpdfwrite");
+        args.append("-f");
+        args.append(print_file);
 
+        start_proc(client_uuid,gsBin,args,VPrn::job_PrintFile);
+    }
+}
+
+void GS_plugin::mergeWithTemplate(const QString &client_uuid, const QStringList &t_files)
+{
+    // Поиск данных для клиента
+    ClientData *c_data = findClientData(client_uuid);
+    if (!QFile::exists(c_data->getFirstPage() ) ){
+        emit error (VPrn::FileIOError,
+                    QObject::trUtf8("Файл %1 не найден!")
+                    .arg(c_data->getFirstPage()) );
+        return;
+    }
+
+    if (t_files.size() == 0){
+        emit error (VPrn::InternalPluginError,
+                    QObject::trUtf8("Ошибка при подготовке шаблона!")     );
+
+        return;
+    }
+    // Цикл по всем элементам шаблона, для каждого экземпляра
+    QString backPdf;
+    QString outPdf;
+    for (int i=0; i< t_files.size();i++){
+        backPdf = t_files.at(i);
+        // spoolDir/client_uuid/t_firstpage.pdf
+        QRegExp rx("/(.+)/(.+)/(.-copy)/(t_.+).pdf");
+        if(rx.indexIn( backPdf ) != -1){
+            // Наш файлик можно обрабатывать
+            QString copy_num = rx.cap(3);
+            QString page_type  = rx.cap(4);
+
+            if ( page_type.compare("t_firstpage",Qt::CaseInsensitive) == 0){
+                // Лицевая сторона i экз
+                outPdf = QString("%1/%2/%3/firstpage_out.pdf")
+                         .arg(spoolDir,client_uuid,copy_num);
+                c_data->startMerge( i );
+                mergePdf(client_uuid,c_data->getFirstPage(),backPdf,outPdf);
+            }
+            if ( page_type.compare("t_otherpage",Qt::CaseInsensitive) == 0  &&
+                 c_data->getPageCount() >1    ){
+                outPdf = QString("%1/%2/%3/otherpage_out.pdf")
+                         .arg(spoolDir,client_uuid,copy_num);
+                c_data->startMerge( i );
+                // Лицевая сторона второго и последующих листов
+                mergePdf(client_uuid,c_data->getOtherPage(),backPdf,outPdf);
+            }
+            if ( page_type.compare("t_oversidepage",Qt::CaseInsensitive) == 0){
+                outPdf = QString("%1/%2/%3/oversidepage_out.pdf")
+                         .arg(spoolDir,client_uuid,copy_num);
+                QFile::copy(backPdf,outPdf);
+            }
+            if ( page_type.compare("t_lastpage",Qt::CaseInsensitive) == 0){
+                outPdf = QString("%1/%2/%3/lastpage_out.pdf")
+                         .arg(spoolDir,client_uuid,copy_num);
+                QFile::copy(backPdf,outPdf);
+            }
+
+        }
+    }
+}
+
+void GS_plugin::convertPdfToPng(const QString &client_uuid,
+                                const QStringList &files, bool mode)
+{
+    // Поиск данных для клиента
+    ClientData *c_data = findClientData(client_uuid);
+
+    if (files.size() == 0){
+        emit error (VPrn::InternalPluginError,
+                    QObject::trUtf8("Ошибка при подготовке шаблона!")     );
+        return;
+    }
+    if (c_data){
+        QString file_name;
+        for (int i=0; i<files.size();i++){
+            //gs -q -sDEVICE=png16m -dBATCH -dNOPAUSE -dFirstPage=1 -dLastPage=5 -r300 -sOutputFile=out_%d.png in.pdf
+            file_name = files.at(i);
+
+            QRegExp rx("/(.+)/(.+)/(.-copy)/(.+)_out.pdf");
+            if (rx.indexIn(file_name) != -1){
+                // Наш файлик можно обрабатывать
+                QString copy_num = rx.cap(3);
+                QString page_type  = rx.cap(4);
+
+                c_data->startConverPdf2Png( i );
+                QStringList args;
+                args.append("-q");
+                args.append("-dQUIET");
+                args.append("-dNOPAUSE");
+                args.append("-dBATCH");
+                args.append("-dPARANOIDSAFER");
+                args.append("-r600");
+                args.append("-sDEVICE=png16m");
+                args.append("-dFirstPage=1");
+                //Формируем имя файлов для предпросмотра
+                if (!mode){
+                    args.append("-dLastPage=1");
+                    args.append(QString("-sOutputFile=%1/%2/%3/%4_0.png")
+                                .arg(spoolDir,client_uuid,copy_num,page_type));
+                }else{
+                    args.append(QString("-sOutputFile=%1/%2/%3/%4_\%d.png")
+                                .arg(spoolDir,client_uuid,copy_num,page_type));
+                }
+                args.append(file_name);
+                start_proc(client_uuid,gsBin,args,VPrn::job_ConvertToPng);
+            }else{
+                emit error (VPrn::FileIOError,QObject::trUtf8("Ошибка в имени файла, при конвертации в pdf -> png!"));
+
+            }
+        }
+    }
+}
+
+
+QString GS_plugin::getUuid() const
+{
+    return QUuid::createUuid().toString().replace("{","").replace("}","");
+}
+
+void GS_plugin::createClientData(const QString &client_uuid)
+{
+    /**
+    * @todo Кто будет реагировать на ошибку и когда ?
+    * проверить что вызвающая функция сумеет обработать ситуацию когда не удалось создать
+    * каталоги для клиента
+    **/
+
+    ClientData *c_d(0);
+    c_d = findClientData (client_uuid);
+    QDir w_dir;
+
+    QString wrk_dir =QString("%1/%2").arg(this->spoolDir,client_uuid);
+    bool Ok = true;
+    {
+        if (c_d){
+            if (w_dir.exists(wrk_dir)){
+                Ok &=w_dir.rmdir(wrk_dir);
+            }
+            // Формируем требуемые каталоги
+            Ok &= w_dir.mkpath(wrk_dir);
+            for (int i=1; i<5;i++){
+                Ok &= w_dir.mkpath(QString("%1/%2/%3-copy")
+                                   .arg(this->spoolDir,client_uuid)
+                                   .arg(i,0,10));
+            }
+        }else{
+            Ok &=false;
+        }
+    }
+    if (!Ok){
+        emit error(VPrn::InternalPluginError,
+                   QObject::trUtf8("Внутрення ошибка плагина!")
+                   );
+    }
+}
+
+void GS_plugin::deleteClientData(const QString &client_uuid)
+{
+    clients_data.remove(client_uuid);
+}
 
 //---------------------------- PRIVATE SLOTS --------------------------------------------------
 void GS_plugin::threadFinish(const QString &jobKey,int code,
@@ -125,78 +319,92 @@ void GS_plugin::threadFinish(const QString &jobKey,int code,
 
     QString m_client_uuid = clients_list.take(jobKey);
     VPrn::Jobs m_job = jobs_list.take(jobKey);
-    qDebug() << "\nthreadFinish:"
-            << "\nm_client_uuid:" << m_client_uuid
-            << "\njobKey:" <<jobKey
-            << "\nm_job:" <<m_job;
+    // Поиск данных для клиента
+    ClientData *c_data = findClientData(m_client_uuid);
+
+    if (code != 0 ){
+        //Задача завершена с ошибкой, доложим наверх
+        emit jobFinish(m_client_uuid, m_job,code,output_message);
+        return;
+    }
 
     // Формируем пути для файлов
-    QString mainPDF       = QObject::trUtf8("%1/%2_main.pdf")
-                            .arg( tempPath, m_client_uuid );
-    QString firstPage_fn  = QObject::trUtf8("%1/%2_first.pdf")
-                            .arg( tempPath, m_client_uuid );
-    QString otherPages_fn = QObject::trUtf8("%1/%2_other.pdf")
-                            .arg(tempPath, m_client_uuid);
+    QString mainPDF       = c_data->getMainFile();
+
 
     // Посмотрим чем же таким важным был занят клиент
     QRegExp rx;
     switch (m_job){
+    case VPrn::job_ConvertToPng:
+        if ( c_data->isFinishedConvertPdf2Png() ){
+            emit docReady4preview( m_client_uuid );
+        }
+        break;
+    case VPrn::job_MergePdf:
+        if (c_data->isFinishedMerge() ){
+            // Все варианты документа объеденнены с шаблоном отправим сигнал
+            emit docReady4print( m_client_uuid );
+        }
+        break;
+    case VPrn::job_PrintFile:
+        break;
     case VPrn::job_ConvertPs2Pdf: // Завершилась задача конвертирования ps в
-        //Задача завершена, доложим наверх
-        emit jobFinish(m_client_uuid, m_job,code,output_message);
         getPageCount(m_client_uuid,mainPDF);
         break;
     case VPrn::job_SplitPageFirst:
+        break;
     case VPrn::job_SplitPageOther:
-        //Задача завершена, доложим наверх
-        emit jobFinish(m_client_uuid, m_job,code,output_message);
+        if ( !c_data->getOtherPage().isEmpty()  && c_data->getPageCount() > 1  ){
+            emit docReady4work(m_client_uuid,c_data->getPageCount());
+        }
         break;
     case VPrn::job_CalcPageCount: // Завершилась задача получения dump_info из pdf документа
         int pagesCount; // Число страниц в конвертируемом документе
         if (!output_message.isEmpty()) {
-            rx.setPattern("NumberOfPages:\\s+(\\d{1,5}).*");
-            rx.setMinimal(true);
+            rx.setPattern("NumberOfPages:\\s+(\\d{1,}).*");
+            //rx.setMinimal(true);
             if (rx.indexIn(output_message) != -1) {
                 bool ok;
                 qDebug() << Q_FUNC_INFO << rx.cap(0) << rx.cap(1);
                 pagesCount = rx.cap(1).toInt(&ok, 10);
-                // числа листов в документе зависит наш ответ
-                if (pagesCount == 0 || !ok){
-                    emit jobFinish(m_client_uuid,VPrn::job_CalcPageCount,
-                                   1,
-                                   QObject::trUtf8("Ошибка разбора документа PDF %1")
-                                   .arg(mainPDF)
-                                   );
-                }else{
-                    if (pagesCount ==1 && QFile::copy(mainPDF, firstPage_fn) ){
-                        //Так как в документе только 1 страница, то сразу
-                        //рапортуем о завершении сразу 2-х задач.
-                        emit jobFinish(m_client_uuid,VPrn::job_CalcPageCount,
-                                       code,
-                                       QString("%1").arg(pagesCount,0,10)
-                                       );
-                        emit jobFinish(m_client_uuid,VPrn::job_SplitPageFirst,
-                                       code,tr(""));
-                    }else{
-                        emit jobFinish(m_client_uuid,VPrn::job_CalcPageCount,
-                                       1,
+
+                if (ok && pagesCount >0 ){
+                    c_data->setPageCount(pagesCount);
+
+                    c_data->setFirstPage( QObject::trUtf8("%1/%2/firstpage.pdf")
+                                          .arg( spoolDir, m_client_uuid ) );
+                    if ( pagesCount ==1){
+                        if ( QFile::exists(c_data->getFirstPage()) ){
+                            QFile::remove( c_data->getFirstPage() );
+                        }
+                        if ( !QFile::copy(mainPDF,c_data->getFirstPage() )){
+                            emit error(VPrn::InternalPluginError,
                                        QObject::trUtf8("Ошибка копирования документа PDF из %1 в %2")
-                                       .arg(mainPDF,firstPage_fn)
+                                       .arg(mainPDF,c_data->getFirstPage() )
                                        );
-                    }
-                    if (pagesCount > 1){
-                        emit jobFinish(m_client_uuid,VPrn::job_CalcPageCount,
-                                       code,
-                                       QString("%1").arg(pagesCount,0,10)
-                                       );
+                        }else{
+                            emit docReady4work(m_client_uuid,pagesCount);
+                        }
+                    }else{
+                        c_data->setOtherPage( QObject::trUtf8("%1/%2/otherpage.pdf")
+                                              .arg( spoolDir, m_client_uuid) );
                         // Делим файлы на 2 части
-                        splitPdf(m_client_uuid,mainPDF,firstPage_fn, otherPages_fn);
+                        splitPdf(m_client_uuid,c_data->getMainFile(),
+                                 c_data->getFirstPage(),
+                                 c_data->getOtherPage());
                     }
+                }else{
+                    emit error(VPrn::InternalPluginError,
+                               QObject::trUtf8("Ошибка разбора документа PDF %1")
+                               .arg(mainPDF)
+                               );
                 }
+
             }
         }
         break;
     }
+
 }
 
 //----------------------------------- PRIVATE ---------------------------------------------
@@ -208,11 +416,6 @@ void GS_plugin::start_proc(const QString &client_uuid,const QString &bin,
     QString j_key = this->getUuid();
     clients_list.insert(j_key,client_uuid);
     jobs_list.insert(j_key,job_id);
-
-    qDebug() << "\nthreadStart:"
-            << "\nclient_uuid:" << client_uuid
-            << "\njob_key:"     << j_key
-            << "\njob_id:"      << job_id;
 
     ProcessT *proc = new ProcessT(this,j_key);
     // Обработчик сообщений файлового потока
@@ -226,11 +429,13 @@ void GS_plugin::start_proc(const QString &client_uuid,const QString &bin,
 void GS_plugin::getPageCount(const QString &client_uuid,const QString &input_fn)
 {
     //pdfTk input_file dump_data
-    args.clear();
+    QStringList args;
     args.append(input_fn);
     args.append("dump_data");
     start_proc(client_uuid,pdftkBin,args,VPrn::job_CalcPageCount);
 }
+
+
 
 void GS_plugin::splitPdf(const QString &client_uuid,const QString &main_pdf,
                          const QString &first_page, const QString &other_pages)
@@ -240,12 +445,13 @@ void GS_plugin::splitPdf(const QString &client_uuid,const QString &main_pdf,
     // То запускаем 2 потока формирирование 1 стр, и формирование последующих
 
     // Первую страницу
-    args.clear();
+    QStringList args;
     args.append(main_pdf);
     args.append("cat");
     args.append("1");
     args.append("output");
     args.append(first_page);
+
     start_proc(client_uuid,pdftkBin,args,VPrn::job_SplitPageFirst);
 
     args.clear();
@@ -254,12 +460,46 @@ void GS_plugin::splitPdf(const QString &client_uuid,const QString &main_pdf,
     args.append("2-end");
     args.append("output");
     args.append(other_pages);
+
     start_proc(client_uuid,pdftkBin,args,VPrn::job_SplitPageOther);
 }
 
-QString GS_plugin::getUuid() const
+void GS_plugin::mergePdf(const QString &client_uuid,const QString &in_pdf,
+                         const QString &back_pdf,const QString &out_pdf)
 {
-    return QUuid::createUuid().toString().replace("{","").replace("}","");
+    //pdftk in.pdf background back.pdf output out.pdf
+    if (!QFile::exists(in_pdf)) {
+        emit error(VPrn::FileIOError,
+                   QObject::trUtf8("ERROR : Файл %1 не найден\n").arg(in_pdf));
+        return;
+    }
+
+    if (!QFile::exists(back_pdf)) {
+        emit error(VPrn::FileIOError,
+                   QObject::trUtf8("ERROR : Файл %1 не найден\n").arg(back_pdf));
+        return;
+    }
+
+    QStringList args;
+    args.append(in_pdf);
+    args.append("background");
+    args.append(back_pdf);
+    args.append("output");
+    args.append(out_pdf);
+
+    start_proc(client_uuid,pdftkBin,args,VPrn::job_MergePdf);
+}
+
+ClientData * GS_plugin::findClientData(const QString &client_uuid)
+{
+    ClientData *c_d(0);
+
+    QMap<QString,ClientData *>::const_iterator i = clients_data.find(client_uuid);
+    if ( i == clients_data.end() ){
+        clients_data.insert(client_uuid,new ClientData());
+    }
+    c_d = clients_data.value(client_uuid);
+    return c_d;
 }
 
 
--- trunk/vp_plugins/gs_plugin/gs_plugin.h
+++ trunk/vp_plugins/gs_plugin/gs_plugin.h
@@ -1,12 +1,13 @@
 #ifndef GS_PLUGIN_H
 #define GS_PLUGIN_H
 #include "igs_plugin.h"
-#include  "mytypes.h"
+#include "mytypes.h"
+#include "clientdata.h"
 
 #include <QObject>
 #include <QtCore/QLibrary>
-
 #include <QtCore/QMap>
+#include <QtCore/QStack>
 
 /** @todo Сделать загрузку библиотеки gs если под виндой понятно как это сделать,
   * хотя и не очень удобно делать, тот с линуксом засада, не пойму как
@@ -28,8 +29,66 @@ public:
     void convertPs2Pdf(const QString &client_uuid,const QString &input_fn);
 
     void final_clear();
+    /**
+      * @fn void print2devide (const QString &client_uuid,  const QString &print_file,
+                                            const QString &prn_device,bool usePageCount);
+      * @brief Печать  документа сформированного клиентом на устройство
+      */
+    void print2devide (const QString &client_uuid,  const QString &print_file,
+                                  const QString &prn_device,bool usePageCount);
+
+    /**
+      * @fn  QString getUuid() const
+      * @brief служебная функция генерирует ключ
+      */
+    QString getUuid() const;
+
+    /**
+      * @fn void mergeWithTemplate(const QString &client_uuid,
+      *                                                   const QStringList &t_files);
+      * @brief Объединяет исходный файл состоящий из 2х частей с элементами шаблона
+      * для i экз документа
+      */
+    void mergeWithTemplate(const QString &client_uuid, const QStringList &t_files );
+    /**
+      * @fn void convertPdfToPng(const QString &client_uuid,
+      *                          const QString &filename, bool fullFile);
+      * @brief Преобразует исходный файл в набор картинок
+      */
+    void convertPdfToPng(const QString &client_uuid,
+                         const QStringList &files, bool mode);
+    /**
+      * @fn void createClientData(const QString &client_uuid);
+      * @brief Создает новый экз. хранилища данных для клиента
+      */
+    /**
+      * @fn void deleteClientData(const QString &client_uuid);
+      * @brief удаляет экз. хранилища данных для клиента
+      */
+    void createClientData(const QString &client_uuid);
+    void deleteClientData(const QString &client_uuid);
 
 signals:
+    /**
+      * @fn void docReady4work(const QString &client_uuid,int pagecount);
+      * @brief Сигнал испускается после завершения конвертации ps->pdf,
+      * подсчета числа страниц и разбиения на 1 и последующие страницы
+      */
+    void docReady4work(const QString &client_uuid,int pagecount);
+    /**
+      * @fn void docReady4print(const QString &client_uuid);
+      * @brief  Сигнал испускается когда на все экз документа наложен шаблон,
+      * документ готов к отправке на принтер, или к дальнейшей конвертации в png
+      * и предпросмотр
+      */
+    void docReady4print(const QString &client_uuid);
+    /**
+      * @fn void docReady4print(const QString &client_uuid);
+      * @brief  Сигнал испускается когда все экз документа, в соответсвии с режимом
+      * преобразованны в картинки полностью или частично
+      */
+    void docReady4preview(const QString &client_uuid);
+
     void error(pluginsError errCode,QString error_message);
     /**
       * @fn void jobFinish (const QString &client_uuid,
@@ -48,10 +107,10 @@ private slots:
 
 private:
     QString gsBin;
-    QString tempPath;
+    QString spoolDir;
     QString pdftkBin;
     QString gs_rcp;
-    QStringList args;
+
     QStringList myEnv; // Мои переменные среды для gs
 
     /**
@@ -74,17 +133,40 @@ private:
       */
     void start_proc(const QString &client_uuid,const QString &bin,
                     QStringList &arg_list,VPrn::Jobs job_id);
+
+
     /**
-      * @fn  QString getUuid() const
-      * @brief служебная функция генерирует ключ
+      * @fn void mergePdf(const QString &client_uuid,const QString &in_pdf,
+      *                   const QString &back_pdf,const QString &out_pdf);
+      * @brief Объединяет 2 pdf файла используя pdftk
       */
-    QString getUuid() const;
+
+    void mergePdf(const QString &client_uuid,const QString &in_pdf,
+                  const QString &back_pdf,const QString &out_pdf);
+
+
+    /**
+      * @fn ClientData * findClientData(const QString &client_uuid);
+      * @brief Ищет в контейнере объект ClientData если находит, то возвращает ссылку на него,
+      * иначе создает и возвращает ссылку
+      */
+    ClientData * findClientData(const QString &client_uuid);
+    /**
+      * @fn QString formatFileName(const QString &client_uuid,
+      *                            const QString &fName,
+      *                            const QString &fType,
+      *                            const QString &ext);
+      * @brief Формирует имя выходного файла по заданным критериям
+      */
+
+    //-----------------------------------------------------------------------------------------------------
     /**
       * @var clients_list; Список uuid,client_id
       * @var jobs_list;   Список uuid,job_id
       */
     QMap <QString,QString> clients_list;
     QMap <QString,VPrn::Jobs> jobs_list;
+    QMap <QString,ClientData *> clients_data;
 
 };
 
