1a2ff338a5ed636e3928e786d59bd04f915391f6
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-05-26 13:43:43 +0000

    Лог файл теперь пишет в каталог запуска прогруммы

--- trunk/vp_plugins/print_monitor/main.cpp
+++ trunk/vp_plugins/print_monitor/main.cpp
@@ -1,17 +1,54 @@
 
-#include <QtSingleApplication>
-#include <QtCore/QTextCodec>
-#include <QtCore/QTranslator>
-#include <QtCore/QLibraryInfo>
-#include <QtGui/QMessageBox>
-
-#include "config.h"
 #include "mytypes.h"
 #include "printmonitor.h"
 
+#include <QtSingleApplication>
+
+#include <QtCore/QTextCodec>
+#include <QtCore/QTranslator>
+#include <QtCore/QLibraryInfo>
+
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
+#include <QtCore/QDateTime>
+
+#include <QtGui/QMessageBox>
+
+
 using namespace VPrn;
 
-#define MY_DEBUG
+
+
+void myMessageOutput(QtMsgType type, const char *msg)
+{
+    QString log = QString ("%1/%2_print_monitor.log").arg(qApp->applicationDirPath(),QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss"));
+    QFile logFile(log);
+
+    if (!logFile.open(QFile::Append| QFile::Text)){
+        logFile.open(stderr, QIODevice::WriteOnly);
+    }
+
+     QTextStream out;
+     out.setDevice(&logFile);
+     out.setCodec("UTF-8");
+
+     out << "\nDateTime: " << QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss");
+     switch (type) {
+     case QtDebugMsg:
+        out << QObject::trUtf8("Debug: %1\n").arg(QString(msg)) <<"\n";
+         break;
+     case QtWarningMsg:
+         out << QObject::trUtf8("Warning: %1\n").arg(QString(msg))<<"\n";
+         break;
+     case QtCriticalMsg:
+         out << QObject::trUtf8("Critical: %1\n").arg(QString(msg))<<"\n";
+         break;
+     case QtFatalMsg:
+         out << QObject::trUtf8("Fatal: %1\n").arg(QString(msg))<<"\n";
+         abort();
+     }
+     logFile.close();
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,20 +58,10 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForTr(codec);
     QTextCodec::setCodecForCStrings(codec);
     QTextCodec::setCodecForLocale(codec);
-    //qInstallMsgHandler(myMessageOutput);
+    qInstallMsgHandler(myMessageOutput);
 
     QtSingleApplication app(argc, argv, true);
 
-    QString in_file;
-#if defined (MY_DEBUG)
-
-#if defined (Q_OS_WIN32)
-    in_file="d:/opt/vprn/test.ps";
-#elif defined (Q_OS_UNIX)
-    in_file="/opt/vprn/test.ps";
-#endif
-
-#else
     QStringList aList=app.arguments();
     if (aList.size()< 1){
         QMessageBox::critical(0,QObject::trUtf8("Обратитесь к системному администратору"),
@@ -42,10 +69,11 @@ int main(int argc, char *argv[])
         return 1;
     }
     QString in_file=aList.at(1);
-#endif    
+
     if (app.sendMessage(QObject::trUtf8("Попытка запуска второй копии приложения!"))
         || app.isRunning())
         return 0;
+
     QString translatorFileName = QLatin1String("qt_");
     translatorFileName += QLocale::system().name();
     QTranslator *translator = new QTranslator(&app);
