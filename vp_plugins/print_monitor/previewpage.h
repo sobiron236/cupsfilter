cfeb424b20772eca6839441aee5c11df26a8f9f9
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-06-01 08:49:14 +0000

    @fn bool validatePage(); @brief Данная функция проверяет корректность заполнения полей проверка последней минутки
    @fn void showPicturesList(QStringList png_list);
    Загрузка и преобразование списка картинок в набор из превьшки и полной страницы
    
    Сигнал void printCurrentDoc(); если пользователь нажал кнопку дальше (т.е печать текущего документа)
    
    
    @fn void zoomIn();
    @fn void zoomOut();
    @short Изменение масштаба на 1 ед.

--- vp_plugins/print_monitor/previewpage.h
+++ vp_plugins/print_monitor/previewpage.h
@@ -2,9 +2,16 @@
 #define PREVIEWPAGE_H
 
 #include "mytypes.h"
-using namespace VPrn;
+#include "viewport.h"
 
 #include <QtGui/QWizardPage>
+#include <QtGui/QPixmap>
+#include <QtGui/QSlider>
+
+#include <QtCore/QMap>
+
+using namespace VPrn;
+
 
 
 QT_FORWARD_DECLARE_CLASS ( QLabel )
@@ -12,16 +19,42 @@ QT_FORWARD_DECLARE_CLASS ( QCheckBox )
 
 class PreViewPage : public QWizardPage
 {
-Q_OBJECT
+    Q_OBJECT
 public:
     explicit PreViewPage(QWidget *parent = 0);
-
-signals:
+    /**
+     * @fn bool validatePage();
+     * @brief Данная функция проверяет корректность заполнения полей
+     * проверка последней минутки
+     */
+   bool validatePage ();
 
 public slots:
-private:
-    QLabel *topLabel;
+    void showPicturesList(QStringList png_list);
+signals:
+    void printCurrentDoc();
 
+private slots:
+    /**
+      * @fn void zoomIn();
+      * @fn void zoomOut();
+      * @short Изменение масштаба на 1 ед.
+      */
+    void zoomIn();
+    void zoomOut();
+
+private:
+
+    QCheckBox *checkPicturesList;
+    ViewPort *leftFrame;
+    ViewPort *rightFrame;
+    QSize thumbSize;
+    QLabel *topLabel;
+    PixmapList imageFullItems;
+    PixmapList imageThumbItems;
+    QStringList imageFilesList;
+
+    QSlider *zoomSlider;
 };
 
 #endif // PREVIEWPAGE_H
