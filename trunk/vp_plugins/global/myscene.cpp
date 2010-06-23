879c02478971bca1bd0a1b18db447870d3a43fcc
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-06-23 14:16:48 +0000

    Добавил кнопку удалить для картинки

--- trunk/vp_plugins/global/myscene.cpp
+++ trunk/vp_plugins/global/myscene.cpp
@@ -58,31 +58,7 @@ void  myScene::createPageForPrint(qreal width,qreal height)
     this->addItem(paper_rect);
 }
 
-/*
-void myScene::drawForeground  ( QPainter * painter, const QRectF & rect )
-{
-    int gridInterval = 50;
-    //interval to draw grid lines at
-    painter->setWorldMatrixEnabled(true);
 
-    QPen pen(Qt::green, 1, Qt::SolidLine);
-
-    painter->setPen(pen);
-    qreal left = int(rect.left()) - (int(rect.left()) % gridInterval );
-    qreal top = int(rect.top()) - (int(rect.top()) % gridInterval );
-
-    QVarLengthArray<QLineF> linesX;
-    for (qreal x = left; x < rect.right(); x += gridInterval )
-        linesX.append(QLineF(x, rect.top(), x, rect.bottom()));
-
-    QVarLengthArray<QLineF> linesY;
-    for (qreal y = top; y < rect.bottom(); y += gridInterval )
-        linesY.append(QLineF(rect.left(), y, rect.right(), y));
-
-    painter->drawLines(linesX.data(), linesX.size());
-    painter->drawLines(linesY.data(), linesY.size());
-}
-*/
 
 void  myScene::createPage(qreal width,qreal height,qreal m_top,qreal m_bottom,
                           qreal m_right,qreal m_left)
@@ -94,52 +70,7 @@ void  myScene::createPage(qreal width,qreal height,qreal m_top,qreal m_bottom,
     this->setSceneRect(0, 0, width,height);
     //this->setBackgroundBrush(Qt::white);
     this->setBackgroundBrush(Qt::transparent);
-/*
-    this->setBackgroundBrush(Qt::transparent);
-    pixmap = QPixmap(width,height);
-    pixmap.fill(Qt::transparent);
 
-    QGraphicsPixmapItem *paper_rect =
-            new QGraphicsPixmapItem();
-    paper_rect->setPixmap(pixmap);
-    paper_rect->setData(ObjectName, "Paper");
-    paper_rect->setZValue(-1000.0);
-    paper_rect->setShapeMode(QGraphicsPixmapItem::HeuristicMaskShape);
-    this->addItem(paper_rect);
-
-    pixmap2 = QPixmap(width,height);
-    pixmap2.fill(Qt::white);
-    QPainter painter(&pixmap2);
-    //painter.setBrush(Qt::black);
-    //Рисуем сетку
-    int gridInterval = 100; //interval to draw grid lines at
-    painter.setWorldMatrixEnabled(true);
-    QRectF  rect = this->sceneRect();
-
-    qreal left = int(rect.left()) - (int(rect.left()) % gridInterval );
-    qreal top = int(rect.top()) - (int(rect.top()) % gridInterval );
-
-    QVarLengthArray <QLineF, 100 > linesX;
-    for (qreal x = left; x < rect.right(); x += gridInterval )
-        linesX.append(QLineF(x, rect.top(), x, rect.bottom()));
-
-    QVarLengthArray <QLineF, 100 > linesY;
-    for (qreal y = top; y < rect.bottom(); y += gridInterval )
-        linesY.append(QLineF(rect.left(), y, rect.right(), y));
-
-    //painter.drawRect(m_left, m_top,width-m_left-m_right,height-m_top-m_bottom);
-    painter.drawLines(linesX.data(), linesX.size());
-    painter.drawLines(linesY.data(), linesY.size());
-
-    QGraphicsPixmapItem *border_rect = new QGraphicsPixmapItem();
-
-    border_rect->setPixmap(pixmap2);
-    border_rect->setData(ObjectName, "Border");
-    border_rect->setZValue(-900.0);
-    border_rect->setShapeMode(QGraphicsPixmapItem::HeuristicMaskShape);
-    this->addItem(border_rect);
-
-    */
     QGraphicsRectItem *paper_rect =
             new QGraphicsRectItem (QRectF(0,0, width,height));
     paper_rect->setPen(QPen(Qt::black));
@@ -212,9 +143,6 @@ void myScene::addImgElem(const QPointF &ps,const qreal m_angle,const qreal m_sca
     Item->setTransformationMode(Qt::SmoothTransformation);
 
     Item->setMyTransform(m_angle,m_scaled);
-    //Item->setScaledSize(m_scaled);
-    //Item->setAngle(m_angle);
-
     Item->setParentItem(this->getPaperItem());
 }
 
@@ -293,12 +221,14 @@ void  myScene::contextMenuEvent( QGraphicsSceneContextMenuEvent* event )
     // we only want to display a menu if user clicked a textItem
     qreal     x       = event->scenePos().x();
     qreal     y       = event->scenePos().y();
+    QMenu menu;
     myTextItem*  textItem = dynamic_cast<myTextItem*>( itemAt( x, y ) );
-    if ( textItem == 0 ){
-        return;
-    }else{
-        qreal angle;
-        QMenu menu;
+    PicItem*  picItem = dynamic_cast<PicItem*>( itemAt( x, y ) );
+    qreal angle;
+    if (textItem == 0 && picItem ==0) return;
+    if ( textItem != 0 ){
+
+
         setTagAction      = menu.addAction(QIcon(":/edit.png"),
                                            QObject::trUtf8("Изменить значение тега"));
         changeFontAction  = menu.addAction(QIcon(":/fontDialog.png"),
@@ -311,52 +241,57 @@ void  myScene::contextMenuEvent( QGraphicsSceneContextMenuEvent* event )
         rotateLeftAction  = menu.addAction(QIcon(":/rotateLeft.png"),
                                            QObject::trUtf8("Вращать влево на 90град."));
         menu.addSeparator();
+    }
+    delElemAction = menu.addAction(QObject::trUtf8("Удалить элемент"));
 
-        delElemAction = menu.addAction(QObject::trUtf8("Удалить элемент"));
-
-        QAction * act = menu.exec(event->screenPos());
-        if (act ==setTagAction){
-            QString tag = QInputDialog::getText(event->widget(),
-                                                QObject::trUtf8("Введите текст"),
-                                                QObject::trUtf8("Новый тэг элемента:"),
-                                                QLineEdit::Normal, textItem->getETag());
-            if (!tag.isEmpty()){
-                m_undoStack->push( new CommandTextItemChangeTag( this, textItem,tag ) );
-                textItem->setEText(QObject::trUtf8("Новый..."));
-                textItem->setPlainText(tag);
-            }
-        }
-        if (act == delElemAction){
-            m_undoStack->push( new CommandTextItemDelete( this, textItem ) );
-
-        }
-        if (act == rotateRightAction){
-            angle = -90.0;
-            m_undoStack->push( new CommandTextItemRotate( this, textItem,angle ) );
-
-        }
-        if (act == rotateLeftAction){
-            angle = 90.0;
-            m_undoStack->push( new CommandTextItemRotate( this, textItem,angle) );
-
-        }
-        if (act == changeFontAction ){
-            bool ok;
-
-            QFont font = QFontDialog::getFont( &ok, QFont("Times", 14,QFont::Bold),0,
-                                               QObject::trUtf8("Выберите шрифт для элемента "));
-            if (ok) {// пользователь нажимает OK, и шрифт устанавливается в выбранный
-                //currentFont = font;
-                m_undoStack->push( new CommandTextItemChangeFont( this, textItem,font ) );
-            }
-        }
-        if (act == changeColorAction ){
-            QColor col = QColorDialog::getColor ( Qt::white,0,QObject::trUtf8("Выберите цвет текущего элемента ") ) ;
-            if (col.isValid()) {// пользователь нажимает OK, и цвет устанавливается в выбранный
-                m_undoStack->push( new CommandTextItemChangeColor( this, textItem,col ) );
-            }
+    QAction * act = menu.exec(event->screenPos());
+    if (act ==setTagAction){
+        QString tag = QInputDialog::getText(event->widget(),
+                                            QObject::trUtf8("Введите текст"),
+                                            QObject::trUtf8("Новый тэг элемента:"),
+                                            QLineEdit::Normal, textItem->getETag());
+        if (!tag.isEmpty()){
+            m_undoStack->push( new CommandTextItemChangeTag( this, textItem,tag ) );
+            textItem->setEText(QObject::trUtf8("Новый..."));
+            textItem->setPlainText(tag);
         }
     }
+    if (act == delElemAction){
+        if ( textItem != 0 ){
+            m_undoStack->push( new CommandTextItemDelete( this, textItem ) );
+        }
+        if ( picItem != 0 ){
+            this->removeItem(picItem);
+        }
+    }
+    if (act == rotateRightAction){
+        angle = -90.0;
+        m_undoStack->push( new CommandTextItemRotate( this, textItem,angle ) );
+
+    }
+    if (act == rotateLeftAction){
+        angle = 90.0;
+        m_undoStack->push( new CommandTextItemRotate( this, textItem,angle) );
+
+    }
+    if (act == changeFontAction ){
+        bool ok;
+
+        QFont font = QFontDialog::getFont( &ok, QFont("Times", 14,QFont::Bold),0,
+                                           QObject::trUtf8("Выберите шрифт для элемента "));
+        if (ok) {// пользователь нажимает OK, и шрифт устанавливается в выбранный
+            //currentFont = font;
+            m_undoStack->push( new CommandTextItemChangeFont( this, textItem,font ) );
+        }
+    }
+    if (act == changeColorAction ){
+        QColor col = QColorDialog::getColor ( Qt::white,0,QObject::trUtf8("Выберите цвет текущего элемента ") ) ;
+        if (col.isValid()) {// пользователь нажимает OK, и цвет устанавливается в выбранный
+            m_undoStack->push( new CommandTextItemChangeColor( this, textItem,col ) );
+        }
+    }
+
+
 }
 
 void  myScene::setViewMode()
