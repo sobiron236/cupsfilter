b852fb1113d4c6b7f1deeb0d0f5db9ac455e5279
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-07-29 06:41:29 +0000

    Переход от QwizardPage -> QWidget

--- vp_plugins/print_monitor/selectpage.cpp
+++ vp_plugins/print_monitor/selectpage.cpp
@@ -5,71 +5,56 @@
 #include <QtGui/QComboBox>
 #include <QtGui/QRadioButton>
 
+#include "mytypes.h"
+using namespace VPrn;
+
 
 SelectPage::SelectPage(QWidget *parent)
-    : QWizardPage(parent)
-    , printer_id (0)
+    : QWidget(parent)
+    , printer_id (-1)
 {
-    setTitle(QObject::trUtf8("Выбор режима работы ..."));
-    setPixmap(QWizard::WatermarkPixmap, QPixmap(":/select_mode.png"));
+    this->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::MinimumExpanding);
 
+    this->setWindowTitle(QObject::trUtf8("Выбор режима работы ..."));
 
-    topLabel = new QLabel(QObject::trUtf8("На этом шаге необхоимо выбрать Принтер и один из режимов работы:"
-                                          "<ul><li>Пометка зарегистрованного документа как БРАК</li>"
-                                          "<li>Учет листов в БД</li><li>Печать на учтенных листах</li>"
-                                          "<li>Печать с автоматическим учетом листов</li></ul>"
-                                          ));
+    QLabel *topLabel = new QLabel(QObject::trUtf8("На этом шаге необходимо выбрать <b>Принтер</b> и один из режимов работы:"
+                                                  "<ul><li>Пометка зарегистрованного документа как БРАК</li><br/>"
+                                                  "<li>Учет листов в БД</li><br/><li>Печать на учтенных листах</li><br/>"
+                                                  "<li>Печать с автоматическим учетом листов</li></ul>"
+                                                  ));
     topLabel->setWordWrap(true);
 
-    printersCBox = new QComboBox(this);
+    printersCBox  = new QComboBox(this);
+    markBrakDoc   = new QRadioButton(this);
+    accountingDoc = new QRadioButton(this);
+    printDoc      = new QRadioButton(this);
+    both_step     = new QRadioButton(this);
 
-    markBrakDoc = new QRadioButton(this);
     markBrakDoc->setText(QObject::trUtf8("Пометка зарегистрированного и распечатанного документа как брак"));
     markBrakDoc->setEnabled(false);
-    accountingDoc = new QRadioButton(this);
     accountingDoc->setText(QObject::trUtf8("Учет листов в БД"));
-
-    printDoc      = new QRadioButton(this);
     printDoc->setText(QObject::trUtf8("Печать на учтенных листах"));
-
-    both_step      = new QRadioButton(this);
     both_step->setText(QObject::trUtf8("Печать на учтенных листах, с автоматичеcкой регистрацией в БД"));
 
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(topLabel);
-    layout->addSpacing(3);
-    layout->addWidget(printersCBox);
-    layout->addWidget(markBrakDoc);
-    layout->addWidget(accountingDoc);
-    layout->addWidget(printDoc);
-    layout->addWidget(both_step);
-
-    setLayout(layout);
-    registerField("printersCBox*",printersCBox);
-    registerField("markBrakDoc",  markBrakDoc);
-    registerField("accountingDoc", accountingDoc);
-    registerField("printDoc",printDoc);
-    registerField("both_step",both_step);
+    QVBoxLayout *verticalLayout = new QVBoxLayout();
+    verticalLayout->addStretch(0);
+    verticalLayout->addWidget(topLabel);
+    verticalLayout->addSpacing(3);
+    verticalLayout->addWidget(printersCBox);
+    verticalLayout->addWidget(markBrakDoc);
+    verticalLayout->addWidget(accountingDoc);
+    verticalLayout->addWidget(printDoc);
+    verticalLayout->addWidget(both_step);
+    verticalLayout->addStretch(0);
+    setLayout(verticalLayout);
 
     connect (printersCBox,SIGNAL(currentIndexChanged(int)),
              this, SLOT(setCurrentPrinter(int)));
-
-    this->setButtonText(QWizard::NextButton,QObject::trUtf8("Вперед"));
-    this->setButtonText(QWizard::BackButton,QObject::trUtf8("Назад"));
 }
 
 
-
-int SelectPage::nextId() const
+void SelectPage::setPrintersModel(QStandardItemModel *p_model )
 {
-    if (markBrakDoc->isChecked()) {
-        //return VPrn::Page_PrintData;
-    }
-    return VPrn::Page_PrintData;
+    printersCBox->setModel( p_model );
+    printersCBox->setModelColumn( 0 );
 }
-
- void SelectPage::setPrintersModel(QStandardItemModel *p_model )
- {
-     printersCBox->setModel( p_model );
-     printersCBox->setModelColumn( 0 );
- }
