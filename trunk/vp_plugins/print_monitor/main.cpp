d10070526ba4a6335594f0ab07c126b1e81a4b9e
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-07-29 06:42:19 +0000

    Настройка стилей кнопок

--- trunk/vp_plugins/print_monitor/main.cpp
+++ trunk/vp_plugins/print_monitor/main.cpp
@@ -1,89 +1,47 @@
 #include <QtSingleApplication>
-
-#include <QtCore/QTextCodec>
-#include <QtCore/QTranslator>
-#include <QtCore/QLibraryInfo>
-
-#include <QtCore/QFile>
-#include <QtCore/QTextStream>
-#include <QtCore/QDateTime>
-
 #include <QtGui/QMessageBox>
+#include <QtCore/QTextCodec>
+#include <QtCore/QStringList>
 
-#include "mytypes.h"
-#include "printmonitor.h"
-
-using namespace VPrn;
-
-void myMessageOutput(QtMsgType type, const char *msg)
-{
-    QString log = QString ("%1/print_monitor.log").arg( qApp->applicationDirPath() );
-    QFile logFile(log);
-
-    if (!logFile.open(QFile::Append| QFile::Text)){
-        logFile.open(stderr, QIODevice::WriteOnly);
-    }
-
-     QTextStream out;
-     out.setDevice(&logFile);
-     out.setCodec("UTF-8");
-
-     out << "\nDateTime: " << QDateTime::currentDateTime ().toString("dd.MM.yyyy hh:mm:ss");
-     switch (type) {
-     case QtDebugMsg:
-        out << QObject::trUtf8("Debug: %1\n").arg(QString(msg)) <<"\n";
-         break;
-     case QtWarningMsg:
-         out << QObject::trUtf8("Warning: %1\n").arg(QString(msg))<<"\n";
-         break;
-     case QtCriticalMsg:
-         out << QObject::trUtf8("Critical: %1\n").arg(QString(msg))<<"\n";
-         break;
-     case QtFatalMsg:
-         out << QObject::trUtf8("Fatal: %1\n").arg(QString(msg))<<"\n";
-         abort();
-     }
-     logFile.close();
-}
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-
-    Q_INIT_RESOURCE(images);
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     QTextCodec::setCodecForTr(codec);
     QTextCodec::setCodecForCStrings(codec);
     QTextCodec::setCodecForLocale(codec);
-    qInstallMsgHandler(myMessageOutput);
 
     QtSingleApplication app(argc, argv, true);
 
     QStringList aList=app.arguments();
     if (aList.size()< 2){
         QMessageBox::critical(0,QObject::trUtf8("Обратитесь к системному администратору"),
-                              QObject::trUtf8("Ошибка запуска приложения, не достаточно параметров запуска"));
+                              QObject::trUtf8("Ошибка запуска приложения, не задан файл для печати"));
         return 1;
     }
 
     QString in_file=aList.at(1);
 
-    if (app.sendMessage(QObject::trUtf8("Попытка запуска второй копии приложения!"))
+    if (app.sendMessage(QObject::trUtf8("Потребован повторный запуск приложения!"))
         || app.isRunning())
         return 0;
 
-    QString translatorFileName = QLatin1String("qt_");
-    translatorFileName += QLocale::system().name();
-    QTranslator *translator = new QTranslator(&app);
-    if (translator->load(translatorFileName,
-                         QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
-        app.installTranslator(translator);
+    QApplication::setQuitOnLastWindowClosed(true);
+    qApp->setStyleSheet(" QPushButton:hover {background-color: black;} "
+                        " QPushButton:hover {background-color: white;} "
+                        " QPushButton { border-style: outset; border-width: 2px;"
+                        "  border-radius: 10px; border-color: beige;"
+                        "  font: bold 10px;  min-width: 10em; padding: 6px}"
+                        );
 
-    PrintMonitor monitor(0,in_file);
+    MainWindow client;
+    client.init ( qApp->applicationDirPath(),in_file );
+    client.show();
+    app.setActivationWindow(&client);
 
     QObject::connect(&app, SIGNAL(messageReceived(const QString&)),
-                     &monitor, SLOT(appendStartMsg(const QString&)));
+                     &client, SLOT(appendStartMsg(const QString&)));
 
-    app.setActivationWindow(&monitor);
-    monitor.show();
     return app.exec();
 }
