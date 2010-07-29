9bc6cfb3eba56674250245f9ba214249ce5719a3
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-07-29 06:52:59 +0000

    Изменил дизайн кнопок и логику работы окна выбора мандата

--- trunk/vp_plugins/print_monitor/getusernamemandatdlg.h
+++ trunk/vp_plugins/print_monitor/getusernamemandatdlg.h
@@ -4,30 +4,36 @@
 #include <QtGui/QDialog>
 #include <QtCore/QStringList>
 
-
-namespace Ui {
-    class getUserNameMandatDlg;
-}
+QT_FORWARD_DECLARE_CLASS ( QFormLayout )
+QT_FORWARD_DECLARE_CLASS ( QLabel )
+QT_FORWARD_DECLARE_CLASS ( QLineEdit )
+QT_FORWARD_DECLARE_CLASS ( QComboBox )
 
 class getUserNameMandatDlg : public QDialog {
     Q_OBJECT
 public:
     getUserNameMandatDlg(QWidget *parent = 0);
-    ~getUserNameMandatDlg();
-
-    QString getCurrentMandat(){return u_mandat;}
 
     void setMandatList(const QStringList &mandatList);
+
 public slots:
     void setUserName(QString &userName);
+    void accept() ;
 private slots:
-    void setCurrentMandat(QString mandat);
-protected:
-    void changeEvent(QEvent *e);
-private:
-    Ui::getUserNameMandatDlg *ui;
-    QString u_name;
-    QString u_mandat;
+
+    void enableButtons(int index);
+signals:
+    void setAuthData(const QString &login,const QString &mandat);
+private:   
+
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout;
+    QLabel *label;
+    QLineEdit *userNameEd;
+    QLabel *label_2;
+    QComboBox *mandatCBox;
+    QPushButton *okButton;
+    QPushButton *cancelButton;
 };
 
 #endif // GETUSERNAMEMANDATDLG_H
