3f6556e8a6bc11a4070949c9ebda069d00dcb866
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-07-29 06:54:12 +0000

    Основное окно клиента

---
+++ trunk/vp_plugins/print_monitor/mainwindow.cpp
@@ -0,0 +1,362 @@
+#include <QtCore/QVariant>
+#include <QtCore/QDebug>
+#include <QtCore/QFile>
+#include <QtCore/QSettings>
+
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QComboBox>
+#include <QtGui/QFrame>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QLabel>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+#include <QtGui/QAction>
+#include <QtGui/QStackedWidget>
+#include <QtGui/QDesktopWidget>
+#include <QtGui/QErrorMessage>
+
+#include "mainwindow.h"
+#include "mytypes.h"
+#include "engine.h"
+#include "getusernamemandatdlg.h"
+
+using namespace VPrn;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+    , pageId(0)
+
+{
+
+    //получим размер экрана
+    desktop_avail = desktop.availableGeometry(desktop.primaryScreen());
+
+    this->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::MinimumExpanding);
+    //this->setMinimumSize(320,240);
+    this->setMaximumSize(desktop_avail.width()-5,desktop_avail.height()-5);
+    this->move( calcDeskTopCenter(this->width(),this->height() ));
+
+    verticalLayoutWidget = new QWidget;
+    mainLayout = new QVBoxLayout(verticalLayoutWidget);
+
+    //------------------------------- Центральная часть    -------------------------------
+    stackedWidget = new QStackedWidget;
+    introPage     = new IntroPage();
+    selectPage    = new SelectPage();
+    printDataPage = new PrintDataPage();
+    checkDataPage = new CheckDataPage();
+    preViewPage   = new PreViewPage();
+    finishPage    = new FinishPage();
+
+    stackedWidget->addWidget(introPage);
+    stackedWidget->addWidget(selectPage);
+    stackedWidget->addWidget(printDataPage);
+    stackedWidget->addWidget(checkDataPage);
+    stackedWidget->addWidget(preViewPage);
+    stackedWidget->addWidget(finishPage);
+    //Установка макс размеров
+    printDataPage->setMaximumSize(desktop_avail.width()-5,desktop_avail.height()-5);
+
+    //------------------------------- Нижний блок - Кнопки -------------------------------
+    line = new QFrame();
+    line->setFrameShape(QFrame::HLine);
+    line->setFrameShadow(QFrame::Sunken);
+
+    horizontalLayoutWidget = new QWidget();
+    horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+
+    helpButton = new QPushButton(horizontalLayoutWidget);
+    horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    prevButton = new QPushButton(horizontalLayoutWidget);
+    nextButton = new QPushButton(horizontalLayoutWidget);
+    restartButton = new QPushButton(horizontalLayoutWidget);
+    horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    exitButton = new QPushButton(horizontalLayoutWidget);
+
+    helpButton->setText(QObject::trUtf8("Справка"));
+    prevButton->setText(QObject::trUtf8("< Назад"));
+    nextButton->setText(QObject::trUtf8("Вперед >"));
+    restartButton->setText(QObject::trUtf8("В начало"));
+    exitButton->setText(QObject::trUtf8("Выход"));
+
+    horizontalLayout->addWidget(helpButton);
+    horizontalLayout->addItem(horizontalSpacer);
+    horizontalLayout->addWidget(prevButton);
+    horizontalLayout->addWidget(nextButton);
+    horizontalLayout->addWidget(restartButton);
+    horizontalLayout->addItem(horizontalSpacer_2);
+    horizontalLayout->addWidget(exitButton);
+    //-------------------------------------- Компоновка ----------------------------------------------------
+    mainLayout->addWidget(stackedWidget);
+    mainLayout->addWidget(line);    
+    mainLayout->addWidget(horizontalLayoutWidget);
+
+    setLayout(mainLayout);
+    this->setWindowTitle(stackedWidget->currentWidget()->windowTitle() );
+}
+
+void MainWindow::init(const QString &app_dir,const QString &input_file)
+{
+    this->readConfig(app_dir);
+    //Основной потребитель ошибок
+    eMessage = new QErrorMessage(this);
+
+    //Формируем хранителя данных
+    data_module = new DataModule(this);
+
+    //Запуск ядра
+    engine = new Engine(this,link_name,gatekeeper_bin);
+
+    createConnection();
+
+    if ( data_module->setWorkFile(input_file)){
+        introPage->setInputFile();
+    }
+
+    engine->setDocCardModel    ( data_module->getDocCardModel()     );
+    engine->setPrintersModel   ( data_module->getPrintersModel()    );
+    engine->setStampsModel     ( data_module->getStampsModel()      );
+    engine->setTemplatesModel  ( data_module->getTemplatesModel()   );
+    engine->setMandatsModel    ( data_module->getMandatsModel() );
+    engine->launchAndConnect();
+}
+
+void MainWindow::appendStartMsg(const QString &txt_msg)
+{
+    eMessage->showMessage(txt_msg);
+}
+MainWindow::~MainWindow()
+{
+
+}
+
+//------------------------------------PRIVATE SLOTS ------------------------------------
+void MainWindow::do_next()
+{
+    switch (pageId +1){
+    case VPrn::Page_Intro:
+    case VPrn::Page_Select:
+        break;
+    case VPrn::Page_PrintData:
+        //Увеличим окно
+        this->resize(800,600);
+        break;
+    case VPrn::Page_CheckData:
+        //Уменьшим окно        
+        this->resize(320,240);
+        break;
+    case VPrn::Page_Preview:
+        this->resize(800,600);
+        break;
+    case VPrn::Page_Finish:
+        break;
+    case VPrn::Page_SetBrak:
+        break;
+    }
+    if (lastCheck(pageId)){
+        pageId++;
+        if ( ( pageId > VPrn::Page_Finish ) || ( pageId > stackedWidget->count()-1 ) ){
+            pageId = VPrn::Page_Finish;
+        }
+        stackedWidget->setCurrentIndex(pageId);
+        this->setWindowTitle(stackedWidget->currentWidget()->windowTitle() );
+    }
+}
+
+void MainWindow::do_prev()
+{
+    switch (pageId - 1){
+    case VPrn::Page_Intro:
+    case VPrn::Page_Select:
+        //Уменьшим окно
+        this->resize(320,240);
+        break;
+    case VPrn::Page_PrintData:
+        //Увеличим окно
+        this->resize(800,600);
+        break;
+    case VPrn::Page_CheckData:
+        //Уменьшим окно
+        this->resize(320,240);
+        break;
+    case VPrn::Page_Preview:
+        //Увеличим окно
+        this->resize(800,600);
+        break;
+    case VPrn::Page_Finish:
+        break;
+    case VPrn::Page_SetBrak:
+        break;
+    }
+    pageId--;
+    if (pageId < VPrn::Page_Intro) {
+        pageId = VPrn::Page_Intro;
+    }    
+    stackedWidget->setCurrentIndex(pageId);
+    this->setWindowTitle(stackedWidget->currentWidget()->windowTitle() );
+}
+
+void MainWindow::do_restart()
+{
+    pageId =0;
+    stackedWidget->setCurrentIndex(pageId);
+    this->setWindowTitle(stackedWidget->currentWidget()->windowTitle() );
+    this->resize(320,240);
+}
+
+
+void MainWindow::do_needAuthUser(const QString &login_mandat_list)
+{
+    QRegExp rx("\\[(.+)\\];:;(.+)");
+    //rx.setMinimal(true);
+    if(rx.indexIn(login_mandat_list) != -1){
+        QString m_login  = rx.cap(1);
+        QString m_list  = rx.cap(2);
+
+        // Запуск окошка авторизации
+        getUserNameMandatDlg *UMDlg = new getUserNameMandatDlg(this);
+        UMDlg->move(this->calcDeskTopCenter(UMDlg->width(),UMDlg->height()));
+        UMDlg->setUserName(m_login);
+        UMDlg->setMandatList(QStringList() << m_list.split(";:;"));
+        connect(UMDlg,SIGNAL(setAuthData(const QString &,const QString &)),
+                engine, SLOT(setAuthData(const QString &,const QString &))
+                );
+
+        UMDlg->exec();
+    }else{
+        eMessage->showMessage(QObject::trUtf8("Ошибка разбора сообщения сервера."));
+    }
+}
+
+void MainWindow::sendFileToConvertor()
+{
+    engine->prepareFileToPrint(data_module->getWorkFile());
+}
+
+//------------------------------------ PRIVATE ---------------------------------------------
+void MainWindow::createConnection()
+{
+    connect (helpButton, SIGNAL(clicked()),
+             this, SLOT( showHelp() )
+             );
+    connect (exitButton,SIGNAL(clicked()),
+             this,      SLOT(close())
+             );
+    connect (nextButton,SIGNAL(clicked()),
+             this,      SLOT(do_next())
+             );
+    connect (prevButton,SIGNAL(clicked()),
+             this,      SLOT(do_prev())
+             );
+    connect (restartButton,SIGNAL(clicked()),
+             this,      SLOT(do_restart())
+             );
+
+    connect (data_module,SIGNAL(error(QString,QString)),
+             eMessage,SLOT(showMessage(QString,QString))
+             );
+
+    connect (engine,   SIGNAL( error(QString,QString) ),
+             eMessage, SLOT  ( showMessage (QString,QString) )
+             );
+    connect(engine,    SIGNAL( LocalSrvRegistr() ),
+            introPage, SLOT  ( setLocalSrvRegistr() )
+            );
+    connect(engine,    SIGNAL( RemoteDemonRegistr() ),
+            introPage, SLOT  ( setRemoteDemonRegistr() )
+            );
+    connect(engine,    SIGNAL(ReciveUserName()  ),
+            introPage, SLOT  (setReciveUserName() )
+            );
+    connect(engine,    SIGNAL( ReciveUserMandat() ),
+            introPage, SLOT  ( setReciveUserMandat() )
+            );
+
+    /** @short Так как прошли уже успешно пройденны фазы:
+      * @li Регистрация на локальном сервере (Значит он существует и доступен)
+      * @li Регистрация на удаленном сервере (Значит он существует и доступен)
+      * можем отправить файлик на конвертацию, в данное время в этом есть смысл
+      * можем запросить метаинформацию о шаблонах
+      */
+    connect(engine,    SIGNAL( RemoteDemonRegistr()  ),
+            this,      SLOT  ( sendFileToConvertor() )
+            );
+    connect(engine,    SIGNAL( needAuthUser(const QString&) ),
+            this,      SLOT  ( do_needAuthUser(const QString&) )
+            );
+
+}
+
+QPoint MainWindow::calcDeskTopCenter(int width,int height)
+{
+    QPoint centerWindow;
+    //получаем прямоугольник с размерами как у экрана
+    centerWindow = desktop_avail.center(); //получаем координаты центра экрана
+    centerWindow.setX(centerWindow.x() - (width/2));
+    centerWindow.setY(centerWindow.y() - (height/2));
+    return centerWindow;
+}
+
+void MainWindow::readConfig(const QString &app_dir)
+{
+    QString ini_file =QString("%1/Technoserv/safe_printer.ini").arg(app_dir);
+    if (QFile::exists(ini_file)){
+        QSettings settings (ini_file,QSettings::IniFormat);
+        settings.setIniCodec("UTF-8");
+        settings.beginGroup("SERVICE");
+        link_name      = settings.value("link_name").toString();
+        settings.endGroup();
+
+        settings.beginGroup("USED_DIR_FILE");
+        //spoolDir       = settings.value("spool_dir").toString();
+        gatekeeper_bin = settings.value("gatekeeper_bin").toString();
+        settings.endGroup();
+    }else{
+        eMessage->showMessage("FileNotFound",
+                              QObject::trUtf8("Файл с настройками программы %1 не найден!\n%2")
+                              .arg(ini_file)
+                              .arg(QString(Q_FUNC_INFO))
+                              );
+    }
+}
+
+bool MainWindow::lastCheck(int page)
+{
+    bool Ok = true;
+    {
+        switch (page +1){
+        case VPrn::Page_Intro:
+            break;
+        case VPrn::Page_Select:
+             Ok &= introPage->enableNext();
+            break;
+        case VPrn::Page_PrintData:
+            break;
+        case VPrn::Page_CheckData:
+            break;
+        case VPrn::Page_Preview:
+            break;
+        case VPrn::Page_Finish:
+            break;
+        case VPrn::Page_SetBrak:
+            break;
+        }
+    }
+    return Ok;
+}
+
+//------------------------------------- PROTECTED ----------------------------------------
+void MainWindow::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        //ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ trunk/vp_plugins/print_monitor/mainwindow.h
@@ -0,0 +1,97 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "intropage.h"
+#include "selectpage.h"
+#include "printdatapage.h"
+#include "checkdatapage.h"
+#include "previewpage.h"
+#include "finishpage.h"
+#include "datamodule.h"
+
+#include <QtGui/QDialog>
+QT_BEGIN_NAMESPACE
+class QAction;
+class QStackedWidget;
+class QFrame;
+class QComboBox;
+class QLabel;
+class QStandardItemModel;
+class QHBoxLayout;
+class QVBoxLayout;
+class QSpacerItem;
+class QDesktopWidget;
+class QErrorMessage;
+QT_END_NAMESPACE
+
+namespace Ui {
+    class MainWindow;
+}
+
+class Engine;
+
+class MainWindow : public QWidget {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void init(const QString &app_dir,const QString &input_file);
+public slots:
+    void appendStartMsg(const QString &txt_msg);
+
+protected:
+    void changeEvent(QEvent *e);
+
+private slots:
+    void do_next();
+    void do_prev();
+    void do_restart();
+    void do_needAuthUser(const QString &login_mandat_list);
+    void sendFileToConvertor();
+
+private:        
+    QDesktopWidget desktop;
+    int            pageId;
+    IntroPage      *introPage;
+    SelectPage     *selectPage;
+    PrintDataPage  *printDataPage;
+    CheckDataPage  *checkDataPage;
+    PreViewPage    *preViewPage;
+    FinishPage     *finishPage;
+
+    QStackedWidget *stackedWidget;
+    QHBoxLayout    *horizontalLayout;
+    QVBoxLayout    *mainLayout;
+
+    QPushButton    *helpButton;    
+    QPushButton    *prevButton;
+    QPushButton    *nextButton;
+    QPushButton    *restartButton;
+    QPushButton    *exitButton;
+    QSpacerItem    *horizontalSpacer;
+    QSpacerItem    *horizontalSpacer_2;
+
+    QWidget        *verticalLayoutWidget;
+    QWidget        *horizontalLayoutWidget;
+    QFrame         *line;
+    QErrorMessage  *eMessage;
+
+    DataModule     *data_module;
+    Engine         *engine;
+
+    QString link_name;
+    QString gatekeeper_bin;
+    QRect   desktop_avail;
+
+    //----------------------------------------
+    void createConnection();
+    QPoint calcDeskTopCenter(int width,int height);
+    void readConfig(const QString &app_dir);
+    /**
+      * @fn bool lastCheck(int page);
+      * @brief Проверка можно ли перейти на следующую страницу
+      */
+    bool lastCheck(int page);
+
+};
+
+#endif // MAINWINDOW_H
