68669b3a0624f243223436189c8c70cbe070d1f9
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-08-20 17:04:30 +0000

    Обновил интерфейс, запоминаем бинарник gsprint

--- trunk/vp_plugins/interfaces/igs_plugin.h
+++ trunk/vp_plugins/interfaces/igs_plugin.h
@@ -12,7 +12,7 @@ class Igs_plugin{
 public:
     virtual ~Igs_plugin() {}
     virtual void init(const QString &gs_bin,const QString &pdftk_bin,
-                      const QString &temp_folder)=0;
+                      const QString &gsprint_bin,const QString &temp_folder)=0;
     virtual void convertPs2Pdf(const QString &client_uuid,
                                const QString &input_fn) = 0;
     virtual void convertPdfToPng(const QString &client_uuid,
