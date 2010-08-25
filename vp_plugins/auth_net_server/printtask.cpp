5d2a990ca9f8c51f425bc8e61575ca7c5f2ea75d
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2010-08-25 02:59:56 +0000

    Обновил класс управляющий заданиями на печать

--- vp_plugins/auth_net_server/printtask.cpp
+++ vp_plugins/auth_net_server/printtask.cpp
@@ -1,11 +1,41 @@
 #include "printtask.h"
 
 PrintTask::PrintTask(QObject *parent)
-    :QObject(parent)
-    ,m_docName( QString() )
-    ,m_mb( QString() )    
-    ,m_printerQueue( QString() )
-    ,m_pageCount(0)    
+        :QObject(parent)
+        ,m_docName( QString() )        
+        ,m_printerQueue( QString() )
+        ,m_pageCount(0)
 
 {
+    queueFiles2Print.clear();
 }
+
+void PrintTask::setPrinterQueue ( const QString &s)
+{
+    if (s.isEmpty()){
+        return;
+    }
+    m_printerQueue = s;
+}
+
+void PrintTask::setDocName      ( const QString &s)
+{
+    if (s.isEmpty()){
+        return;
+    }
+    m_docName = s;
+}
+
+void PrintTask::setPageCount    ( int cnt)
+{
+    m_pageCount = cnt;
+}
+
+void PrintTask::addFileToPrintQueue  ( const QString &s)
+{
+    if (s.isEmpty()){
+        return;
+    }
+    queueFiles2Print.push(s);
+}
+
