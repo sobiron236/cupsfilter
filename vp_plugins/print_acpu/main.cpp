1ac342bfb8e1e037eab085bf1858727aec57adce
p.slant <p.slant@70a433b4-4736-11de-b25d-9f9778916a3c>
2011-02-27 20:47:46 +0000

    add resource

--- vp_plugins/print_acpu/main.cpp
+++ vp_plugins/print_acpu/main.cpp
@@ -1,14 +1,23 @@
 #include <QtGui/QApplication>
-#include "mainwindow.h"
+#include "maindialog.h"
 #include "config.h"
+#include "glass.h"
 
 int main(int argc, char *argv[])
 {
-QApplication a(argc, argv);
+    Q_INIT_RESOURCE(images);
+
+    QApplication a(argc, argv);
     installLog("t5",QObject::trUtf8("Техносерв А/Г"));
     
-    MainWindow w;
-    w.show();
+    Glass *infoGlass = new Glass();
+    infoGlass->enableInfoTextBlock(QString("Test messages."));
+
+
+
+    mainDialog dlg;
+    infoGlass->install (dlg);
+    dlg.show();
 
     return a.exec();
 }
